d6e97e637a4351db822b698d3da7aa44422dd437
mikebowler <mbowler@GargoyleSoftware.com>
2002-09-11 20:37:20 +0000

    Added a Version class and updated the manifest to make this the "Main-Class"
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@23 5f5364db-9458-4db8-a492-e30667be6df6

---
+++ src/java/com/gargoylesoftware/htmlunit/Version.java
@@ -0,0 +1,13 @@
+/*
+ *  Copyright (C) 2002 Gargoyle Software. All rights reserved.
+ *
+ *  This file is part of HtmlUnit. For details on use and redistribution
+ *  please refer to the license.html file included with these sources.
+ */
+package com.gargoylesoftware.htmlunit;
+
+public class Version {
+    public static void main( final String args[] ) {
+        System.out.println("HTMLUnit (c) Gargoyle Software Inc.");
+    }
+}
