f7e598ed60558700c24b2acea0b919a10797156e
mikebowler <mbowler@GargoyleSoftware.com>
2002-09-16 10:08:20 +0000

    Upgraded version of checkstyle Fixed warnings identified by checkstyle
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk@29 5f5364db-9458-4db8-a492-e30667be6df6

--- htmlunit/src/java/com/gargoylesoftware/htmlunit/Version.java
+++ htmlunit/src/java/com/gargoylesoftware/htmlunit/Version.java
@@ -6,7 +6,18 @@
  */
 package com.gargoylesoftware.htmlunit;
 
+/**
+ * Class to display version information about HtmlUnit.  This is the class
+ * that will be executed if the jar file is run.
+ *
+ * @version  $Revision$
+ * @author  <a href="mailto:mbowler@GargoyleSoftware.com">Mike Bowler</a>
+ */
 public class Version {
+    /**
+     * The main entry point into this class.
+     * @param args The arguments passed on the command line
+     */
     public static void main( final String args[] ) {
         final Package aPackage = Package.getPackage("com.gargoylesoftware.htmlunit");
 
