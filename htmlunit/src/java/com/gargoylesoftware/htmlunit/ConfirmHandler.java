3e56079fd73d5f82ffba83dcf28f408ec7ea967e
mikebowler <mbowler@GargoyleSoftware.com>
2002-10-09 14:45:39 +0000

    Initial checkin
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk@51 5f5364db-9458-4db8-a492-e30667be6df6

---
+++ htmlunit/src/java/com/gargoylesoftware/htmlunit/ConfirmHandler.java
@@ -0,0 +1,24 @@
+/*
+ *  Copyright (C) 2002 Gargoyle Software Inc. All rights reserved.
+ *
+ *  This file is part of HtmlUnit. For details on use and redistribution
+ *  please refer to the license.html file included with these sources.
+ */
+package com.gargoylesoftware.htmlunit;
+
+
+/**
+ * A handler for javascript window.confirm().  Confirms are triggered when the javascript
+ * method Window.confirm() is called.
+ *
+ * @version  $Revision$
+ * @author  <a href="mailto:mbowler@GargoyleSoftware.com">Mike Bowler</a>
+ */
+public interface ConfirmHandler {
+    /**
+     * Handle an alert for the given page.
+     * @param page The page on which the alert occurred.
+     * @param message The message in the alert.
+     */
+    boolean handleConfirm( final Page page, final String message );
+}
