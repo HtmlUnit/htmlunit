4e27058627580a8d76325a10675ad5fc2bb05889
mikebowler <mbowler@GargoyleSoftware.com>
2002-10-11 19:04:51 +0000

    Added click() method for HtmlCheckBoxInput and HtmlRadioButtonInput
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@55 5f5364db-9458-4db8-a492-e30667be6df6

--- src/java/com/gargoylesoftware/htmlunit/html/HtmlRadioButtonInput.java
+++ src/java/com/gargoylesoftware/htmlunit/html/HtmlRadioButtonInput.java
@@ -69,5 +69,22 @@ public class HtmlRadioButtonInput extends HtmlInput {
             getElement().removeAttribute("checked");
         }
     }
+
+
+    /**
+     *  Submit the form that contains this input
+     *
+     * @return  The Page that is the result of submitting this page to the
+     *      server
+     * @exception  IOException If an io error occurs
+     * @exception  ElementNotFoundException If a particular xml element could
+     *      not be found in the dom model
+     */
+    public Page click()
+        throws
+            IOException,
+            ElementNotFoundException {
+        return super.click();
+    }
 }
 
