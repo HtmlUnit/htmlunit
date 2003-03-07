115cf03309e409087812c06eba4b24fc8f131eb8
mikebowler <mbowler@GargoyleSoftware.com>
2003-03-07 19:39:37 +0000

    All tests now pretend to be Mozilla 1.0 Https tests now fail with a message rather than a stack trace if https is not supported (ie java 1.3)
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@157 5f5364db-9458-4db8-a492-e30667be6df6

--- src/java/com/gargoylesoftware/htmlunit/test/SanityCheck.java
+++ src/java/com/gargoylesoftware/htmlunit/test/SanityCheck.java
@@ -6,9 +6,11 @@
  */
 package com.gargoylesoftware.htmlunit.test;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import java.net.MalformedURLException;
 import java.net.URL;
 import junit.textui.TestRunner;
 
@@ -21,6 +23,7 @@ import junit.textui.TestRunner;
  * @author     <a href="mailto:mbowler@GargoyleSoftware.com">Mike Bowler</a>
  */
 public class SanityCheck extends WebTestCase {
+    private static final BrowserVersion browserVersion_ = BrowserVersion.MOZILLA_1_0;
 
     public SanityCheck( final String name ) {
         super( name );
@@ -35,19 +38,19 @@ public class SanityCheck extends WebTestCase {
 
 
     public void testYahooMail() throws Exception {
-        final WebClient webClient = new WebClient();
+        final WebClient webClient = new WebClient(browserVersion_);
         assertInstanceOf(webClient.getPage( new URL( "http://mail.yahoo.com/" ) ), HtmlPage.class);
     }
 
 
     public void testYahoo() throws Exception {
-        final WebClient webClient = new WebClient();
+        final WebClient webClient = new WebClient(browserVersion_);
         assertInstanceOf( webClient.getPage( new URL( "http://yahoo.com/" ) ), HtmlPage.class );
     }
 
 
     public void testIBM() throws Exception {
-        final WebClient webClient = new WebClient();
+        final WebClient webClient = new WebClient(browserVersion_);
         webClient.setRedirectEnabled( true );
         final HtmlPage page = (HtmlPage)webClient.getPage( new URL( "http://www.ibm.com/" ) );
         assertEquals( "http://www.ibm.com/us/", page.getWebResponse().getUrl().toExternalForm() );
@@ -55,41 +58,51 @@ public class SanityCheck extends WebTestCase {
 
 
     public void testAlphaWorks() throws Exception {
-        final WebClient webClient = new WebClient();
+        final WebClient webClient = new WebClient(browserVersion_);
         assertInstanceOf(webClient.getPage(new URL( "http://www.alphaworks.ibm.com" ) ), HtmlPage.class);
     }
 
 
     public void testCNN() throws Exception {
-        final WebClient webClient = new WebClient();
+        final WebClient webClient = new WebClient(browserVersion_);
         assertInstanceOf( webClient.getPage( new URL( "http://www.cnn.com" ) ), HtmlPage.class);
     }
 
 
     public void testToyotaCanada() throws Exception {
-        final WebClient webClient = new WebClient();
+        final WebClient webClient = new WebClient(browserVersion_);
         assertInstanceOf(webClient.getPage( new URL( "http://www.toyota.ca" ) ), HtmlPage.class);
     }
 
 
     public void testSourceForge_secure() throws Exception {
-        final WebClient webClient = new WebClient();
-        webClient.setPrintContentOnFailingStatusCode(true);
-        assertInstanceOf( webClient.getPage( new URL( "https://sourceforge.net/projects/htmlunit/" ) ), HtmlPage.class );
+        try {
+            final WebClient webClient = new WebClient(browserVersion_);
+            webClient.setPrintContentOnFailingStatusCode(true);
+            assertInstanceOf( webClient.getPage( new URL( "https://sourceforge.net/projects/htmlunit/" ) ), HtmlPage.class );
+        }
+        catch( final MalformedURLException e ) {
+            System.out.println("Skipping https test: "+getName());
+        }
     }
 
 
     public void testYahooLogin_secure() throws Exception {
-        final WebClient webClient = new WebClient();
-        final HtmlPage page = (HtmlPage)webClient.getPage( new URL( "https://login.yahoo.com/" ) );
-        final HtmlForm form = page.getFormByName("login_form");
-        assertNotNull(form);
+        try {
+            final WebClient webClient = new WebClient(browserVersion_);
+            final HtmlPage page = (HtmlPage)webClient.getPage( new URL( "https://login.yahoo.com/" ) );
+            final HtmlForm form = page.getFormByName("login_form");
+            assertNotNull(form);
+        }
+        catch( final MalformedURLException e ) {
+            System.out.println("Skipping https test: "+getName());
+        }
     }
 
 
     public void testAmazonCanada() throws Exception {
-        final WebClient webClient = new WebClient();
-        assertInstanceOf( webClient.getPage( new URL( "http://www.amazon.ca" ) ), HtmlPage.class );
+        final WebClient webClient = new WebClient(browserVersion_);
+        assertInstanceOf( webClient.getPage( new URL( "http://www.amazon.ca/" ) ), HtmlPage.class );
     }
 
 
