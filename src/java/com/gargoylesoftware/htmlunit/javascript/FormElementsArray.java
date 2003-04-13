f795e7ceb696990eeb1c5224b5c63fc2c788e897
mikebowler <mbowler@GargoyleSoftware.com>
2003-04-13 23:22:11 +0000

    Refactored some logic out of Form into FormElementsArray for easier maintenance
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@247 5f5364db-9458-4db8-a492-e30667be6df6

---
+++ src/java/com/gargoylesoftware/htmlunit/javascript/FormElementsArray.java
@@ -0,0 +1,155 @@
+/*
+ *  Copyright (C) 2002, 2003 Gargoyle Software Inc. All rights reserved.
+ *
+ *  This file is part of HtmlUnit. For details on use and redistribution
+ *  please refer to the license.html file included with these sources.
+ */
+package com.gargoylesoftware.htmlunit.javascript;
+
+import com.gargoylesoftware.htmlunit.ElementNotFoundException;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlRadioButtonInput;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Function;
+import org.mozilla.javascript.NativeArray;
+import org.mozilla.javascript.Scriptable;
+import org.w3c.dom.Element;
+
+/**
+ * This is the array returned by the "elements" property of Form.
+ *
+ * @version  $Revision$
+ * @author  <a href="mailto:mbowler@GargoyleSoftware.com">Mike Bowler</a>
+ */
+public class FormElementsArray extends SimpleScriptable {
+    private HtmlForm htmlForm_;
+
+    /**
+     * Create an instance.  Javascript objects must have a default constructor.
+     */
+    public FormElementsArray() {
+    }
+
+
+    /**
+     * Javascript constructor.  This must be declared in every javascript file because
+     * the rhino engine won't walk up the hierarchy looking for constructors.
+     */
+    public final void jsConstructor() {
+    }
+
+
+    /**
+     * Initialize this object
+     * @param page The HtmlPage that this object will retrive elements from.
+     */
+    public void initialize( final HtmlForm form ) {
+        assertNotNull("form", form);
+        htmlForm_ = form;
+    }
+
+
+    /**
+     * <p>Return the object at the specified index.</p>
+     *
+     * <p>TODO: This implementation is particularly inefficient but without a way
+     * to detect if an element has been inserted or removed, it isn't safe to
+     * cache the array.<p>
+     *
+     * @param index The index
+     * @param start The object that get is being called on.
+     * @return The object or NOT_FOUND
+     */
+    public Object get( final int index, final Scriptable start ) {
+        final HtmlForm htmlForm = ((FormElementsArray)start).htmlForm_;
+        final List formElements = getHtmlElementsInForm(htmlForm);
+        try {
+            return getScriptableFor( (HtmlElement)formElements.get(index) );
+        }
+        catch( final IndexOutOfBoundsException e ) {
+            return NOT_FOUND;
+        }
+    }
+
+
+    /**
+     * Return the object at the specified id/name or NOT_FOUND.  First we search
+     * for an element with this id, if that fails then we try the same string as
+     * a name.  If more than one element has the same name then an array of
+     * elements is returned.
+     *
+     * @param name The name.
+     * @param start The object that get is being called on.
+     * @return The object or NOT_FOUND
+     */
+     public Object get( final String name, final Scriptable start ) {
+         final HtmlForm htmlForm = htmlForm_;
+         if(htmlForm == null ) {
+             // The object hasn't fully been initialized yet so just pass through to the superclass
+             return super.get(name, start);
+         }
+
+        final List elementList = getHtmlElementsInForm(htmlForm);
+        HtmlElement htmlElement;
+        final Iterator iterator = elementList.iterator();
+        while( iterator.hasNext() ) {
+            htmlElement = (HtmlElement)iterator.next();
+            if( htmlElement.getAttributeValue("name").equals(name) ) {
+                if( htmlElement instanceof HtmlRadioButtonInput ) {
+                    final List collectedRadioButtons = new ArrayList(elementList.size());
+                    collectedRadioButtons.add(getScriptableFor(htmlElement));
+                    while( iterator.hasNext() ) {
+                        htmlElement = (HtmlElement)iterator.next();
+                        if( htmlElement instanceof HtmlRadioButtonInput
+                            && ((HtmlRadioButtonInput)htmlElement).getNameAttribute().equals(name) ) {
+                            collectedRadioButtons.add(getScriptableFor(htmlElement));
+                        }
+                    }
+
+                    switch( collectedRadioButtons.size() ) {
+                        case 0:  return NOT_FOUND;
+                        case 1:  return collectedRadioButtons.get(0);
+                        default: return new NativeArray( collectedRadioButtons.toArray() );
+                    }
+                }
+                else {
+                    return getScriptableFor(htmlElement);
+                }
+            }
+        }
+
+        return super.get(name, start);
+     }
+
+
+   /**
+    * <p>Return the number of elements in this array</p>
+    *
+    * <p>TODO: This implementation is particularly inefficient but without a way
+    * to detect if an element has been inserted or removed, it isn't safe to
+    * cache the array/<p>
+    *
+    * @return The number of elements in the array
+    */
+    public int jsGet_length() {
+        return getHtmlElementsInForm(htmlForm_).size();
+    }
+
+
+    private List getHtmlElementsInForm( final HtmlForm htmlForm ) {
+        assertNotNull("htmlForm", htmlForm);
+
+        return htmlForm.getHtmlElementsByTagNames(
+            Arrays.asList( new String[]{
+                "input", "button", /*"option",*/ "select", "textarea"
+            } ) );
+    }
+}
