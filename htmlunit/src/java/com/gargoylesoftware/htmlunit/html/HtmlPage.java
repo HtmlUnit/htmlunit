f37fd25ca957e097488be4efdbd6470614f3b65d
mikebowler <mbowler@GargoyleSoftware.com>
2003-10-12 16:03:04 +0000

    Removed unused import
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk@401 5f5364db-9458-4db8-a492-e30667be6df6

--- htmlunit/src/java/com/gargoylesoftware/htmlunit/html/HtmlPage.java
+++ htmlunit/src/java/com/gargoylesoftware/htmlunit/html/HtmlPage.java
@@ -49,7 +49,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
 
 import org.apache.commons.httpclient.HttpConstants;
 import org.apache.xerces.xni.parser.XMLDocumentFilter;
