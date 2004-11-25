3f6791e2d535012e872deb3b87f57b5f29df08e6
mguillem <mguillemot@yahoo.fr>
2004-11-25 09:44:23 +0000

    added some simple xpath tests
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk@664 5f5364db-9458-4db8-a492-e30667be6df6

---
+++ htmlunit/src/test/java/com/gargoylesoftware/htmlunit/html/xpath/HtmlUnitXPathTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2002, 2004 Gargoyle Software Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. The end-user documentation included with the redistribution, if any, must
+ *    include the following acknowledgment:
+ *
+ *       "This product includes software developed by Gargoyle Software Inc.
+ *        (http://www.GargoyleSoftware.com/)."
+ *
+ *    Alternately, this acknowledgment may appear in the software itself, if
+ *    and wherever such third-party acknowledgments normally appear.
+ * 4. The name "Gargoyle Software" must not be used to endorse or promote
+ *    products derived from this software without prior written permission.
+ *    For written permission, please contact info@GargoyleSoftware.com.
+ * 5. Products derived from this software may not be called "HtmlUnit", nor may
+ *    "HtmlUnit" appear in their name, without prior written permission of
+ *    Gargoyle Software Inc.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GARGOYLE
+ * SOFTWARE INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.gargoylesoftware.htmlunit.html.xpath;
+
+import com.gargoylesoftware.htmlunit.WebTestCase;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+/**
+ * <p>Tests for XPath evaluation on HtmlUnit DOM.
+ *
+ * @version $Revision$
+ * @author Marc Guillemot
+ */
+public class HtmlUnitXPathTest extends WebTestCase {
+
+    /**
+     *  Create an instance
+     *
+     * @param  name The name of the test
+     */
+    public HtmlUnitXPathTest( final String name ) {
+        super( name );
+    }
+
+    /**
+     * Test evaluation of some simple paths
+     * @throws Exception if test fails
+     */
+    public void testSimplePath() throws Exception {
+        final String content = "<html><head><title>Test page</title></head>\n"
+            + "<body><a href='foo.html' id='myLink'>foo</a></body>\n"
+            + "</html>";
+
+        final HtmlPage page = loadPage(content);
+        HtmlUnitXPath xpath = new HtmlUnitXPath("/html");
+        assertEquals(page.getDocumentElement(), xpath.selectSingleNode(page));
+        xpath = new HtmlUnitXPath("/html/head");
+        assertEquals(page.getDocumentElement().getFirstChild(), xpath.selectSingleNode(page));
+        xpath = new HtmlUnitXPath("/html/body/a");
+        assertEquals(page.getHtmlElementById("myLink"), xpath.selectSingleNode(page));
+        xpath = new HtmlUnitXPath("/html/head/title/text()");
+        assertEquals("Test page", xpath.stringValueOf(page));
+    }
+}
