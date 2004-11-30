b5ebc177aa039f6ec225dc5d53c6d310f5d90991
mguillem <mguillemot@yahoo.fr>
2004-11-30 10:32:58 +0000

    responses of type "text/xml" are now represented as XmlPage
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@667 5f5364db-9458-4db8-a492-e30667be6df6

---
+++ src/java/com/gargoylesoftware/htmlunit/xml/XmlPage.java
@@ -0,0 +1,189 @@
+/*
+ * Copyright (c) 2002, 2004 Gargoyle Software Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. The end-user documentation included with the redistribution, if any, must
+ *    include the following acknowledgment:
+ *
+ *       "This product includes software developed by Gargoyle Software Inc.
+ *        (http://www.GargoyleSoftware.com/)."
+ *
+ *    Alternately, this acknowledgment may appear in the software itself, if
+ *    and wherever such third-party acknowledgments normally appear.
+ * 4. The name "Gargoyle Software" must not be used to endorse or promote
+ *    products derived from this software without prior written permission.
+ *    For written permission, please contact info@GargoyleSoftware.com.
+ * 5. Products derived from this software may not be called "HtmlUnit", nor may
+ *    "HtmlUnit" appear in their name, without prior written permission of
+ *    Gargoyle Software Inc.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GARGOYLE
+ * SOFTWARE INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.gargoylesoftware.htmlunit.xml;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebResponse;
+import com.gargoylesoftware.htmlunit.WebWindow;
+
+/**
+ * A page that will be returned for response with content type "text/xml".
+ * It doesn't implement itself {@link org.w3c.dom.Document} to allow to see the source of badly formed
+ * xml responses.
+ * @version  $Revision$
+ * @author Marc Guillemot
+ */
+public class XmlPage implements Page {
+    private final String content_;
+    private Document document_;
+
+    private WebWindow enclosingWindow_;
+    private final WebResponse webResponse_;
+    private static final ErrorHandler DISCARD_MESSAGES_HANDLER = new ErrorHandler() {
+        /**
+         * Does nothing as we're not interested in.
+         * @see org.xml.sax.ErrorHandler#error(org.xml.sax.SAXParseException)
+         */
+        public void error(final SAXParseException exception) throws SAXException {
+            // Does nothing as we're not interested in.
+        }
+        /**
+         * Does nothing as we're not interested in.
+         * @see org.xml.sax.ErrorHandler#fatalError(org.xml.sax.SAXParseException)
+         */
+        public void fatalError(final SAXParseException exception)
+                throws SAXException {
+            // Does nothing as we're not interested in.
+        }
+        /**
+         * Does nothing as we're not interested in.
+         * @see org.xml.sax.ErrorHandler#warning(org.xml.sax.SAXParseException)
+         */
+        public void warning(final SAXParseException exception)
+                throws SAXException {
+            // Does nothing as we're not interested in.
+        }
+       };
+
+
+    /**
+     * Create an instance. 
+     * A warning is logged if an exception is thrown while parsing the xml content 
+     * (for instance when the content is not a valid xml and can't be parsed).
+     *
+     * @param  webResponse The response from the server
+     * @param  enclosingWindow The window that holds the page.
+     * @throws IOException If the page could not be created
+     */
+    public XmlPage( final WebResponse webResponse, final WebWindow enclosingWindow ) throws IOException {
+        webResponse_ = webResponse;
+        content_ = webResponse.getContentAsString();
+        enclosingWindow_ = enclosingWindow;
+
+        DocumentBuilderFactory factory =
+            DocumentBuilderFactory.newInstance();
+        InputSource source = new InputSource(new StringReader(content_));
+        try {
+           DocumentBuilder builder = factory.newDocumentBuilder();
+           builder.setErrorHandler(DISCARD_MESSAGES_HANDLER);
+           document_ = builder.parse(source);
+        }
+        catch (final SAXException e) {
+            getLog().warn("Failed parsing xml document " + webResponse.getUrl() + ": " + e.getMessage());
+        }
+        catch (final ParserConfigurationException e) {
+            getLog().warn("Failed parsing xml document " + webResponse.getUrl() + ": " + e.getMessage());
+        }
+
+    }
+
+
+    /**
+     * Clean up this page.
+     */
+    public void cleanUp() {
+    }
+
+
+    /**
+     *  Return the content of the page
+     *
+     * @return  See above
+     */
+    public String getContent() {
+        return content_;
+    }
+
+
+    /**
+     * Return the window that this page is sitting inside.
+     *
+     * @return The enclosing frame or null if this page isn't inside a frame.
+     */
+    public WebWindow getEnclosingWindow() {
+        return enclosingWindow_;
+    }
+
+    /**
+     * Return the log object for this web client
+     * @return The log object
+     */
+    protected final Log getLog() {
+        return LogFactory.getLog(getClass());
+    }
+
+
+    /**
+     *  Return the web response that was originally used to create this page.
+     *
+     * @return  The web response
+     */
+    public WebResponse getWebResponse() {
+        return webResponse_;
+    }
+
+    /**
+     * Gets the DOM representation of the xml content 
+     * @return <code>null</code> if the content couldn't be parsed.
+     */
+    public Document getXmlDocument() {
+        return document_;
+    }
+
+    /**
+     * Initialize this page.
+     */
+    public void initialize() {
+    }
+}
+
---
+++ src/test/java/com/gargoylesoftware/htmlunit/xml/XmlPageTest.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2002, 2004 Gargoyle Software Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. The end-user documentation included with the redistribution, if any, must
+ *    include the following acknowledgment:
+ *
+ *       "This product includes software developed by Gargoyle Software Inc.
+ *        (http://www.GargoyleSoftware.com/)."
+ *
+ *    Alternately, this acknowledgment may appear in the software itself, if
+ *    and wherever such third-party acknowledgments normally appear.
+ * 4. The name "Gargoyle Software" must not be used to endorse or promote
+ *    products derived from this software without prior written permission.
+ *    For written permission, please contact info@GargoyleSoftware.com.
+ * 5. Products derived from this software may not be called "HtmlUnit", nor may
+ *    "HtmlUnit" appear in their name, without prior written permission of
+ *    Gargoyle Software Inc.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GARGOYLE
+ * SOFTWARE INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.gargoylesoftware.htmlunit.xml;
+
+import com.gargoylesoftware.htmlunit.MockWebConnection;
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebTestCase;
+
+/**
+ * Tests for SimpleScriptable.
+ *
+ * @version  $Revision$
+ * @author Marc Guillemot
+ */
+public class XmlPageTest extends WebTestCase {
+    /**
+     * Create an instance
+     * @param name The name of the test
+     */
+    public XmlPageTest( final String name ) {
+        super(name);
+    }
+
+
+    /**
+     * Tests a simple valid xml document
+     * @throws Exception if the test fails
+     */
+    public void testValidDocument() throws Exception {
+        final WebClient client = new WebClient();
+        final MockWebConnection webConnection = new MockWebConnection( client );
+
+        final String content
+             = "<?xml version=\"1.0\"?>"
+             + "<foo>\n"
+             + "    <foofoo name='first'>something</foofoo>\n"
+             + "    <foofoo name='second'>something else</foofoo>\n"
+             + "</foo>";
+
+        webConnection.setDefaultResponse(content, 200, "OK", "text/xml");
+        client.setWebConnection( webConnection );
+
+        final Page page = client.getPage(URL_FIRST);
+        assertEquals(URL_FIRST.toExternalForm(), page.getWebResponse().getUrl().toExternalForm());
+        assertEquals("OK", page.getWebResponse().getStatusMessage());
+        assertEquals(200, page.getWebResponse().getStatusCode());
+        assertEquals("text/xml", page.getWebResponse().getContentType());
+
+        assertInstanceOf(page, XmlPage.class);
+        final XmlPage xmlPage = (XmlPage) page;
+        assertEquals(content, xmlPage.getContent());
+        assertNotNull(xmlPage.getXmlDocument());
+        assertEquals("foo", xmlPage.getXmlDocument().getFirstChild().getNodeName());
+    }
+
+
+    /**
+     * Tests a simple invalid (badly formed) xml document
+     * @throws Exception if the test fails
+     */
+    public void testInvalidDocument() throws Exception {
+        final WebClient client = new WebClient();
+        final MockWebConnection webConnection = new MockWebConnection( client );
+
+        final String content
+             = "<?xml version=\"1.0\"?>"
+             + "<foo>\n"
+             + "    <foofoo invalid\n"
+             + "    <foofoo name='first'>something</foofoo>\n"
+             + "    <foofoo name='second'>something else</foofoo>\n"
+             + "</foo>";
+
+        webConnection.setDefaultResponse(content, 200, "OK", "text/xml");
+        client.setWebConnection( webConnection );
+
+        final Page page = client.getPage(URL_FIRST);
+        assertEquals(URL_FIRST.toExternalForm(), page.getWebResponse().getUrl().toExternalForm());
+        assertEquals("OK", page.getWebResponse().getStatusMessage());
+        assertEquals(200, page.getWebResponse().getStatusCode());
+        assertEquals("text/xml", page.getWebResponse().getContentType());
+
+        assertInstanceOf(page, XmlPage.class);
+        final XmlPage xmlPage = (XmlPage) page;
+        assertEquals(content, xmlPage.getContent());
+        assertNull(xmlPage.getXmlDocument());
+    }
+}
