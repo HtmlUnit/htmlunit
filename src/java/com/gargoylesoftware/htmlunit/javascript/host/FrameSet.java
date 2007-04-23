298c3b5602f54cd1ec41e85c35f3fea787645420
gredler <daniel.gredler@gmail.com>
2007-04-23 17:07:29 +0000

    Fix bug 1705559: add support for frameset.rows and frameset.cols properties.
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@1391 5f5364db-9458-4db8-a492-e30667be6df6

---
+++ src/java/com/gargoylesoftware/htmlunit/javascript/host/FrameSet.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2002-2006 Gargoyle Software Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. The end-user documentation included with the redistribution, if any, must
+ *    include the following acknowledgment:
+ *
+ *       "This product includes software developed by Gargoyle Software Inc.
+ *        (http://www.GargoyleSoftware.com/)."
+ *
+ *    Alternately, this acknowledgment may appear in the software itself, if
+ *    and wherever such third-party acknowledgments normally appear.
+ * 4. The name "Gargoyle Software" must not be used to endorse or promote
+ *    products derived from this software without prior written permission.
+ *    For written permission, please contact info@GargoyleSoftware.com.
+ * 5. Products derived from this software may not be called "HtmlUnit", nor may
+ *    "HtmlUnit" appear in their name, without prior written permission of
+ *    Gargoyle Software Inc.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GARGOYLE
+ * SOFTWARE INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.gargoylesoftware.htmlunit.javascript.host;
+
+import com.gargoylesoftware.htmlunit.html.HtmlFrameSet;
+
+/**
+ * Wrapper for the HTML element "frameset".
+ *
+ * @version  $Revision: $
+ * @author Bruce Chapman
+ */
+public class FrameSet extends HTMLElement {
+
+    private static final long serialVersionUID = 5630843390548382869L;
+
+    /**
+     * Creates a new frameset instance.
+     */
+    public FrameSet() {
+        // Empty.
+    }
+
+    /**
+     * JavaScript constructor. This must be declared in every JavaScript file because the Rhino
+     * engine won't walk up the hierarchy looking for constructors.
+     */
+    public void jsConstructor() {
+        // Empty.
+    }
+
+    /**
+     * Sets the rows property.
+     * 
+     * @param rows The rows attribute value.
+     */
+    public void jsxSet_rows(final String rows) {
+        final HtmlFrameSet htmlFrameSet = (HtmlFrameSet) getHtmlElementOrNull();
+        if (htmlFrameSet != null) {
+            htmlFrameSet.setAttributeValue("rows", rows);
+        }
+    }
+
+    /**
+     * Gets the rows property.
+     * 
+     * @return The rows attribute value.
+     */
+
+    public String jsxGet_rows() {
+        final HtmlFrameSet htmlFrameSet = (HtmlFrameSet) getHtmlElementOrNull();
+        return htmlFrameSet.getRowsAttribute();
+    }
+
+    /**
+     * Sets the cols property.
+     * 
+     * @param cols The cols attribute value.
+     */
+    public void jsxSet_cols(final String cols) {
+        final HtmlFrameSet htmlFrameSet = (HtmlFrameSet) getHtmlElementOrNull();
+        if (htmlFrameSet != null) {
+            htmlFrameSet.setAttributeValue("cols", cols);
+        }
+    }
+
+    /**
+     * Gets the cols property.
+     * 
+     * @return The cols attribute value.
+     */
+
+    public String jsxGet_cols() {
+        final HtmlFrameSet htmlFrameSet = (HtmlFrameSet) getHtmlElementOrNull();
+        return htmlFrameSet.getColsAttribute();
+    }
+
+}
---
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/FrameSetTest.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) 2002-2006 Gargoyle Software Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. The end-user documentation included with the redistribution, if any, must
+ *    include the following acknowledgment:
+ *
+ *       "This product includes software developed by Gargoyle Software Inc.
+ *        (http://www.GargoyleSoftware.com/)."
+ *
+ *    Alternately, this acknowledgment may appear in the software itself, if
+ *    and wherever such third-party acknowledgments normally appear.
+ * 4. The name "Gargoyle Software" must not be used to endorse or promote
+ *    products derived from this software without prior written permission.
+ *    For written permission, please contact info@GargoyleSoftware.com.
+ * 5. Products derived from this software may not be called "HtmlUnit", nor may
+ *    "HtmlUnit" appear in their name, without prior written permission of
+ *    Gargoyle Software Inc.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GARGOYLE
+ * SOFTWARE INC. OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.gargoylesoftware.htmlunit.javascript.host;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.gargoylesoftware.htmlunit.CollectingAlertHandler;
+import com.gargoylesoftware.htmlunit.MockWebConnection;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebTestCase;
+
+/**
+ * Unit tests for {@link FrameSet}.
+ *
+ * @version  $Revision: $
+ * @author Bruce Chapman
+ */
+public class FrameSetTest extends WebTestCase {
+
+    /**
+     * Creates an instance.
+     * 
+     * @param name The name of the test.
+     */
+    public FrameSetTest(final String name) {
+        super(name);
+    }
+
+    /**
+     * @throws Exception If an error occurs.
+     */
+    public void testCols() throws Exception {
+
+        final String framesetContent =
+            "<html><head><title>First</title></head>"
+            + "<frameset id='fs' cols='20%,*'>"
+            + "    <frame name='left' src='about:blank' />"
+            + "    <frame name='right' src='about:blank' />"
+            + "</frameset>"
+            + "<script>"
+            + "    alert(document.getElementById('fs').cols);"
+            + "    document.getElementById('fs').cols = '*,*';"
+            + "    alert(document.getElementById('fs').cols);"
+            + "</script>"
+            + "</html>";
+
+        final List collectedAlerts = new ArrayList();
+        loadPage(framesetContent, collectedAlerts);
+
+        final List expectedAlerts = Arrays.asList(new String[] { "20%,*", "*,*" });
+        assertEquals(expectedAlerts, collectedAlerts);
+    }
+
+    /**
+     * @throws Exception If an error occurs.
+     */
+    public void testRows() throws Exception {
+
+        final String framesetContent =
+            "<html><head><title>First</title></head>"
+            + "<frameset id='fs' rows='20%,*'>"
+            + "    <frame name='top' src='" + URL_SECOND + "' />"
+            + "    <frame name='bottom' src='about:blank' />"
+            + "</frameset>"
+            + "</html>";
+
+        final String frameContent =
+            "<html><head><title>TopFrame</title>"
+            + "<script>\n"
+            + "function doTest() {"
+            + "    alert(parent.document.getElementById('fs').rows);"
+            + "    parent.document.getElementById('fs').rows = '*,*';"
+            + "    alert(parent.document.getElementById('fs').rows);"
+            + "}</script>"
+            + "</head>"
+            + "<body onload='doTest()'></body></html>";
+
+        final List collectedAlerts = new ArrayList();
+        final WebClient webClient = new WebClient();
+        webClient.setAlertHandler(new CollectingAlertHandler(collectedAlerts));
+
+        final MockWebConnection webConnection = new MockWebConnection(webClient);
+        webConnection.setResponse(URL_FIRST, framesetContent);
+        webConnection.setResponse(URL_SECOND, frameContent);
+        webClient.setWebConnection(webConnection);
+
+        final List expectedAlerts = Arrays.asList(new String[] { "20%,*", "*,*" });
+        webClient.getPage(URL_FIRST);
+        assertEquals(expectedAlerts, collectedAlerts);
+    }
+
+}
