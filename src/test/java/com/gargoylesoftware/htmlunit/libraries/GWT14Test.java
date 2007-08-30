26db5bda40e3e5c7185f7418bc49a1605cbe8fba
asashour <asashour@yahoo.com>
2007-08-30 21:25:21 +0000

    Implement GWT14Test#testI18N().
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@1840 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/libraries/GWT14Test.java
+++ src/test/java/com/gargoylesoftware/htmlunit/libraries/GWT14Test.java
@@ -39,15 +39,25 @@ package com.gargoylesoftware.htmlunit.libraries;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.CollectingAlertHandler;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.WebTestCase;
+import com.gargoylesoftware.htmlunit.html.DomNode;
 import com.gargoylesoftware.htmlunit.html.DomText;
 import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlDivision;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSelect;
+import com.gargoylesoftware.htmlunit.html.HtmlTableDataCell;
+import com.gargoylesoftware.htmlunit.html.HtmlTableRow;
 
 /**
  * Tests for 1.4 version of <a href="http://code.google.com/webtoolkit">Google Web Toolkit</a>.
@@ -81,6 +91,93 @@ public class GWT14Test extends WebTestCase {
     }
 
     /**
+     * Test I18N default language
+     *
+     * @throws Exception If an error occurs.
+     */
+    public void testI18N() throws Exception {
+        final List collectedAlerts = new ArrayList();
+        final HtmlPage page = loadPage(BrowserVersion.getDefault(), collectedAlerts, "I18N");
+        testI18N(page, "numberFormatOutputText", "31,415,926,535.898");
+        testI18N(page, "dateTimeFormatOutputText", "Monday, September 13, 1999 12:00:00 AM GMT+03:00");
+        testI18N(page, "messagesFormattedOutputText",
+                "User 'amelie' has security clearance 'guest' and cannot access '/secure/blueprints.xml'");
+        testI18N(page, "constantsFirstNameText", "Amelie");
+        testI18N(page, "constantsLastNameText", "Crutcher");
+        testI18N(page, "constantsFavoriteColorList",
+                new String[] {"Red", "White", "Yellow", "Black", "Blue", "Green", "Grey", "Light Grey"});
+        testI18N(page, "constantsWithLookupResultsText", "Red");
+        final Map map = new HashMap();
+        map.put("name", "Amelie Crutcher");
+        map.put("timeZone", "EST");
+        map.put("userID", "123");
+        map.put("lastLogOn", "2/2/2006");
+        testI18NDictionary(page, map);
+    }
+
+    /**
+     * Test value inside {@link HtmlDivision} or {@link HtmlInput}
+     * @param page The page to load.
+     * @param id id of the element to search for.
+     * @param expectedValue Expected value of the value inside the element
+     * @throws Exception If the test fails.
+     */
+    private void testI18N(final HtmlPage page, final String id, final String expectedValue) throws Exception {
+        final HtmlTableDataCell cell = (HtmlTableDataCell) page.getHtmlElementById(id);
+        final Object child = cell.getFirstDomChild();
+        if (child instanceof HtmlDivision) {
+            final HtmlDivision div = (HtmlDivision) child;
+            final DomText text = (DomText) div.getFirstDomChild();
+            assertEquals(expectedValue, text.getData());
+        }
+        else if (child instanceof HtmlInput) {
+            final HtmlInput input = (HtmlInput) child;
+            assertEquals(expectedValue, input.getValueAttribute());
+        }
+    }
+
+    /**
+     * Test value of {@link HtmlSelect}
+     *
+     * @param page The page to load.
+     * @param id id of the element to search for.
+     * @param expectedValues Expected value of the value inside the select.
+     * @throws Exception If the test fails.
+     */
+    private void testI18N(final HtmlPage page, final String id, final String[] expectedValues) {
+        final HtmlTableDataCell cell = (HtmlTableDataCell) page.getHtmlElementById(id);
+        final Object child = cell.getFirstDomChild();
+        if (child instanceof HtmlSelect) {
+            final HtmlSelect select = (HtmlSelect) child;
+            assertEquals(expectedValues.length, select.getOptionSize());
+            for (int i = 0; i < expectedValues.length; i++) {
+                assertEquals(expectedValues[i], select.getOption(i).getFirstDomChild().getNodeValue());
+            }
+        }
+    }
+
+    private void testI18NDictionary(final HtmlPage page, final Map expectedMap) throws Exception {
+        final HtmlTableRow headerRow =
+            (HtmlTableRow) page.getByXPath("//*[@class='i18n-dictionary-header-row']").get(0);
+        final HtmlTableRow valueRow = (HtmlTableRow) headerRow.getNextDomSibling();
+        DomNode headerNode = headerRow.getFirstDomChild();
+        DomNode valueNode = valueRow.getFirstDomChild();
+        final Set foundHeaders = new HashSet();
+        for (int i = 0; i < expectedMap.size(); i++) {
+            final String header = headerNode.getFirstDomChild().getNodeValue();
+            final String value = valueNode.getFirstDomChild().getNodeValue();
+            
+            assertNotNull(expectedMap.get(header));
+            assertEquals(expectedMap.get(header), value);
+            foundHeaders.add(header);
+            
+            valueNode = valueNode.getNextDomSibling();
+            headerNode = headerNode.getNextDomSibling();
+        }
+        assertEquals(expectedMap.size(), foundHeaders.size());
+    }
+
+    /**
      * Returns the GWT directory being tested.
      * @return the GWT directory being tested.
      */
@@ -107,7 +204,7 @@ public class GWT14Test extends WebTestCase {
         client.setAlertHandler(new CollectingAlertHandler(collectedAlerts));
 
         final HtmlPage page = (HtmlPage) client.getPage(url);
-        page.getEnclosingWindow().getThreadManager().joinAll(1000);
+        page.getEnclosingWindow().getThreadManager().joinAll(10000);
         return page;
     }
 
