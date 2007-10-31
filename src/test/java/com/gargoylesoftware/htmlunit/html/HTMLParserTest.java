f05c8129ae61fcec3479f17807e6a3714ef06965
asashour <asashour@yahoo.com>
2007-10-31 17:11:03 +0000

    1) Move Tests from com.gargoylesoftware.htmlunit.HTMLParserTest to com.gargoylesoftware.htmlunit.html.HTMLParserTest 2) Delete the former.
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@1988 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/html/HTMLParserTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/html/HTMLParserTest.java
@@ -37,12 +37,20 @@
  */
 package com.gargoylesoftware.htmlunit.html;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.gargoylesoftware.htmlunit.StringWebResponse;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebResponse;
 import com.gargoylesoftware.htmlunit.WebTestCase;
+import com.gargoylesoftware.htmlunit.html.xpath.HtmlUnitXPath;
 
 /**
  * Test class for {@link HTMLParser}.
  *
  * @version $Revision$
+ * @author <a href="mailto:cse@dynabean.de">Christian Sell</a>
  * @author Marc Guillemot
  */
 public class HTMLParserTest extends WebTestCase {
@@ -53,6 +61,113 @@ public class HTMLParserTest extends WebTestCase {
      */
     public HTMLParserTest(final String name) {
         super(name);
+    }
+
+    /**
+     * test the new HTMLParser on a simple HTML string and use the Jaxen XPath navigator
+     * to validate results
+     * @throws Exception failure
+     */
+    public void testSimpleHTMLString() throws Exception {
+        final WebClient webClient = new WebClient();
+        final WebResponse webResponse = new StringWebResponse(
+            "<html><head><title>TITLE</title><noscript>TEST</noscript></head><body></body></html>");
+
+        final HtmlPage page = HTMLParser.parse(webResponse, webClient.getCurrentWindow());
+
+        HtmlUnitXPath xpath = new HtmlUnitXPath("//noscript");
+        final String stringVal = xpath.stringValueOf(page);
+
+        assertEquals("TEST", stringVal);
+
+        xpath = new HtmlUnitXPath("//*[./text() = 'TEST']");
+        final HtmlElement node = (HtmlElement) xpath.selectSingleNode(page);
+
+        assertEquals(node.getTagName(), HtmlNoScript.TAG_NAME);
+    }
+
+    /**
+     * Test when <form> inside <table> and before <tr>
+     * @throws Exception failure
+     */
+    public void testBadlyFormedHTML() throws Exception {
+        final String content
+            = "<html><head><title>first</title>\n"
+            + "<script>\n"
+            + "function test()"
+            + "{"
+            + "  alert(document.getElementById('myInput').form.id);\n"
+            + "}"
+            + "</script>\n"
+            + "</head>\n"
+            + "<body onload='test()'>\n"
+            + "<table>\n"
+            + "<form name='myForm' action='foo' id='myForm'>\n"
+            + "<tr><td>\n"
+            + "<input type='text' name='myInput' id='myInput'/>\n"
+            + "</td></tr>\n"
+            + "</form>\n"
+            + "</table>\n"
+            + "</body></html>";
+
+        final List collectedAlerts = new ArrayList();
+        final String[] expectedAlerts = {"myForm"};
+        createTestPageForRealBrowserIfNeeded(content, expectedAlerts);
+
+        loadPage(content, collectedAlerts);
+
+        assertEquals(expectedAlerts, collectedAlerts);
+    }
+
+    /**
+     * Test when an illegal tag is found in head as some websites do
+     * @throws Exception failure
+     */
+    public void testUnknownTagInHead() throws Exception {
+        if (notYetImplemented()) {
+            return;
+        }
+
+        // Note: the <meta> tag in this test is quite important because
+        // I could adapt the TagBalancer to make it work except with this <meta http-equiv...
+        // (it worked with <meta name=...)
+        final String content
+            = "<html><head><mainA3>\n"
+            + "<meta http-equiv='Content-Type' content='text/html; charset=ISO-8859-1'>\n"
+            + "<title>first</title>\n"
+            + "<script>\n"
+            + "function test()"
+            + "{"
+            + "  alert(document.title);\n"
+            + "}"
+            + "</script>\n"
+            + "</head>\n"
+            + "<body onload='test()'>\n"
+            + "</body></html>";
+
+        final List collectedAlerts = new ArrayList();
+        final String[] expectedAlerts = {"first"};
+        createTestPageForRealBrowserIfNeeded(content, expectedAlerts);
+
+        loadPage(content, collectedAlerts);
+
+        assertEquals(expectedAlerts, collectedAlerts);
+    }
+
+    /**
+     * Test the HTMLParser by accessing the HtmlUnit home page and detecting the copyright
+     * string.
+     *
+     * @throws Exception failure
+     */
+    public void testHtmlUnitHomePage() throws Exception {
+        final HtmlPage page = loadUrl("http://htmlunit.sourceforge.net");
+        if (page != null) {
+            // No connectivity issues.
+            final HtmlUnitXPath xpath = new HtmlUnitXPath("//div[@id='footer']/div[@class='xright']");
+            final String stringVal = xpath.stringValueOf(page).trim();
+            assertEquals("\u00A9 2002-2007, Gargoyle Software Inc.", stringVal);
+        }
     }
 
     /**
