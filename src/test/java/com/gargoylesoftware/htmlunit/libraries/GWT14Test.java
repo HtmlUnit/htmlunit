080d64c4cc4905ec5ed18f969e8b0b9c8934f2b9
asashour <asashour@yahoo.com>
2007-11-01 11:33:21 +0000

    Chechstyle fix: m2 now use checkstyle.
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@2010 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/libraries/GWT14Test.java
+++ src/test/java/com/gargoylesoftware/htmlunit/libraries/GWT14Test.java
@@ -160,7 +160,8 @@ public class GWT14Test extends WebTestCase {
 
         testI18N(page, "dateTimeFormatOutputText", "lundi 13 septembre 1999 00 h 00 GMT" + timeZone);
         testI18N(page, "messagesFormattedOutputText",
-            "L'utilisateur 'amelie' a un niveau de securit\u00E9 'guest', et ne peut acc\u00E9der \u00E0 '/secure/blueprints.xml'");
+            "L'utilisateur 'amelie' a un niveau de securit\u00E9 'guest', "
+            + "et ne peut acc\u00E9der \u00E0 '/secure/blueprints.xml'");
         testI18N(page, "constantsFirstNameText", "Amelie");
         testI18N(page, "constantsLastNameText", "Crutcher");
         testI18N(page, "constantsFavoriteColorList",
