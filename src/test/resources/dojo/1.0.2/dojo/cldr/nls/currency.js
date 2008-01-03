719aad5b36cb2b450d7705ed4ebafcb861e7d4a4
asashour <asashour@yahoo.com>
2008-01-03 08:45:38 +0000

    Adding Dojo102Test, as notYetImplemented().
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@2138 5f5364db-9458-4db8-a492-e30667be6df6

---
+++ src/test/resources/dojo/1.0.2/dojo/cldr/nls/currency.js
@@ -0,0 +1,1 @@
+({"USD_symbol": "$", "EUR_displayName": "EUR", "GBP_displayName": "GBP", "JPY_displayName": "JPY", "GBP_symbol": "£", "JPY_symbol": "¥", "EUR_symbol": "€", "USD_displayName": "USD"})
