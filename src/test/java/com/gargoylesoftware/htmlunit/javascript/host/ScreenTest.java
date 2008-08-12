d4b7e4e4e3b142268653b56518ee60d71ffb8525
asashour <asashour@yahoo.com>
2008-08-12 21:23:47 +0000

    Update MSDN references
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@3260 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/ScreenTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/ScreenTest.java
@@ -27,8 +27,7 @@ import com.gargoylesoftware.htmlunit.WebTestCase;
  * @version $Revision$
  * @author Daniel Gredler
  * @author Marc Guillemot
- * @see <a href="http://msdn.microsoft.com/workshop/author/dhtml/reference/objects/obj_screen.asp">
- * MSDN documentation</a>
+ * @see <a href="http://msdn.microsoft.com/en-us/library/ms535868.aspx">MSDN documentation</a>
  * @see <a href="http://www.mozilla.org/docs/dom/domref/dom_window_ref.html">Mozilla documentation</a>
  */
 public class ScreenTest extends WebTestCase {
