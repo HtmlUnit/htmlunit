c7e3c3f54697e8282c4116093eabbe58361db855
asashour <asashour@yahoo.com>
2008-12-17 06:05:59 +0000

    Add EventNodeTest.fireEvent() as @NotYetImplemented Issue 2421302
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@3941 5f5364db-9458-4db8-a492-e30667be6df6

---
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/EventNodeTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2002-2008 Gargoyle Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.gargoylesoftware.htmlunit.javascript.host;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.gargoylesoftware.htmlunit.BrowserRunner;
+import com.gargoylesoftware.htmlunit.WebTestCase;
+import com.gargoylesoftware.htmlunit.BrowserRunner.Alerts;
+import com.gargoylesoftware.htmlunit.BrowserRunner.Browser;
+import com.gargoylesoftware.htmlunit.BrowserRunner.Browsers;
+import com.gargoylesoftware.htmlunit.BrowserRunner.NotYetImplemented;
+
+/**
+ * Tests for {@link EventNode}.
+ *
+ * @version $Revision$
+ * @author Ahmed Ashour
+ */
+@RunWith(BrowserRunner.class)
+public class EventNodeTest extends WebTestCase {
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Browsers(Browser.IE)
+    @NotYetImplemented
+    @Alerts("true")
+    public void fireEvent() throws Exception {
+        final String html
+            = "<html><head>\n"
+            + "<script>\n"
+            + "  function test() {\n"
+            + "    var form = document.getElementById('myForm');\n"
+            + "    alert(form.fireEvent('onsubmit'));\n"
+            + "  }\n"
+            + "</script>\n"
+            + "</head><body>\n"
+            + "  <form id='myForm'>\n"
+            + "    <div id='myDiv' onclick='test()'>\n"
+            + "      Hello There\n"
+            + "    </div>\n"
+            + "  </form>\n"
+            + "</body></html>";
+        loadPageWithAlerts(html);
+    }
+
+}
