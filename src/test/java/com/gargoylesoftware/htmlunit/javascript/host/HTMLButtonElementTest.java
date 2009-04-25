9219691799d7f34dbe78c34d0d34d83d02168d93
gredler <daniel.gredler@gmail.com>
2009-04-25 03:13:21 +0000

    JavaScript: add support for button.accessKey (Firefox only; IE already has this property on HtmlElement).
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@4432 5f5364db-9458-4db8-a492-e30667be6df6

---
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/HTMLButtonElementTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2002-2009 Gargoyle Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.gargoylesoftware.htmlunit.javascript.host;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.gargoylesoftware.htmlunit.BrowserRunner;
+import com.gargoylesoftware.htmlunit.WebTestCase;
+import com.gargoylesoftware.htmlunit.BrowserRunner.Alerts;
+
+/**
+ * Tests for {@link HTMLButtonElement}.
+ *
+ * @version $Revision$
+ * @author Daniel Gredler
+ */
+@RunWith(BrowserRunner.class)
+public class HTMLButtonElementTest extends WebTestCase {
+
+    /**
+     * @throws Exception if an error occurs
+     */
+    @Test
+    @Alerts({ "", "A", "a", "A", "a8", "8Afoo", "8", "@" })
+    public void readWriteAccessKey() throws Exception {
+        final String html
+            = "<html><body><button id='a1'>a1</button><button id='a2' accesskey='A'>a2</button><script>\n"
+            + "var a1 = document.getElementById('a1'), a2 = document.getElementById('a2');\n"
+            + "alert(a1.accessKey);\n"
+            + "alert(a2.accessKey);\n"
+            + "a1.accessKey = 'a';\n"
+            + "a2.accessKey = 'A';\n"
+            + "alert(a1.accessKey);\n"
+            + "alert(a2.accessKey);\n"
+            + "a1.accessKey = 'a8';\n"
+            + "a2.accessKey = '8Afoo';\n"
+            + "alert(a1.accessKey);\n"
+            + "alert(a2.accessKey);\n"
+            + "a1.accessKey = '8';\n"
+            + "a2.accessKey = '@';\n"
+            + "alert(a1.accessKey);\n"
+            + "alert(a2.accessKey);\n"
+            + "</script></body></html>";
+        loadPageWithAlerts(html);
+    }
+
+}
