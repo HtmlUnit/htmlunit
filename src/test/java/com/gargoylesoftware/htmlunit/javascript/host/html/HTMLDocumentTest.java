23389b1876b11712effd5e6c386b196c937c7696
mguillem <mguillemot@yahoo.fr>
2009-06-23 08:37:33 +0000

    rework test to capture exception in JS and be able to better test in real browser
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@4612 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLDocumentTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLDocumentTest.java
@@ -352,20 +352,24 @@ public class HTMLDocumentTest extends WebTestCase {
      * @throws Exception if an error occurs
      */
     @Test
-    @Browsers(Browser.IE)
-    @Alerts({ "0", "1", "f", "f", "urn:f", "urn:f", "true" })
+    @Alerts(FF = { "undefined", "exception" },
+            IE = { "[object]", "0", "1", "f", "f", "urn:f", "urn:f", "true" })
     public void namespaces() throws Exception {
         final String html =
               "<body><script>\n"
             + "var ns = document.namespaces;\n"
-            + "alert(ns.length);\n"
-            + "ns.add('f', 'urn:f');\n"
-            + "alert(ns.length);\n"
-            + "alert(ns.item(0).name);\n"
-            + "alert(ns[0].name);\n"
-            + "alert(ns.item('f').urn);\n"
-            + "alert(ns['f'].urn);\n"
-            + "alert(ns == document.namespaces);\n"
+            + "alert(ns);\n"
+            + "try {\n"
+            + "  alert(ns.length);\n"
+            + "  ns.add('f', 'urn:f');\n"
+            + "  alert(ns.length);\n"
+            + "  alert(ns.item(0).name);\n"
+            + "  alert(ns[0].name);\n"
+            + "  alert(ns.item('f').urn);\n"
+            + "  alert(ns['f'].urn);\n"
+            + "  alert(ns == document.namespaces);\n"
+            + "}\n"
+            + "catch(e) { alert('exception') }\n"
             + "</script></body>";
         loadPageWithAlerts(html);
     }
