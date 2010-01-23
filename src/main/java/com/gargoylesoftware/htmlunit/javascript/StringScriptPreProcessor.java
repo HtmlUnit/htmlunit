5d2b9346e6f28f8ec106f5b748708a3e0113982d
asashour <asashour@yahoo.com>
2010-01-23 22:16:50 +0000

    JavaScript: StringScriptPreProcessor to use Rhino AST. Issue 2924097
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@5406 5f5364db-9458-4db8-a492-e30667be6df6

--- src/main/java/com/gargoylesoftware/htmlunit/javascript/StringScriptPreProcessor.java
+++ src/main/java/com/gargoylesoftware/htmlunit/javascript/StringScriptPreProcessor.java
@@ -14,15 +14,21 @@
  */
 package com.gargoylesoftware.htmlunit.javascript;
 
+import java.util.Map;
+import java.util.TreeMap;
+
+import net.sourceforge.htmlunit.corejs.javascript.Parser;
+import net.sourceforge.htmlunit.corejs.javascript.ast.AstNode;
+import net.sourceforge.htmlunit.corejs.javascript.ast.NodeVisitor;
+import net.sourceforge.htmlunit.corejs.javascript.ast.StringLiteral;
+
 import com.gargoylesoftware.htmlunit.ScriptPreProcessor;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 /**
  * Process occurrences of \xDD in string literals.
- * <p>This is done by Rhino, but it is needed sometimes before using other PreProcessors.
- * <p>The current implementation uses primitive string search of the source code, not parsing it,
- * which may result in improper replacements.
+ * <p>This is automatically done by Rhino, but it is sometimes needed before using other PreProcessors.
  *
  * @version $Revision$
  * @author Ahmed Ashour
@@ -34,99 +40,57 @@ public class StringScriptPreProcessor implements ScriptPreProcessor {
     /**
      * {@inheritDoc}
      */
-    public String preProcess(final HtmlPage htmlPage, final String sourceCode, final String sourceName,
+    public String preProcess(final HtmlPage htmlPage, String sourceCode, final String sourceName,
                 final HtmlElement htmlElement) {
 
-        // parsing logic should be synchronized with #indexOf()
+        final AstNode root = new Parser().parse(sourceCode, sourceName, 0);
+        final Map<Integer, Integer> strings = new TreeMap<Integer, Integer>();
+        root.visit(new NodeVisitor() {
+
+            public boolean visit(final AstNode node) {
+                if (node instanceof StringLiteral) {
+                    strings.put(node.getAbsolutePosition() + 1, node.getLength() - 2);
+                }
+                return true;
+            }
+        });
+
+        int variant = 0;
+        for (int index : strings.keySet()) {
+            final int length = strings.get(index);
+            final String string = sourceCode.substring(index + variant, index +  variant + length);
+            final String newString = replace(string);
+            if (newString != null) {
+                sourceCode = sourceCode.substring(0, index + variant)
+                    + newString + sourceCode.substring(index + variant + length);
+                variant = newString.length() - string.length();
+            }
+        }
+        return sourceCode;
+    }
+
+    /**
+     * Replaces \xDD with the corresponding character.
+     */
+    private String replace(final String string) {
+        if (string.indexOf("\\x") == -1) {
+            return null;
+        }
         final StringBuilder sb = new StringBuilder();
-        PARSING_STATUS parsingStatus = PARSING_STATUS.NORMAL;
-        char stringChar = 0;
-        for (int i = 0; i < sourceCode.length(); i++) {
-            final char ch = sourceCode.charAt(i);
-            switch (ch) {
-                case '/':
-                    if (parsingStatus == PARSING_STATUS.NORMAL && i + 1 < sourceCode.length()) {
-                        final char nextCh = sourceCode.charAt(i + 1);
-                        if (nextCh == '/') {
-                            parsingStatus = PARSING_STATUS.IN_SINGLE_LINE_COMMENT;
-                        }
-                        else if (nextCh == '*') {
-                            parsingStatus = PARSING_STATUS.IN_MULTI_LINE_COMMENT;
-                        }
-                        else {
-                            boolean regExp = true;
-                            for (int x  = i - 1; x >= 0; x--) {
-                                final char c = sourceCode.charAt(x);
-                                if (!Character.isWhitespace(c)) {
-                                    if (Character.isLetterOrDigit(c) || c == ')') {
-                                        //division operator, not Regular Expression
-                                        regExp = false;
-                                    }
-                                    break;
-                                }
-                            }
-                            if (regExp) {
-                                stringChar = ch;
-                                parsingStatus = PARSING_STATUS.IN_REG_EXP;
-                            }
-                        }
+        for (int i = 0; i < string.length(); i++) {
+            final char ch = string.charAt(i);
+            if (ch == '\\' && i + 3 < string.length() && string.charAt(i + 1) == 'x') {
+                final char ch1 = Character.toUpperCase(string.charAt(i + 2));
+                final char ch2 = Character.toUpperCase(string.charAt(i + 3));
+                if ((ch1 >= '0' && ch1 <= '9' || ch1 >= 'A' && ch1 <= 'F')
+                        && (ch2 >= '0' && ch2 <= '9' || ch2 >= 'A' && ch2 <= 'F')) {
+                    final char character = (char) Integer.parseInt(string.substring(i + 2, i + 4), 16);
+                    if (character >= ' ') {
+                        sb.append(character);
+                        i += 3;
+                        continue;
                     }
-                    else if (parsingStatus == PARSING_STATUS.IN_REG_EXP && ch == stringChar) {
-                        stringChar = 0;
-                        parsingStatus = PARSING_STATUS.NORMAL;
-                    }
-                    break;
-
-                case '*':
-                    if (parsingStatus == PARSING_STATUS.IN_MULTI_LINE_COMMENT && i + 1 < sourceCode.length()) {
-                        final char nextCh = sourceCode.charAt(i + 1);
-                        if (nextCh == '/') {
-                            parsingStatus = PARSING_STATUS.NORMAL;
-                        }
-                    }
-                    break;
-
-                case '\n':
-                    if (parsingStatus == PARSING_STATUS.IN_SINGLE_LINE_COMMENT) {
-                        parsingStatus = PARSING_STATUS.NORMAL;
-                    }
-                    break;
-
-                case '\'':
-                case '"':
-                    if (parsingStatus == PARSING_STATUS.NORMAL) {
-                        stringChar = ch;
-                        parsingStatus = PARSING_STATUS.IN_STRING;
-                    }
-                    else if (parsingStatus == PARSING_STATUS.IN_STRING && ch == stringChar) {
-                        stringChar = 0;
-                        parsingStatus = PARSING_STATUS.NORMAL;
-                    }
-                    break;
-
-                case '\\':
-                    if (parsingStatus == PARSING_STATUS.IN_STRING) {
-                        if (i + 3 < sourceCode.length() && sourceCode.charAt(i + 1) == 'x') {
-                            final char ch1 = Character.toUpperCase(sourceCode.charAt(i + 2));
-                            final char ch2 = Character.toUpperCase(sourceCode.charAt(i + 3));
-                            if ((ch1 >= '0' && ch1 <= '9' || ch1 >= 'A' && ch1 <= 'F')
-                                    && (ch2 >= '0' && ch2 <= '9' || ch2 >= 'A' && ch2 <= 'F')) {
-                                final char character = (char) Integer.parseInt(sourceCode.substring(i + 2, i + 4), 16);
-                                if (character >= ' ') {
-                                    sb.append(character);
-                                    i += 3;
-                                    continue;
-                                }
-                            }
-                        }
-                        else if (i + 1 < sourceCode.length()) {
-                            sb.append(ch).append(sourceCode.charAt(i + 1));
-                            i++;
-                            continue;
-                        }
-                    }
-
-                default:
+                }
             }
             sb.append(ch);
         }
