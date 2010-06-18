c13622cbd8d9f3141779fcb38f45cff4c29dd9c2
asashour <asashour@yahoo.com>
2010-06-18 14:54:44 +0000

    JavaScript: fix event.button of click events with IE. Issue 3017828
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@5782 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/MouseEventTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/MouseEventTest.java
@@ -20,6 +20,8 @@ import java.net.URL;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 import com.gargoylesoftware.htmlunit.BrowserRunner;
 import com.gargoylesoftware.htmlunit.WebClient;
@@ -34,6 +36,7 @@ import com.gargoylesoftware.htmlunit.html.HtmlTextArea;
  *
  * @version $Revision$
  * @author Marc Guillemot
+ * @author Ahmed Ashour
  */
 @RunWith(BrowserRunner.class)
 public class MouseEventTest extends WebDriverTestCase {
@@ -42,7 +45,7 @@ public class MouseEventTest extends WebDriverTestCase {
      * @throws Exception if the test fails
      */
     @Test
-    public void testEventCoordinates() throws Exception {
+    public void eventCoordinates() throws Exception {
         final URL url = getClass().getClassLoader().getResource("event_coordinates.html");
         assertNotNull(url);
 
@@ -75,7 +78,7 @@ public class MouseEventTest extends WebDriverTestCase {
     @Test
     @Alerts(FF = { "click", "true", "true", "true", "1", "2", "3", "4", "true", "true", "true", "true" },
             IE = "exception")
-    public void testInitMouseEvent() throws Exception {
+    public void initMouseEvent() throws Exception {
         final String html = "<html><body><script>\n"
             + "try {\n"
             + "  var e = document.createEvent('MouseEvents');\n"
@@ -98,4 +101,103 @@ public class MouseEventTest extends WebDriverTestCase {
         loadPageWithAlerts2(html);
     }
 
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts({ "1", "1" })
+    public void dispatchEvent() throws Exception {
+        final String html = "<html><head><title>foo</title><script>\n"
+            + "  var clickCount = 0;\n"
+            + "  var dblClickCount = 0;\n"
+            + "  function test() {\n"
+            + "    var div = document.getElementById('myDiv');\n"
+            + "    div.onclick = clickHandler;\n"
+            + "    div.ondblclick = dblClickHandler;\n"
+            + "    if (div.fireEvent) {\n"
+            + "      var clickEvent = (evt = document.createEventObject(), evt.type = 'click', evt);\n"
+            + "      div.fireEvent('onclick', clickEvent);\n"
+            + "      var dblclickEvent = (evt_0 = document.createEventObject(), evt_0.type = 'dblclick', evt_0);\n"
+            + "      div.fireEvent('ondblclick', dblclickEvent);\n"
+            + "    }\n"
+            + "    else {\n"
+            + "      var clickEvent = $createMouseEvent(document, 'click', true, true, 0, 0, 0, 0, 0,"
+            + " false, false, false, false, 1, null);\n"
+            + "      div.dispatchEvent(clickEvent);\n"
+            + "      var dblclickEvent = $createMouseEvent(document, 'dblclick', true, true, 0, 0, 0, 0, 0,"
+            + " false, false, false, false, 1, null);\n"
+            + "      div.dispatchEvent(dblclickEvent);\n"
+            + "    }\n"
+            + "    alert(clickCount);\n"
+            + "    alert(dblClickCount);\n"
+            + "  }\n"
+            + "  function clickHandler() {\n"
+            + "    clickCount++;\n"
+            + "  }\n"
+            + "  function dblClickHandler() {\n"
+            + "    dblClickCount++;\n"
+            + "  }\n"
+            + "  function $createMouseEvent(doc, type, canBubble, cancelable, detail, screenX, screenY, clientX,"
+            + " clientY, ctrlKey, altKey, shiftKey, metaKey, button, relatedTarget) {\n"
+            + "    button == 1?(button = 0):button == 4?(button = 1):(button = 2);\n"
+            + "    var evt = doc.createEvent('MouseEvents');\n"
+            + "    evt.initMouseEvent(type, canBubble, cancelable, null, detail, screenX, screenY, clientX,"
+            + " clientY, ctrlKey, altKey, shiftKey, metaKey, button, relatedTarget);\n"
+            + "    return evt;\n"
+            + "  }\n"
+            + "</script></head><body onload='test()'>\n"
+            + "  <div id='myDiv'></div>\n"
+            + "</body></html>";
+        loadPageWithAlerts2(html);
+    }
+
+    /**
+     * @throws Exception if an error occurs
+     */
+    @Test
+    @Alerts("0")
+    public void button_onclick() throws Exception {
+        final String html = "<html><body>\n"
+            + "<p id='clicker'>Click me!</p>\n"
+            + "<script>\n"
+            + "  function handler(event) {\n"
+            + "    alert(event.button);\n"
+            + "  }\n"
+            + "  var p = document.getElementById('clicker');\n"
+            + "  if (p.addEventListener ) {\n"
+            + "    p.addEventListener('click', handler, false);\n"
+            + "  } else if (p.attachEvent) {\n"
+            + "    p.attachEvent('onclick', handler);\n"
+            + "  }\n"
+            + "</script></body></html>";
+
+        final WebDriver driver = loadPage2(html);
+        driver.findElement(By.id("clicker")).click();
+        assertEquals(getExpectedAlerts(), getCollectedAlerts(driver));
+    }
+
+    /**
+     * @throws Exception if an error occurs
+     */
+    @Test
+    @Alerts(IE = "1", FF = "0")
+    public void button_onmousedown() throws Exception {
+        final String html = "<html><body>\n"
+            + "<p id='clicker'>Click me!</p>\n"
+            + "<script>\n"
+            + "  function handler(event) {\n"
+            + "    alert(event.button);\n"
+            + "  }\n"
+            + "  var p = document.getElementById('clicker');\n"
+            + "  if (p.addEventListener ) {\n"
+            + "    p.addEventListener('mousedown', handler, false);\n"
+            + "  } else if (p.attachEvent) {\n"
+            + "    p.attachEvent('onmousedown', handler);\n"
+            + "  }\n"
+            + "</script></body></html>";
+
+        final WebDriver driver = loadPage2(html);
+        driver.findElement(By.id("clicker")).click();
+        assertEquals(getExpectedAlerts(), getCollectedAlerts(driver));
+    }
 }
