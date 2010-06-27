3b258ba2eccdf1cfa5c400703e6bccd309e2877f
asashour <asashour@yahoo.com>
2010-06-27 07:43:34 +0000

    Add HTMLOpenElement2Test.click2() case for bug #3019326
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@5800 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLOptionElement2Test.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLOptionElement2Test.java
@@ -67,4 +67,37 @@ public class HTMLOptionElement2Test extends WebDriverTestCase {
         driver.findElement(By.id("s")).click();
         assertEquals(getExpectedAlerts(), getCollectedAlerts(driver));
     }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts("b")
+    public void click2() throws Exception {
+        final String html
+            = "<html><head><title>foo</title><script>\n"
+            + "  function init() {\n"
+            + "    var s = document.getElementById('s');\n"
+            + "    if (s.addEventListener) {\n"
+            + "      s.addEventListener('click', handle, false);\n"
+            + "    } else if (s.attachEvent) {\n"
+            + "      s.attachEvent('onclick', handle);\n"
+            + "    }\n"
+            + "  }\n"
+            + "  function handle(event) {\n"
+            + "    alert(s.options[s.selectedIndex].value);\n"
+            + "  }\n"
+            + "</script></head><body onload='init()'>\n"
+            + "  <select id='s'>\n"
+            + "    <option value='a'>A</option>\n"
+            + "    <option id='opb' value='b'>B</option>\n"
+            + "    <option value='c'>C</option>\n"
+            + "  </select>\n"
+            + "</body></html>";
+
+        final WebDriver driver = loadPage2(html);
+        driver.findElement(By.id("opb")).setSelected();
+        driver.findElement(By.id("s")).click();
+        assertEquals(getExpectedAlerts(), getCollectedAlerts(driver));
+    }
 }
