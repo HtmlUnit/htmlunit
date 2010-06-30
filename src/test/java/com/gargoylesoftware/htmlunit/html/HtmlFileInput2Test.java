48640bf55114deb24a16a9ec9761e97db83a836d
asashour <asashour@yahoo.com>
2010-06-30 11:10:00 +0000

    Add HtmlFileInput2Test.contentType() as @NYI
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@5818 5f5364db-9458-4db8-a492-e30667be6df6

---
+++ src/test/java/com/gargoylesoftware/htmlunit/html/HtmlFileInput2Test.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (c) 2002-2010 Gargoyle Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.gargoylesoftware.htmlunit.html;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadBase;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import com.gargoylesoftware.htmlunit.BrowserRunner;
+import com.gargoylesoftware.htmlunit.WebDriverTestCase;
+import com.gargoylesoftware.htmlunit.BrowserRunner.NotYetImplemented;
+
+/**
+ * Tests for {@link HtmlFileInput}.
+ *
+ * @version $Revision$
+ * @author Ahmed Ashour
+ */
+@RunWith(BrowserRunner.class)
+public class HtmlFileInput2Test extends WebDriverTestCase {
+
+    /**
+     * @throws Exception if an error occurs
+     */
+    @Test
+    @NotYetImplemented
+    public void contentType() throws Exception {
+        final Map<String, Class<? extends Servlet>> servlets = new HashMap<String, Class<? extends Servlet>>();
+        servlets.put("/upload1", Upload1Servlet.class);
+        servlets.put("/upload2", Upload2Servlet.class);
+        startWebServer("./", new String[0], servlets);
+
+        final WebDriver driver = getWebDriver();
+        driver.get("http://localhost:" + PORT + "/upload1");
+        String path = getClass().getClassLoader().getResource("realm.properties").toExternalForm();
+        if (driver instanceof InternetExplorerDriver) {
+            path = path.substring(path.indexOf('/') + 1).replace('/', '\\');
+        }
+        driver.findElement(By.name("myInput")).sendKeys(path);
+        driver.findElement(By.id("mySubmit")).click();
+        assertTrue(driver.getPageSource().contains("CONTENT_TYPE:application/octet-stream"));
+    }
+
+    /**
+     * Servlet for '/upload1'.
+     */
+    public static class Upload1Servlet extends HttpServlet {
+
+        private static final long serialVersionUID = 6693252829875297263L;
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("text/html");
+            response.getWriter().write("<html>"
+                + "<body><form action='upload2' method='post' enctype='multipart/form-data'>\n"
+                + "Name: <input name='myInput' type='file'><br>\n"
+                + "Name 2 (should stay empty): <input name='myInput2' type='file'><br>\n"
+                + "<input type='submit' value='Upload' id='mySubmit'>\n"
+                + "</form></body></html>\n");
+        }
+    }
+
+    /**
+     * Servlet for '/upload2'.
+     */
+    public static class Upload2Servlet extends HttpServlet {
+
+        private static final long serialVersionUID = -1350878755076138012L;
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        @SuppressWarnings("unchecked")
+        protected void doPost(final HttpServletRequest request, final HttpServletResponse response)
+            throws ServletException, IOException {
+            request.setCharacterEncoding("UTF-8");
+            response.setContentType("text/html");
+            final Writer writer = response.getWriter();
+            if (ServletFileUpload.isMultipartContent(request)) {
+                try {
+                    final ServletFileUpload upload = new ServletFileUpload(new DiskFileItemFactory());
+                    for (final FileItem item : (List<FileItem>) upload.parseRequest(request)) {
+                        if ("myInput".equals(item.getFieldName())) {
+                            writer.write("CONTENT_TYPE:" + item.getContentType());
+                        }
+                    }
+                }
+                catch (final FileUploadBase.SizeLimitExceededException e) {
+                    writer.write("SizeLimitExceeded");
+                }
+                catch (final Exception e) {
+                    writer.write("error");
+                }
+            }
+            writer.close();
+        }
+    }
+}
