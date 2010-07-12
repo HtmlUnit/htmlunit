56f2fbe2f1387a164c5f398d6009a46a4e437016
asashour <asashour@yahoo.com>
2010-07-12 21:19:56 +0000

    Increase the time to wait, as it seems to still block the CI server
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@5874 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/UrlFetchWebConnectionTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/UrlFetchWebConnectionTest.java
@@ -81,11 +81,11 @@ public class UrlFetchWebConnectionTest extends WebDriverTestCase {
         final WebRequest referenceRequest = getPostRequest(FormEncodingType.URL_ENCODED);
 
         //TODO: mostly Jetty 7.1.5 issue
-        Thread.sleep(5000);
+        Thread.sleep(10000);
         getWebClient().setWebConnection(new UrlFetchWebConnection(getWebClient()));
         final WebRequest newRequest = getPostRequest(FormEncodingType.URL_ENCODED);
 
-        Thread.sleep(5000);
+        Thread.sleep(10000);
         // compare the two requests
         compareRequests(referenceRequest, newRequest);
     }
