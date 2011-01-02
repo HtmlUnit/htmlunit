9a834df5cbf8a8505f8ff5151cf919cdd03296bf
rbri <rbri@rbri.de>
2011-01-02 15:46:12 +0000

    Happy New Year!
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@6204 5f5364db-9458-4db8-a492-e30667be6df6

--- src/main/java/com/gargoylesoftware/htmlunit/Version.java
+++ src/main/java/com/gargoylesoftware/htmlunit/Version.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002-2010 Gargoyle Software Inc.
+ * Copyright (c) 2002-2011 Gargoyle Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -78,6 +78,6 @@ public final class Version {
      * @return the copyright notice
      */
     public static String getCopyright() {
-        return "Copyright (c) 2002-2010 Gargoyle Software Inc. All rights reserved.";
+        return "Copyright (c) 2002-2011 Gargoyle Software Inc. All rights reserved.";
     }
 }
