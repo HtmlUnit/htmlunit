0d8bbae485592a9c5219d6754f3c36102294b5ee
mguillem <mguillemot@yahoo.fr>
2011-09-27 10:55:03 +0000

    Add NiceRefreshHandler.
    Issue 3026460
    
    
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@6565 5f5364db-9458-4db8-a492-e30667be6df6

---
+++ src/main/java/com/gargoylesoftware/htmlunit/NiceRefreshHandler.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2002-2011 Gargoyle Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.gargoylesoftware.htmlunit;
+
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * This refresh handler performs an immediate refresh if the refresh delay is
+ * less or equal to the configured time and otherwise ignores totally the the refresh instruction.
+ *
+ * @version $Revision$
+ * @author Marc Guillemot
+ */
+public class NiceRefreshHandler extends ImmediateRefreshHandler {
+    private final int maxDelay_;
+
+    /**
+     * Creates a new refresh handler that will immediately refresh if the refresh delay is no
+     * longer than <tt>maxDelay</tt>. No refresh will be perform at all for refresh values
+     * larger than <tt>maxDelay</tt>.
+     *
+     * @param maxDelay the maximum refreshValue (in seconds) that should cause a refresh
+     */
+    public NiceRefreshHandler(final int maxDelay) {
+        if (maxDelay <= 0) {
+            throw new IllegalArgumentException("Invalid maxDelay: " + maxDelay);
+        }
+        maxDelay_ = maxDelay;
+    }
+
+    /**
+     * Refreshes the specified page using the specified URL immediately if the <tt>requestedWait</tt>
+     * not larget that the <tt>maxDelay</tt>. Does nothing otherwise.
+     * @param page the page that is going to be refreshed
+     * @param url the URL where the new page will be loaded
+     * @param requestedWait the number of seconds to wait before reloading the page
+     * @throws IOException if the refresh fails
+     */
+    public void handleRefresh(final Page page, final URL url, final int requestedWait) throws IOException {
+        if (requestedWait > maxDelay_) {
+            return;
+        }
+
+        super.handleRefresh(page, url, requestedWait);
+    }
+
+}
---
+++ src/test/java/com/gargoylesoftware/htmlunit/NiceRefreshHandlerTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2002-2011 Gargoyle Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.gargoylesoftware.htmlunit;
+
+import java.net.URL;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+/**
+ * Tests for {@link NiceRefreshHandler}.
+ *
+ * @version $Revision$
+ * @author Marc Guillemot
+ */
+@RunWith(BrowserRunner.class)
+public final class NiceRefreshHandlerTest extends WebTestCase {
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    public void refreshImmediatelyForDelaySmallerThanMax() throws Exception {
+        doTest(2, URL_THIRD);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    public void noRefreshForDelayLargerThanMax() throws Exception {
+        doTest(1, URL_FIRST);
+    }
+
+    private void doTest(final int handlerMaxDelay, final URL expectedUrl) throws Exception {
+        final String html = "<html>\n"
+            + "<head><title>Meta Redirect Page</title>\n"
+            + "<meta http-equiv='Refresh' content='2; URL=" + URL_THIRD + "'>\n"
+            + "</head>\n"
+            + "<body>redirect page body</body>\n"
+            + "</html>";
+
+        final WebClient client = getWebClientWithMockWebConnection();
+        final MockWebConnection conn = getMockWebConnection();
+        conn.setResponse(URL_FIRST, html);
+        conn.setDefaultResponse("");
+        client.setRefreshHandler(new NiceRefreshHandler(handlerMaxDelay));
+
+        final HtmlPage page = client.getPage(URL_FIRST);
+        assertEquals(expectedUrl, page.getUrl());
+    }
+}
