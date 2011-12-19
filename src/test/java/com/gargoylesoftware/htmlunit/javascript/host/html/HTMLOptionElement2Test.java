765a7f28abcf61e1276e21481dc18f93918adc83
mguillem <mguillemot@yahoo.fr>
2011-12-19 07:31:44 +0000

    adapted click2 to avoid alerts and allow to run the test manually, added expectations for chrome
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@6677 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLOptionElement2Test.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLOptionElement2Test.java
@@ -21,6 +21,8 @@ import org.openqa.selenium.WebDriver;
 
 import com.gargoylesoftware.htmlunit.BrowserRunner;
 import com.gargoylesoftware.htmlunit.BrowserRunner.Alerts;
+import com.gargoylesoftware.htmlunit.BrowserRunner.Browser;
+import com.gargoylesoftware.htmlunit.BrowserRunner.NotYetImplemented;
 import com.gargoylesoftware.htmlunit.WebDriverTestCase;
 
 /**
@@ -72,9 +74,12 @@ public class HTMLOptionElement2Test extends WebDriverTestCase {
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts(FF = { "a", "a", "a" }, IE8 = { "a", "b", "b" }, IE = { "a", "b" })
+    @Alerts(FF = "ab", IE8 = "abb", IE = "ab", CHROME = "")
+    @NotYetImplemented(Browser.FF)
     //TODO: Needs further investigation of clicking an option without clicking the select
     //See the first comment in http://code.google.com/p/selenium/issues/detail?id=2131#c1
+    // Additionally, FF and Chrome drivers look buggy as they don't allow to capture
+    // what happens when running the test manually in the browser.
     public void click2() throws Exception {
         final String html
             = "<html><head><title>foo</title><script>\n"
@@ -87,7 +92,7 @@ public class HTMLOptionElement2Test extends WebDriverTestCase {
             + "    }\n"
             + "  }\n"
             + "  function handle(event) {\n"
-            + "    alert(s.options[s.selectedIndex].value);\n"
+            + "    document.getElementById('input').value += s.options[s.selectedIndex].value;\n"
             + "  }\n"
             + "</script></head><body onload='init()'>\n"
             + "  <select id='s'>\n"
@@ -95,12 +100,13 @@ public class HTMLOptionElement2Test extends WebDriverTestCase {
             + "    <option id='opb' value='b'>B</option>\n"
             + "    <option value='c'>C</option>\n"
             + "  </select>\n"
+            + "  <input id='input'>\n"
             + "</body></html>";
 
         final WebDriver driver = loadPage2(html);
         driver.findElement(By.id("s")).click();
         driver.findElement(By.id("opb")).click();
-        assertEquals(getExpectedAlerts(), getCollectedAlerts(driver));
+        assertEquals(getExpectedAlerts()[0], driver.findElement(By.id("input")).getAttribute("value"));
     }
 
     /**
@@ -109,7 +115,7 @@ public class HTMLOptionElement2Test extends WebDriverTestCase {
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts(FF = { "1", "option1", "0" }, IE = { "1", "option2", "1" })
+    @Alerts(DEFAULT = { "1", "option1", "0" }, IE = { "1", "option2", "1" })
     public void unselectResetToFirstOption() throws Exception {
         final String html
             = "<html><head><title>foo</title><script>\n"
