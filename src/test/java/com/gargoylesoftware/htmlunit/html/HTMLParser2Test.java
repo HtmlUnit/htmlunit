84eca3857f5df87a1f473734c5a01aaec6c1743b
rbri <rbri@rbri.de>
2012-05-17 17:28:00 +0000

    test for issue 2968697 Issue 2968697
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@6886 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/html/HTMLParser2Test.java
+++ src/test/java/com/gargoylesoftware/htmlunit/html/HTMLParser2Test.java
@@ -141,4 +141,81 @@ public class HTMLParser2Test extends WebDriverTestCase {
 
         loadPageWithAlerts2(html);
     }
+
+    /**
+     * This tests for an bug in NekoHTML.
+     * Without setting a property NekoHTML closes all inline tags
+     * when a table start tag is detected. This is ok from the spec
+     * but different with real browsers.
+     *
+     * @throws Exception on test failure
+     */
+    @Test
+    @Alerts(FF3 = { "<ABBR>", "<ACRONYM>", "<CITE>", "<CODE>",
+                    "<DFN>", "<KBD>", "<LABEL>", "<SAMP>", "<VAR>" },
+            FF3_6 = { "<ABBR>", "<ACRONYM>", "<CITE>", "<CODE>",
+                    "<DFN>", "<KBD>", "<LABEL>", "<SAMP>", "<VAR>" },
+            DEFAULT = { }
+    )
+    @NotYetImplemented
+    public void tableClosesInlineTags() throws Exception {
+        final String html = "<html><head>\n"
+                + "<script>\n"
+                + "   function test() {\n"
+                + "     var myP = document.getElementById('myP');\n"
+                + "     for(var i=0; i<myP.childNodes.length; i++) {\n"
+                + "       var myNode = myP.childNodes[i];\n"
+                + "       if (myNode.nodeType == 1 && myNode.nodeName != 'TABLE') {\n"
+                + "         var hasTable = false;\n"
+                + "         for(var j=0; j<myNode.childNodes.length; j++) {\n"
+                + "           if (myNode.childNodes[j].nodeName == 'TABLE') {\n"
+                + "             hasTable = true;\n"
+                + "           }\n"
+                + "         }\n"
+                + "         if (!hasTable) {\n"
+                + "           alert('<' + myNode.nodeName + '>');\n"
+                + "         }\n"
+                + "       }\n"
+                + "     }\n"
+                + "   }   \n"
+                + "</script>\n"
+                + "</head>\n"
+                + "\n"
+                + "<body onload='test()'>\n"
+                + "\n"
+                + "<p id='myP'>\n"
+                + "  <a><table></table></a>\n"
+                + "  <abbr><table></table></abbr>\n"
+                + "  <acronym><table></table></acronym>\n"
+                + "  <b><table></table></b>\n"
+                + "  <bdo><table></table></bdo>\n"
+                + "  <big><table></table></big>\n"
+                + "  <button><table></table></button>\n"
+                + "  <cite><table></table></cite>\n"
+                + "  <code><table></table></code>\n"
+                + "  <del><table></table></del>\n"
+                + "  <dfn><table></table></dfn>\n"
+                + "  <em><table></table></em>\n"
+                + "  <font><table></table></font>\n"
+                + "  <i><table></table></i>\n"
+                + "  <ins><table></table></ins>\n"
+                + "  <kbd><table></table></kbd>\n"
+                + "  <label><table></table></label>\n"
+                + "  <map><table></table></map>\n"
+                + "  <q><table></table></q>\n"
+                + "  <samp><table></table></samp>\n"
+                + "  <small><table></table></small>\n"
+                + "  <span><table></table></span>\n"
+                + "  <strong><table></table></strong>\n"
+                + "  <sub><table></table></sub>\n"
+                + "  <sup><table></table></sup>\n"
+                + "  <tt><table></table></tt>\n"
+                + "  <var><table></table></var>\n"
+                + "</p>\n"
+                + "\n"
+                + "</body>\n"
+                + "</html>";
+
+        loadPageWithAlerts2(html);
+    }
 }
