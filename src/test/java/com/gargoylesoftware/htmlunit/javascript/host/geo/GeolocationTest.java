04c4335040d946d30e060c2b9a8528e4680ad64d
asashour <asashour@yahoo.com>
2012-08-01 18:43:10 +0000

    Mark as @NYI (as it runs on Windows only for now)
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@7154 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/geo/GeolocationTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/geo/GeolocationTest.java
@@ -32,6 +32,7 @@ import org.junit.runner.RunWith;
 
 import com.gargoylesoftware.htmlunit.BrowserRunner;
 import com.gargoylesoftware.htmlunit.BrowserRunner.Alerts;
+import com.gargoylesoftware.htmlunit.BrowserRunner.NotYetImplemented;
 import com.gargoylesoftware.htmlunit.CollectingAlertHandler;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.WebServerTestCase;
@@ -51,6 +52,7 @@ public class GeolocationTest extends WebServerTestCase {
      */
     @Test
     @Alerts(FF = "12.34567891 98.76543211")
+    @NotYetImplemented //since it runs on Windows only (for now)
     public void getCurrentPosition() throws Exception {
         final Map<String, Class<? extends Servlet>> servlets = new HashMap<String, Class<? extends Servlet>>();
         servlets.put("/test", GetCurrentPositionTestServlet.class);
