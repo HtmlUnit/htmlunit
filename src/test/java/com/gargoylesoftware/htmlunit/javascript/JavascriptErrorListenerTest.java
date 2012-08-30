76c13c3bb3e256b9baf4c5caf3a7a82ac933880f
mguillem <mguillemot@yahoo.fr>
2012-08-30 14:01:33 +0000

    split test method in more precise methods
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@7321 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/JavascriptErrorListenerTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/JavascriptErrorListenerTest.java
@@ -41,6 +41,7 @@ import com.gargoylesoftware.htmlunit.util.NameValuePair;
  *
  * @version $Revision$
  * @author Ronald Brill
+ * @author Marc Guillemot
  */
 @RunWith(BrowserRunner.class)
 public class JavascriptErrorListenerTest extends WebServerTestCase {
@@ -117,127 +118,151 @@ public class JavascriptErrorListenerTest extends WebServerTestCase {
     }
 
     /**
-     * Test for running with a JavaScript error listener.
-     *
+     * Listener should capture script exception.
      * @throws Exception if the test fails
      */
     @Test
-    public void testJavaScriptErrorListener() throws Exception {
-        final StringBuilder scriptExceptions = new StringBuilder();
-        final StringBuilder timeoutErrors = new StringBuilder();
-        final StringBuilder loadScriptErrors = new StringBuilder();
-        final StringBuilder malformedScriptURLErrors = new StringBuilder();
-
+    public void listenForScriptException() throws Exception {
         final WebClient webClient = getWebClient();
         webClient.getOptions().setThrowExceptionOnScriptError(false);
-        webClient.setJavaScriptErrorListener(new JavaScriptErrorListener() {
-
-            public void loadScriptError(final HtmlPage htmlPage, final URL scriptUrl, final Exception exception) {
-                loadScriptErrors.append(scriptUrl.toExternalForm());
-                loadScriptErrors.append(", ");
-                loadScriptErrors.append(exception.toString());
-            }
-
-            public void malformedScriptURL(final HtmlPage htmlPage, final String url,
-                    final MalformedURLException malformedURLException) {
-                malformedScriptURLErrors.append(url);
-                malformedScriptURLErrors.append(", ");
-                malformedScriptURLErrors.append(malformedURLException.toString());
-            }
-
-            public void scriptException(final HtmlPage htmlPage, final ScriptException scriptException) {
-                scriptExceptions.append(scriptException.toString());
-            }
-
-            public void timeoutError(final HtmlPage htmlPage, final long allowedTime, final long executionTime) {
-                timeoutErrors.append("Timeout allowed: " + allowedTime);
-            }
-        });
-
-        final MockWebConnection webConnection = new MockWebConnection();
-        final String errorContent = "<html><head><title>ERROR 500</title></head><body></body></html>";
-        final List<? extends NameValuePair> emptyList = Collections.emptyList();
-        webConnection.setResponse(URL_SECOND, errorContent, 500, "BOOM", "text/html", emptyList);
+        final CollectingJavaScriptErrorListener javaScriptErrorListener = new CollectingJavaScriptErrorListener();
+        webClient.setJavaScriptErrorListener(javaScriptErrorListener);
 
         // test script exception
-        String content = "<html><head><title>Throw JavaScript Error</title>"
+        final String html = "<html><head><title>Throw JavaScript Error</title>"
             + "<script>unknown.foo();</script></head>"
             + "<body></body></html>";
-        webConnection.setResponse(URL_FIRST, content);
-        webClient.setWebConnection(webConnection);
-        webClient.getPage(URL_FIRST);
+        loadPage(html);
 
         assertEquals("com.gargoylesoftware.htmlunit.ScriptException: "
                 + "ReferenceError: \"unknown\" is not defined. "
                 + "(script in http://localhost:" + PORT + "/ from (1, 58) to (1, 81)#1)",
-                scriptExceptions.toString());
-        assertEquals("", loadScriptErrors.toString());
-        assertEquals("", malformedScriptURLErrors.toString());
-        assertEquals("", timeoutErrors.toString());
+                javaScriptErrorListener.getScriptExceptions());
+        assertEquals("", javaScriptErrorListener.getLoadScriptErrors());
+        assertEquals("", javaScriptErrorListener.getMalformedScriptURLErrors());
+        assertEquals("", javaScriptErrorListener.getTimeoutErrors());
+    }
 
-        // test load script error
-        content = "<html><head><title>Throw JavaScript Error</title>"
-            + "<script src='" + URL_SECOND + "' type='text/javascript'></script></head>"
+    /**
+     * Listener should capture script exception.
+     * @throws Exception if the test fails
+     */
+    @Test
+    public void listenForLoadScriptError() throws Exception {
+        final WebClient webClient = getWebClient();
+        webClient.getOptions().setThrowExceptionOnScriptError(false);
+        final CollectingJavaScriptErrorListener javaScriptErrorListener = new CollectingJavaScriptErrorListener();
+        webClient.setJavaScriptErrorListener(javaScriptErrorListener);
+
+        getMockWebConnection().setDefaultResponse("", 500, "Server Error", "text/html");
+
+        final String html = "<html><head>"
+            + "<script src='notExisting.js' type='text/javascript'></script></head>"
             + "<body></body></html>";
-        webConnection.setResponse(URL_FIRST, content);
-        webClient.setWebConnection(webConnection);
+
         try {
-            webClient.getPage(URL_FIRST);
+            loadPage(html);
             Assert.fail("FailingHttpStatusCodeException expected");
         }
         catch (final FailingHttpStatusCodeException e) {
             // expected
         }
 
-        assertEquals("com.gargoylesoftware.htmlunit.ScriptException: "
-                + "ReferenceError: \"unknown\" is not defined. "
-                + "(script in http://localhost:" + PORT + "/ from (1, 58) to (1, 81)#1)",
-                scriptExceptions.toString());
-        assertEquals("http://localhost:" + PORT + "/second/, "
+        assertEquals("", javaScriptErrorListener.getScriptExceptions());
+        assertEquals(getDefaultUrl() + "notExisting.js, "
                 + "com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException: "
-                + "500 BOOM for http://localhost:" + PORT + "/second/", loadScriptErrors.toString());
-        assertEquals("", malformedScriptURLErrors.toString());
-        assertEquals("", timeoutErrors.toString());
-
-        // test malformed script url error
-        content = "<html><head><title>Throw JavaScript Error</title>"
-            + "<script src='unknown://nowhere' type='text/javascript'></script></head>"
-            + "<body></body></html>";
-        webConnection.setResponse(URL_FIRST, content);
-        webClient.setWebConnection(webConnection);
-        webClient.getPage(URL_FIRST);
-
-        assertEquals("com.gargoylesoftware.htmlunit.ScriptException: "
-                + "ReferenceError: \"unknown\" is not defined. "
-                + "(script in http://localhost:" + PORT + "/ from (1, 58) to (1, 81)#1)",
-                scriptExceptions.toString());
-        assertEquals("http://localhost:" + PORT + "/second/, "
-                + "com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException: "
-                + "500 BOOM for http://localhost:" + PORT + "/second/", loadScriptErrors.toString());
-        assertEquals("unknown://nowhere, java.net.MalformedURLException: unknown protocol: unknown",
-                malformedScriptURLErrors.toString());
-        assertEquals("", timeoutErrors.toString());
-
-        // test timeout error
-        webClient.setJavaScriptTimeout(100);
-
-        content = "<html><head><title>Throw JavaScript Timeout Error</title>"
-            + "<script>while(1) {}</script></head>"
-            + "<body></body></html>";
-        webConnection.setResponse(URL_FIRST, content);
-        webClient.setWebConnection(webConnection);
-        webClient.getPage(URL_FIRST);
-
-        assertEquals("com.gargoylesoftware.htmlunit.ScriptException: "
-                + "ReferenceError: \"unknown\" is not defined. "
-                + "(script in http://localhost:" + PORT + "/ from (1, 58) to (1, 81)#1)",
-                scriptExceptions.toString());
-        assertEquals("http://localhost:" + PORT + "/second/, "
-                + "com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException: "
-                + "500 BOOM for http://localhost:" + PORT + "/second/", loadScriptErrors.toString());
-        assertEquals("unknown://nowhere, java.net.MalformedURLException: unknown protocol: unknown",
-                malformedScriptURLErrors.toString());
-        assertEquals("Timeout allowed: 100", timeoutErrors.toString());
+                + "500 Server Error for " + getDefaultUrl() + "notExisting.js",
+                javaScriptErrorListener.getLoadScriptErrors());
+        assertEquals("", javaScriptErrorListener.getMalformedScriptURLErrors());
+        assertEquals("", javaScriptErrorListener.getTimeoutErrors());
     }
 
+    /**
+     * Listener should capture script exception.
+     * @throws Exception if the test fails
+     */
+    @Test
+    public void listenForMalformedScriptUrl() throws Exception {
+        final WebClient webClient = getWebClient();
+        webClient.getOptions().setThrowExceptionOnScriptError(false);
+        final CollectingJavaScriptErrorListener javaScriptErrorListener = new CollectingJavaScriptErrorListener();
+        webClient.setJavaScriptErrorListener(javaScriptErrorListener);
+
+        final String html = "<html><head><title>Throw JavaScript Error</title>"
+                + "<script src='unknown://nowhere' type='text/javascript'></script></head>"
+                + "<body></body></html>";
+
+        loadPage(html);
+
+        assertEquals("", javaScriptErrorListener.getScriptExceptions());
+        assertEquals("", javaScriptErrorListener.getLoadScriptErrors());
+        assertEquals("unknown://nowhere, java.net.MalformedURLException: unknown protocol: unknown",
+                    javaScriptErrorListener.getMalformedScriptURLErrors());
+        assertEquals("", javaScriptErrorListener.getTimeoutErrors());
+    }
+
+    /**
+     * Listener should capture timeout errors.
+     * Configured with a timeout as the build server seemed to have problem with this test from time to time.
+     * @throws Exception if the test fails
+     */
+    @Test(timeout = 10000)
+    public void listenForTimeoutError() throws Exception {
+        final WebClient webClient = getWebClient();
+        webClient.getOptions().setThrowExceptionOnScriptError(false);
+        final CollectingJavaScriptErrorListener javaScriptErrorListener = new CollectingJavaScriptErrorListener();
+        webClient.setJavaScriptErrorListener(javaScriptErrorListener);
+        webClient.setJavaScriptTimeout(100);
+
+        final String html = "<html><head><title>Throw JavaScript Timeout Error</title>"
+            + "<script>while(1) {}</script></head>"
+            + "<body></body></html>";
+
+        loadPage(html);
+
+        assertEquals("", javaScriptErrorListener.getScriptExceptions());
+        assertEquals("", javaScriptErrorListener.getLoadScriptErrors());
+        assertEquals("", javaScriptErrorListener.getMalformedScriptURLErrors());
+        assertEquals("Timeout allowed: 100", javaScriptErrorListener.getTimeoutErrors());
+    }
+}
+
+class CollectingJavaScriptErrorListener implements JavaScriptErrorListener {
+    private final StringBuilder scriptExceptions_ = new StringBuilder();
+    private final StringBuilder timeoutErrors_ = new StringBuilder();
+    private final StringBuilder loadScriptErrors_ = new StringBuilder();
+    private final StringBuilder malformedScriptURLErrors_ = new StringBuilder();
+
+    public void loadScriptError(final HtmlPage htmlPage, final URL scriptUrl, final Exception exception) {
+        loadScriptErrors_.append(scriptUrl + ", " + exception);
+    }
+
+    public void malformedScriptURL(final HtmlPage htmlPage, final String url,
+            final MalformedURLException malformedURLException) {
+        malformedScriptURLErrors_.append(url + ", " + malformedURLException);
+    }
+
+    public void scriptException(final HtmlPage htmlPage, final ScriptException scriptException) {
+        scriptExceptions_.append(scriptException.toString());
+    }
+
+    public void timeoutError(final HtmlPage htmlPage, final long allowedTime, final long executionTime) {
+        timeoutErrors_.append("Timeout allowed: " + allowedTime);
+    }
+
+    public String getScriptExceptions() {
+        return scriptExceptions_.toString();
+    }
+
+    public String getLoadScriptErrors() {
+        return loadScriptErrors_.toString();
+    }
+
+    public String getMalformedScriptURLErrors() {
+        return malformedScriptURLErrors_.toString();
+    }
+
+    public String getTimeoutErrors() {
+        return timeoutErrors_.toString();
+    }
 }
