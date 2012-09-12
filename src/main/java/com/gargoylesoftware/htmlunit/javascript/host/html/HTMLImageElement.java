ce14d5c257b2f3a07488f31b0c6ff80b3a997fd9
rbri <rbri@rbri.de>
2012-09-12 18:51:25 +0000

    align handling fix (next step)
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@7363 5f5364db-9458-4db8-a492-e30667be6df6

--- src/main/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLImageElement.java
+++ src/main/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLImageElement.java
@@ -15,11 +15,14 @@
 package com.gargoylesoftware.htmlunit.javascript.host.html;
 
 import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import net.sourceforge.htmlunit.corejs.javascript.Context;
 
 import org.apache.xalan.xsltc.runtime.AttributeList;
 
+import com.gargoylesoftware.htmlunit.BrowserVersionFeatures;
 import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.SgmlPage;
 import com.gargoylesoftware.htmlunit.html.DomElement;
@@ -40,6 +43,20 @@ import com.gargoylesoftware.htmlunit.javascript.PostponedAction;
  * @author Ahmed Ashour
  */
 public class HTMLImageElement extends HTMLElement {
+    private static final Map<String, String> NORMALIZED_ALIGN_VALUES;
+    static {
+        NORMALIZED_ALIGN_VALUES = new HashMap<String, String>();
+        NORMALIZED_ALIGN_VALUES.put("center", "center");
+        NORMALIZED_ALIGN_VALUES.put("left", "left");
+        NORMALIZED_ALIGN_VALUES.put("right", "right");
+        NORMALIZED_ALIGN_VALUES.put("bottom", "bottom");
+        NORMALIZED_ALIGN_VALUES.put("middle", "middle");
+        NORMALIZED_ALIGN_VALUES.put("top", "top");
+        NORMALIZED_ALIGN_VALUES.put("absbottom", "absBottom");
+        NORMALIZED_ALIGN_VALUES.put("absmiddle", "absMiddle");
+        NORMALIZED_ALIGN_VALUES.put("baseline", "baseline");
+        NORMALIZED_ALIGN_VALUES.put("texttop", "textTop");
+    }
 
     private boolean instantiatedViaJavaScript_ = false;
 
@@ -166,15 +183,42 @@ public class HTMLImageElement extends HTMLElement {
      * @return the value of the "align" property
      */
     public String jsxGet_align() {
-        return getAlign(true);
+        final boolean acceptArbitraryValues =
+                getBrowserVersion().hasFeature(BrowserVersionFeatures.JS_ALIGN_ACCEPTS_ARBITRARY_VALUES);
+
+        final String align = getDomNodeOrDie().getAttribute("align");
+        if (acceptArbitraryValues) {
+            return align;
+        }
+
+        final String normalizedValue = NORMALIZED_ALIGN_VALUES.get(align.toLowerCase());
+        if (null != normalizedValue) {
+            return normalizedValue;
+        }
+        return "";
     }
 
     /**
      * Sets the value of the "align" property.
      * @param align the value of the "align" property
      */
-    public void jsxSet_align(final String align) {
-        setAlign(align, false);
+    public void jsxSet_align(String align) {
+        align = align.toLowerCase();
+        final boolean acceptArbitraryValues =
+                getBrowserVersion().hasFeature(BrowserVersionFeatures.JS_ALIGN_ACCEPTS_ARBITRARY_VALUES);
+        if (acceptArbitraryValues) {
+            getDomNodeOrDie().setAttribute("align", align);
+            return;
+        }
+
+        final String normalizedValue = NORMALIZED_ALIGN_VALUES.get(align.toLowerCase());
+        if (null != normalizedValue) {
+            getDomNodeOrDie().setAttribute("align", normalizedValue);
+            return;
+        }
+
+        throw Context.reportRuntimeError("Cannot set the align property to invalid value: '" + align + "'");
+
     }
 
     /**
--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLImageElementTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLImageElementTest.java
@@ -22,8 +22,6 @@ import org.openqa.selenium.WebElement;
 
 import com.gargoylesoftware.htmlunit.BrowserRunner;
 import com.gargoylesoftware.htmlunit.BrowserRunner.Alerts;
-import com.gargoylesoftware.htmlunit.BrowserRunner.Browser;
-import com.gargoylesoftware.htmlunit.BrowserRunner.NotYetImplemented;
 import com.gargoylesoftware.htmlunit.WebDriverTestCase;
 
 /**
@@ -204,7 +202,6 @@ public class HTMLImageElementTest extends WebDriverTestCase {
                 "top", "absbottom", "absmiddle", "bottom", "texttop", "wrong", "" },
             IE = { "left", "right", "center", "", "bottom", "middle",
                 "top", "absBottom", "absMiddle", "baseline", "textTop", "", "" })
-    @NotYetImplemented(Browser.IE)
     public void getAlign() throws Exception {
         final String html
             = "<html><body>\n"
@@ -245,14 +242,13 @@ public class HTMLImageElementTest extends WebDriverTestCase {
      * @throws Exception if an error occurs
      */
     @Test
-    @Alerts(DEFAULT = { "CenTer", "8", "foo", "left", "right", "center", "justify",
+    @Alerts(DEFAULT = { "center", "8", "foo", "left", "right", "center", "justify",
                 "bottom", "middle", "top", "absbottom", "absmiddle", "baseline", "texttop" },
             FF10 = { "CenTer", "8", "foo", "left", "right", "middle", "justify",
                 "bottom", "middle", "top", "absbottom", "absmiddle", "bottom", "texttop" },
             IE = { "center", "error", "center", "error", "center", "left", "right",
                 "center", "error", "center", "bottom", "middle", "top", "absBottom",
                 "absMiddle", "baseline", "textTop" })
-    @NotYetImplemented
     public void setAlign() throws Exception {
         final String html
             = "<html><body>\n"
