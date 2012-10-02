b4d9643a541dcf575a7cfc6bd6764e5b964dcf43
asashour <asashour@yahoo.com>
2012-10-02 18:40:22 +0000

    Tries(3) for  jQuery tests
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@7500 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/libraries/JQuery182Test.java
+++ src/test/java/com/gargoylesoftware/htmlunit/libraries/JQuery182Test.java
@@ -770,6 +770,7 @@ public class JQuery182Test extends WebDriverTestCase {
      */
     @Test
     @Alerts("callbacks: jQuery.Callbacks( \"once stopOnFalse\" ) - no filter (0, 20, 20)")
+    @Tries(3)
     public void test_72() throws Exception {
         runTest(72);
     }
@@ -788,6 +789,7 @@ public class JQuery182Test extends WebDriverTestCase {
      */
     @Test
     @Alerts("callbacks: jQuery.Callbacks( \"once stopOnFalse\" ) - filter (0, 20, 20)")
+    @Tries(3)
     public void test_74() throws Exception {
         runTest(74);
     }
@@ -1519,6 +1521,7 @@ public class JQuery182Test extends WebDriverTestCase {
         FF10 = "attributes: attr(String, Object) - Loaded via XML document (0, 2, 2)",
         CHROME = "attributes: attr(String, Object) - Loaded via XML document (0, 2, 2)",
         IE = "attributes: attr(jquery_method) (0, 9, 9)")
+    @Tries(3)
     public void test_144() throws Exception {
         runTest(144);
     }
