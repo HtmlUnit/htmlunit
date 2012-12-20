75786f85de717be21ad7a37b8df25ce97eb8376d
asashour <asashour@yahoo.com>
2012-12-20 03:18:30 +0000

    Upper-case the charset detected from "meta" tag.
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@7893 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/util/EncodingSnifferTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/util/EncodingSnifferTest.java
@@ -36,16 +36,16 @@ public class EncodingSnifferTest {
      * @throws Exception if an error occurs
      */
     @Test
-    public void testSniffEncodingFromHttpHeaders() throws Exception {
-        testHeader(null, null, null);
-        testHeader(null, "foo", "bar");
-        testHeader(null, "Content-Type", "blah");
-        testHeader(null, "Content-Type", "text/html;charset=blah");
-        testHeader("UTF-8", "Content-Type", "text/html;charset=utf-8");
-        testHeader("UTF-8", "Content-Type", "text/html;charset=utf-8;");
+    public void fromHttpHeaders() throws Exception {
+        header(null, null, null);
+        header(null, "foo", "bar");
+        header(null, "Content-Type", "blah");
+        header(null, "Content-Type", "text/html;charset=blah");
+        header("UTF-8", "Content-Type", "text/html;charset=utf-8");
+        header("UTF-8", "Content-Type", "text/html;charset=utf-8;");
     }
 
-    private void testHeader(final String expectedEncoding, final String headerName, final String headerValue) {
+    private void header(final String expectedEncoding, final String headerName, final String headerValue) {
         final NameValuePair header = new NameValuePair(headerName, headerValue);
         assertEquals(expectedEncoding, sniffEncodingFromHttpHeaders(singletonList(header)));
     }
@@ -54,31 +54,31 @@ public class EncodingSnifferTest {
      * @throws Exception if an error occurs
      */
     @Test
-    public void testSniffEncodingFromMetaTag() throws Exception {
-        testMeta(null, "");
-        testMeta(null, "foo");
-        testMeta(null, "<!--");
-        testMeta(null, " <!-- blah");
-        testMeta(null, " <!-- blah --> ");
-        testMeta(null, "<");
-        testMeta(null, "</");
-        testMeta(null, "<meta/>");
-        testMeta(null, "<meta />");
-        testMeta(null, "<meta blah />");
-        testMeta(null, "<meta");
-        testMeta(null, "<meta ");
-        testMeta(null, "<meta blah");
-        testMeta(null, "<meta blah  ");
-        testMeta(null, "<meta a='b'");
-        testMeta(null, "<meta a='b' c=d e=\"f\"/>");
-        testMeta(null, "<meta a='b' c=d e=\"f\" content='text/html; charset=blah' />");
-        testMeta("UTF-8", "<meta a='b' c=d e=\"f\" content='text/html; charset=utf-8' />");
-        testMeta("UTF-8", "abc <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>");
-        testMeta("UTF-8", "abc <meta http-equiv='Content-Type' content='text/html; CHARSET=UTF-8'/>");
-        testMeta("UTF-8", "abc <meta http-equiv='Content-Type' content='text/html; chArsEt=UtF-8'/>");
+    public void fromMetaTag() throws Exception {
+        meta(null, "");
+        meta(null, "foo");
+        meta(null, "<!--");
+        meta(null, " <!-- blah");
+        meta(null, " <!-- blah --> ");
+        meta(null, "<");
+        meta(null, "</");
+        meta(null, "<meta/>");
+        meta(null, "<meta />");
+        meta(null, "<meta blah />");
+        meta(null, "<meta");
+        meta(null, "<meta ");
+        meta(null, "<meta blah");
+        meta(null, "<meta blah  ");
+        meta(null, "<meta a='b'");
+        meta(null, "<meta a='b' c=d e=\"f\"/>");
+        meta(null, "<meta a='b' c=d e=\"f\" content='text/html; charset=blah' />");
+        meta("UTF-8", "<meta a='b' c=d e=\"f\" content='text/html; charset=utf-8' />");
+        meta("UTF-8", "abc <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>");
+        meta("UTF-8", "abc <meta http-equiv='Content-Type' content='text/html; CHARSET=UTF-8'/>");
+        meta("UTF-8", "abc <meta http-equiv='Content-Type' content='text/html; chArsEt=UtF-8'/>");
     }
 
-    private void testMeta(final String expectedEncoding, final String content) {
+    private void meta(final String expectedEncoding, final String content) {
         assertEquals(expectedEncoding, sniffEncodingFromMetaTag(content.getBytes()));
     }
 
@@ -86,24 +86,24 @@ public class EncodingSnifferTest {
      * @throws Exception if an error occurs
      */
     @Test
-    public void testSniffEncodingFromXmlDeclaration() throws Exception {
-        testXmlDeclaration(null, "");
-        testXmlDeclaration(null, "foo");
-        testXmlDeclaration(null, "<?");
-        testXmlDeclaration(null, "<?xml");
-        testXmlDeclaration(null, "<?xml ");
-        testXmlDeclaration(null, "<?xml encoding");
-        testXmlDeclaration(null, "<?xml encoding=");
-        testXmlDeclaration(null, "<?xml encoding='utf-8");
-        testXmlDeclaration(null, "<?xml encoding='utf-8'");
-        testXmlDeclaration(null, "<?xml encoding='blah'?>");
-        testXmlDeclaration("utf-8", "<?xml encoding='utf-8'?>");
-        testXmlDeclaration(null, "<?xml encoding=\"utf-8");
-        testXmlDeclaration(null, "<?xml encoding=\"utf-8\"");
-        testXmlDeclaration("utf-8", "<?xml encoding=\"utf-8\"?>");
+    public void fromXmlDeclaration() throws Exception {
+        xmlDeclaration(null, "");
+        xmlDeclaration(null, "foo");
+        xmlDeclaration(null, "<?");
+        xmlDeclaration(null, "<?xml");
+        xmlDeclaration(null, "<?xml ");
+        xmlDeclaration(null, "<?xml encoding");
+        xmlDeclaration(null, "<?xml encoding=");
+        xmlDeclaration(null, "<?xml encoding='utf-8");
+        xmlDeclaration(null, "<?xml encoding='utf-8'");
+        xmlDeclaration(null, "<?xml encoding='blah'?>");
+        xmlDeclaration("utf-8", "<?xml encoding='utf-8'?>");
+        xmlDeclaration(null, "<?xml encoding=\"utf-8");
+        xmlDeclaration(null, "<?xml encoding=\"utf-8\"");
+        xmlDeclaration("utf-8", "<?xml encoding=\"utf-8\"?>");
     }
 
-    private void testXmlDeclaration(final String expectedEncoding, final String content) {
+    private void xmlDeclaration(final String expectedEncoding, final String content) {
         assertEquals(expectedEncoding, sniffEncodingFromXmlDeclaration(content.getBytes()));
     }
 
@@ -111,35 +111,35 @@ public class EncodingSnifferTest {
      * @throws Exception if an error occurs
      */
     @Test
-    public void testExtractEncodingFromContentType() throws Exception {
-        testContentType(null, null);
-        testContentType(null, "");
-        testContentType(null, " \t \n ");
-        testContentType(null, "foo");
-        testContentType(null, "text/html");
-        testContentType(null, "\n text/html \t");
-        testContentType(null, "\n text/html ; char \t");
-        testContentType(null, "\n text/html ; charset \t");
-        testContentType(null, "\n text/html ; charset=");
-        testContentType(null, "\n text/html ; charset= \t");
-        testContentType(null, "\n text/html ; charset =");
-        testContentType(null, "\n text/html ; charset = \n");
-        testContentType(null, "\n text/html ; charset=blah");
-        testContentType("utf-8", "\n text/html ; charset=utf-8");
-        testContentType("utf-8", "\n text/html ; charset=utf-8;");
-        testContentType("utf-8", "\n text/html ; charset = \n utf-8 ");
-        testContentType("utf-8", "\n text/html ; charset = \n utf-8 ; ");
-        testContentType(null, "\n text/html ; charset = \n'");
-        testContentType(null, "\n text/html ; charset = \n' ");
-        testContentType(null, "\n text/html ; charset = \n' utf-8");
-        testContentType("utf-8", "\n text/html ; charset = \n'utf-8'");
-        testContentType(null, "\n text/html ; charset = \n\"");
-        testContentType(null, "\n text/html ; charset = \n\" ");
-        testContentType(null, "\n text/html ; charset = \n\" utf-8");
-        testContentType("utf-8", "\n text/html ; charset = \n\"utf-8\"");
+    public void fromContentType() throws Exception {
+        contentType(null, null);
+        contentType(null, "");
+        contentType(null, " \t \n ");
+        contentType(null, "foo");
+        contentType(null, "text/html");
+        contentType(null, "\n text/html \t");
+        contentType(null, "\n text/html ; char \t");
+        contentType(null, "\n text/html ; charset \t");
+        contentType(null, "\n text/html ; charset=");
+        contentType(null, "\n text/html ; charset= \t");
+        contentType(null, "\n text/html ; charset =");
+        contentType(null, "\n text/html ; charset = \n");
+        contentType(null, "\n text/html ; charset=blah");
+        contentType("utf-8", "\n text/html ; charset=utf-8");
+        contentType("utf-8", "\n text/html ; charset=utf-8;");
+        contentType("utf-8", "\n text/html ; charset = \n utf-8 ");
+        contentType("utf-8", "\n text/html ; charset = \n utf-8 ; ");
+        contentType(null, "\n text/html ; charset = \n'");
+        contentType(null, "\n text/html ; charset = \n' ");
+        contentType(null, "\n text/html ; charset = \n' utf-8");
+        contentType("utf-8", "\n text/html ; charset = \n'utf-8'");
+        contentType(null, "\n text/html ; charset = \n\"");
+        contentType(null, "\n text/html ; charset = \n\" ");
+        contentType(null, "\n text/html ; charset = \n\" utf-8");
+        contentType("utf-8", "\n text/html ; charset = \n\"utf-8\"");
     }
 
-    private void testContentType(final String expectedEncoding, final String contentType) {
+    private void contentType(final String expectedEncoding, final String contentType) {
         assertEquals(expectedEncoding, extractEncodingFromContentType(contentType));
     }
 
