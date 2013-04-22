ee8d4b2772a588a127415c4bd53f0ac814ab4f96
rbri <rbri@rbri.de>
2013-04-22 18:45:34 +0000

    more testcases added
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@8238 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/html/HtmlRadioButtonInput2Test.java
+++ src/test/java/com/gargoylesoftware/htmlunit/html/HtmlRadioButtonInput2Test.java
@@ -186,6 +186,153 @@ public class HtmlRadioButtonInput2Test extends WebDriverTestCase {
     }
 
     /**
+     * Verifies the behavior of 'checked' property on being attached to a page.
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts(DEFAULT = { "true", "true", "true", "true", "true", "true" })
+    public void checked_appendChild_fromHtml() throws Exception {
+        final String html = "<html>\n"
+            + "<head>\n"
+            + "  <script>\n"
+            + "    function test() {\n"
+            + "      var builder = document.createElement('div');\n"
+            + "      builder.innerHTML = '<input type=\"radio\" checked>';\n"
+            + "      var input = builder.firstChild;\n"
+            + "      alert(input.checked);\n"
+            + "      var parent=document.getElementById('myDiv');\n"
+            + "      parent.appendChild(input);\n"
+            + "      alert(input.checked);\n"
+            + "      parent.removeChild(input);\n"
+            + "      alert(input.checked);\n"
+            + "\n"
+            + "      input.defaultChecked = true;\n"
+            + "      alert(input.checked);\n"
+            + "      parent.appendChild(input);\n"
+            + "      alert(input.checked);\n"
+            + "      parent.removeChild(input);\n"
+            + "      alert(input.checked);\n"
+            + "    }\n"
+            + "  </script>\n"
+            + "</head><body onload='test()'>\n"
+            + "  <form><div id='myDiv'></div></div></form>\n"
+            + "</body></html>";
+
+        loadPageWithAlerts2(html);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts(DEFAULT = { "false", "false", "false", "true", "true", "true" },
+            IE = { "false", "false", "false", "false", "false", "false" },
+            IE6 = { "false", "false", "false", "false", "true", "true" })
+    public void notchecked_appendChild_fromHtml() throws Exception {
+        final String html = "<html>\n"
+            + "<head>\n"
+            + "  <script>\n"
+            + "    function test() {\n"
+            + "      var builder = document.createElement('div');\n"
+            + "      builder.innerHTML = '<input type=\"radio\">';\n"
+            + "      var input = builder.firstChild;\n"
+            + "      alert(input.checked);\n"
+            + "      var parent=document.getElementById('myDiv');\n"
+            + "      parent.appendChild(input);\n"
+            + "      alert(input.checked);\n"
+            + "      parent.removeChild(input);\n"
+            + "      alert(input.checked);\n"
+            + "\n"
+            + "      input.defaultChecked = true;\n"
+            + "      alert(input.checked);\n"
+            + "      parent.appendChild(input);\n"
+            + "      alert(input.checked);\n"
+            + "      parent.removeChild(input);\n"
+            + "      alert(input.checked);\n"
+            + "    }\n"
+            + "  </script>\n"
+            + "</head><body onload='test()'>\n"
+            + "  <form><div id='myDiv'></div></form>\n"
+            + "</body></html>";
+
+        loadPageWithAlerts2(html);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts(DEFAULT = { "false", "false", "false", "true", "true", "true" },
+            IE = { "false", "false", "false", "false", "false", "false" },
+            IE6 = { "false", "false", "false", "false", "true", "true" })
+    public void notchecked_insertBefore_fromHtml() throws Exception {
+        final String html = "<html>\n"
+            + "<head>\n"
+            + "  <script>\n"
+            + "    function test() {\n"
+            + "      var builder = document.createElement('div');\n"
+            + "      builder.innerHTML = '<input type=\"radio\">';\n"
+            + "      var input = builder.firstChild;\n"
+            + "      alert(input.checked);\n"
+            + "      var parent=document.getElementById('myDiv');\n"
+            + "      var after=document.getElementById('divAfter');\n"
+            + "      parent.insertBefore(input, after);\n"
+            + "      alert(input.checked);\n"
+            + "      parent.removeChild(input);\n"
+            + "      alert(input.checked);\n"
+            + "\n"
+            + "      input.defaultChecked = true;\n"
+            + "      alert(input.checked);\n"
+            + "      parent.insertBefore(input, after);\n"
+            + "      alert(input.checked);\n"
+            + "      parent.removeChild(input);\n"
+            + "      alert(input.checked);\n"
+            + "    }\n"
+            + "  </script>\n"
+            + "</head><body onload='test()'>\n"
+            + "  <form><div id='myDiv'><div id='divAfter'></div></div></form>\n"
+            + "</body></html>";
+
+        loadPageWithAlerts2(html);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts(DEFAULT = { "true", "true", "true", "true", "true", "true" })
+    public void checked_insertBefore_fromHtml() throws Exception {
+        final String html = "<html>\n"
+            + "<head>\n"
+            + "  <script>\n"
+            + "    function test() {\n"
+            + "      var builder = document.createElement('div');\n"
+            + "      builder.innerHTML = '<input type=\"radio\" checked>';\n"
+            + "      var input = builder.firstChild;\n"
+            + "      alert(input.checked);\n"
+            + "      var parent=document.getElementById('myDiv');\n"
+            + "      var after=document.getElementById('divAfter');\n"
+            + "      parent.insertBefore(input, after);\n"
+            + "      alert(input.checked);\n"
+            + "      parent.removeChild(input);\n"
+            + "      alert(input.checked);\n"
+            + "\n"
+            + "      input.defaultChecked = true;\n"
+            + "      alert(input.checked);\n"
+            + "      parent.insertBefore(input, after);\n"
+            + "      alert(input.checked);\n"
+            + "      parent.removeChild(input);\n"
+            + "      alert(input.checked);\n"
+            + "    }\n"
+            + "  </script>\n"
+            + "</head><body onload='test()'>\n"
+            + "  <form><div id='myDiv'><div id='divAfter'></div></div></form>\n"
+            + "</body></html>";
+
+        loadPageWithAlerts2(html);
+    }
+
+    /**
      * Regression test for bug 2956588.
      * As of HttmlUnit-2.8-SNAPSHOT on 26.02.10, reading responseXML with xhtml namespace
      * was causing ClassCastException for IE simulation when it contained a checked radio button.
