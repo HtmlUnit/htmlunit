cf4320a7ba5ea52ec77f5691433eb6fbffa76b23
asashour <asashour@yahoo.com>
2014-01-20 11:55:43 +0000

    Testing new HttpWebConnection on the server, as local tests aren't consistent. To be revereted back.
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@9001 5f5364db-9458-4db8-a492-e30667be6df6

--- src/main/java/com/gargoylesoftware/htmlunit/SocksSocketFactory.java
+++ src/main/java/com/gargoylesoftware/htmlunit/SocksSocketFactory.java
@@ -21,6 +21,7 @@ import java.net.Socket;
 import org.apache.http.HttpHost;
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HttpContext;
 
 /**
  * SOCKS aware {@link org.apache.http.conn.scheme.SchemeSocketFactory}.
@@ -39,6 +40,10 @@ class SocksSocketFactory extends PlainSocketFactory {
 
     static HttpHost getSocksProxy(final HttpParams parameters) {
         return (HttpHost) parameters.getParameter(SOCKS_PROXY);
+    }
+
+    static HttpHost getSocksProxy(final HttpContext context) {
+        return (HttpHost) context.getAttribute(SOCKS_PROXY);
     }
 
     static Socket createSocketWithSocksProxy(final HttpHost socksProxy) {
--- src/test/java/com/gargoylesoftware/htmlunit/HttpWebConnectionTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/HttpWebConnectionTest.java
@@ -42,8 +42,7 @@ import org.apache.http.ProtocolVersion;
 import org.apache.http.StatusLine;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.impl.client.AbstractHttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicHttpResponse;
 import org.apache.http.message.BasicStatusLine;
 import org.apache.log4j.Level;
@@ -244,9 +243,9 @@ public class HttpWebConnectionTest extends WebServerTestCase {
         final boolean[] tabCalled = {false};
         final WebConnection myWebConnection = new HttpWebConnection(webClient) {
             @Override
-            protected AbstractHttpClient createHttpClient() {
+            protected HttpClientBuilder createHttpClient() {
                 tabCalled[0] = true;
-                return new DefaultHttpClient();
+                return HttpClientBuilder.create();
             }
         };
 
