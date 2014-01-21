63b8d0a2aa4645d650fd11605aef027a9061b9d7
rbri <rbri@rbri.de>
2014-01-21 19:16:54 +0000

    fix expectations, webdriver is buggy
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@9028 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/libraries/Prototype161Test.java
+++ src/test/java/com/gargoylesoftware/htmlunit/libraries/Prototype161Test.java
@@ -23,6 +23,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.gargoylesoftware.htmlunit.BrowserRunner;
+import com.gargoylesoftware.htmlunit.BrowserRunner.BuggyWebDriver;
 import com.gargoylesoftware.htmlunit.BrowserRunner.NotYetImplemented;
 import com.gargoylesoftware.htmlunit.WebServerTestCase;
 
@@ -139,7 +140,8 @@ public class Prototype161Test extends PrototypeTestBase {
      * @throws Exception if test fails
      */
     @Test
-    @NotYetImplemented({ FF, IE11, CHROME })
+    @NotYetImplemented({ IE11, CHROME })
+    @BuggyWebDriver(FF)
     public void form() throws Exception {
         test("form.html");
     }
