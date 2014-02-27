0ca78b2450cb9645b39183f2d2aefb0a988f1e04
rbri <rbri@rbri.de>
2014-02-27 22:45:33 +0000

    missing commit
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@9151 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/KeyboardEventTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/KeyboardEventTest.java
@@ -252,10 +252,14 @@ public class KeyboardEventTest extends WebDriverTestCase {
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts({ "97", "98", "99",
+    @Alerts(DEFAULT = { "97", "98", "99",
             "100", "101", "102", "103", "104", "105", "106", "107", "108", "109",
             "110", "111", "112", "113", "114", "115", "116", "117", "118", "119",
-            "120", "121", "122" })
+            "120", "121", "122" },
+            IE8 = { "undefined", "undefined", "undefined", "undefined", "undefined", "undefined",
+            "undefined", "undefined", "undefined", "undefined", "undefined", "undefined", "undefined",
+            "undefined", "undefined", "undefined", "undefined", "undefined", "undefined", "undefined",
+            "undefined", "undefined", "undefined", "undefined", "undefined", "undefined" })
     public void keyCodes2_keypress() throws Exception {
         final String html = "<html><head>"
             + "<script>"
