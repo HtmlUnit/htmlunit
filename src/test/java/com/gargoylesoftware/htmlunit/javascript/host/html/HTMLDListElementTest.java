ad18373af312e1f9507a8a577178d4d8af1f17e9
rbri <rbri@rbri.de>
2014-03-07 19:05:20 +0000

    source cleanup and some testcases added (nyi)
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@9159 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLDListElementTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLDListElementTest.java
@@ -14,12 +14,20 @@
  */
 package com.gargoylesoftware.htmlunit.javascript.host.html;
 
+import static com.gargoylesoftware.htmlunit.BrowserRunner.Browser.FF;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import com.gargoylesoftware.htmlunit.BrowserRunner;
 import com.gargoylesoftware.htmlunit.BrowserRunner.Alerts;
+import com.gargoylesoftware.htmlunit.BrowserRunner.NotYetImplemented;
 import com.gargoylesoftware.htmlunit.WebDriverTestCase;
+import com.gargoylesoftware.htmlunit.html.HtmlDefinitionList;
 
 /**
  * Unit tests for {@link HTMLDListElement}.
@@ -27,9 +35,34 @@ import com.gargoylesoftware.htmlunit.WebDriverTestCase;
  * @version $Revision$
  * @author Daniel Gredler
  * @author Frank Danek
+ * @author Ronald Brill
  */
 @RunWith(BrowserRunner.class)
 public class HTMLDListElementTest extends WebDriverTestCase {
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts(DEFAULT = "[object HTMLDListElement]",
+            IE8 = "[object]")
+    public void simpleScriptable() throws Exception {
+        final String html = "<html><head>\n"
+            + "<script>\n"
+            + "  function test() {\n"
+            + "    alert(document.getElementById('myId'));\n"
+            + "  }\n"
+            + "</script>\n"
+            + "</head><body onload='test()'>\n"
+            + "  <dl id='myId'/>\n"
+            + "</body></html>";
+
+        final WebDriver driver = loadPageWithAlerts2(html);
+        if (driver instanceof HtmlUnitDriver) {
+            final WebElement element = driver.findElement(By.id("myId"));
+            assertTrue(toHtmlElement(element) instanceof HtmlDefinitionList);
+        }
+    }
 
     /**
      * @throws Exception if an error occurs
@@ -40,35 +73,96 @@ public class HTMLDListElementTest extends WebDriverTestCase {
             IE8 = { "false", "true", "true", "true", "false", "true", "true", "true",
                         "true", "false", "true", "false", "true", "false", "true", "false" })
     public void compact() throws Exception {
-        final String html = "<html><body>\n"
-            + "<dl id='dl1'><dt>a</dt><dd>b</dd></dl>\n"
-            + "<dl compact='' id='dl2'><dt>a</dt><dd>b</dd></dl>\n"
-            + "<dl compact='blah' id='dl3'><dt>a</dt><dd>b</dd></dl>\n"
-            + "<dl compact='2' id='dl4'><dt>a</dt><dd>b</dd></dl>\n"
-            + "<script>\n"
-            + "alert(document.getElementById('dl1').compact);\n"
-            + "alert(document.getElementById('dl2').compact);\n"
-            + "alert(document.getElementById('dl3').compact);\n"
-            + "alert(document.getElementById('dl4').compact);\n"
-            + "alert(document.getElementById('dl1').getAttribute('compact'));\n"
-            + "alert(document.getElementById('dl2').getAttribute('compact'));\n"
-            + "alert(document.getElementById('dl3').getAttribute('compact'));\n"
-            + "alert(document.getElementById('dl4').getAttribute('compact'));\n"
-            + "document.getElementById('dl1').compact = true;\n"
-            + "document.getElementById('dl2').compact = false;\n"
-            + "document.getElementById('dl3').compact = 'xyz';\n"
-            + "document.getElementById('dl4').compact = null;\n"
-            + "alert(document.getElementById('dl1').compact);\n"
-            + "alert(document.getElementById('dl2').compact);\n"
-            + "alert(document.getElementById('dl3').compact);\n"
-            + "alert(document.getElementById('dl4').compact);\n"
-            + "alert(document.getElementById('dl1').getAttribute('compact'));\n"
-            + "alert(document.getElementById('dl2').getAttribute('compact'));\n"
-            + "alert(document.getElementById('dl3').getAttribute('compact'));\n"
-            + "alert(document.getElementById('dl4').getAttribute('compact'));\n"
-            + "</script>\n"
-            + "</body></html>";
+        final String html =
+                "<html>\n"
+                + "  <head>\n"
+                + "    <script>\n"
+                + "      function test() {\n"
+                + "        alert(document.getElementById('dl1').compact);\n"
+                + "        alert(document.getElementById('dl2').compact);\n"
+                + "        alert(document.getElementById('dl3').compact);\n"
+                + "        alert(document.getElementById('dl4').compact);\n"
+                + "        alert(document.getElementById('dl1').getAttribute('compact'));\n"
+                + "        alert(document.getElementById('dl2').getAttribute('compact'));\n"
+                + "        alert(document.getElementById('dl3').getAttribute('compact'));\n"
+                + "        alert(document.getElementById('dl4').getAttribute('compact'));\n"
+
+                + "        document.getElementById('dl1').compact = true;\n"
+                + "        document.getElementById('dl2').compact = false;\n"
+                + "        document.getElementById('dl3').compact = 'xyz';\n"
+                + "        document.getElementById('dl4').compact = null;\n"
+                + "        alert(document.getElementById('dl1').compact);\n"
+                + "        alert(document.getElementById('dl2').compact);\n"
+                + "        alert(document.getElementById('dl3').compact);\n"
+                + "        alert(document.getElementById('dl4').compact);\n"
+                + "        alert(document.getElementById('dl1').getAttribute('compact'));\n"
+                + "        alert(document.getElementById('dl2').getAttribute('compact'));\n"
+                + "        alert(document.getElementById('dl3').getAttribute('compact'));\n"
+                + "        alert(document.getElementById('dl4').getAttribute('compact'));\n"
+                + "      }\n"
+                + "    </script>\n"
+                + "  </head>\n"
+                + "  <body onload='test()'>\n"
+                + "    <dl id='dl1'><dt>a</dt><dd>b</dd></dl>\n"
+                + "    <dl compact='' id='dl2'><dt>a</dt><dd>b</dd></dl>\n"
+                + "    <dl compact='blah' id='dl3'><dt>a</dt><dd>b</dd></dl>\n"
+                + "    <dl compact='2' id='dl4'><dt>a</dt><dd>b</dd></dl>\n"
+                + "  </body>\n"
+                + "</html>";
         loadPageWithAlerts2(html);
     }
 
+    /**
+     * @throws Exception if an error occurs
+     */
+    @Test
+    @Alerts(DEFAULT = { "undefined", "undefined", "undefined", "undefined",
+                        "null", "", "blah", "A", "1", "a", "A", "i", "I", "u" },
+            IE8 = { "undefined", "", "blah", "A",
+                    "null", "", "blah", "A", "1", "a", "A", "i", "I", "u" })
+    @NotYetImplemented(FF)
+    public void type() throws Exception {
+        final String html =
+                "<html>\n"
+                + "  <head>\n"
+                + "    <script>\n"
+                + "      function test() {\n"
+                + "        alert(document.getElementById('dl1').type);\n"
+                + "        alert(document.getElementById('dl2').type);\n"
+                + "        alert(document.getElementById('dl3').type);\n"
+                + "        alert(document.getElementById('dl4').type);\n"
+                + "        alert(document.getElementById('dl1').getAttribute('type'));\n"
+                + "        alert(document.getElementById('dl2').getAttribute('type'));\n"
+                + "        alert(document.getElementById('dl3').getAttribute('type'));\n"
+                + "        alert(document.getElementById('dl4').getAttribute('type'));\n"
+
+                + "        document.getElementById('dl1').type = '1';\n"
+                + "        alert(document.getElementById('dl1').type);\n"
+
+                + "        document.getElementById('dl1').type = 'a';\n"
+                + "        alert(document.getElementById('dl1').type);\n"
+
+                + "        document.getElementById('dl1').type = 'A';\n"
+                + "        alert(document.getElementById('dl1').type);\n"
+
+                + "        document.getElementById('dl1').type = 'i';\n"
+                + "        alert(document.getElementById('dl1').type);\n"
+
+                + "        document.getElementById('dl1').type = 'I';\n"
+                + "        alert(document.getElementById('dl1').type);\n"
+
+                + "        try { document.getElementById('dl1').type = 'u' } catch(e) {alert('exception');};\n"
+                + "        alert(document.getElementById('dl1').type);\n"
+                + "      }\n"
+                + "    </script>\n"
+                + "  </head>\n"
+                + "  <body onload='test()'>\n"
+                + "    <dl id='dl1'><dt>a</dt><dd>b</dd></dl>\n"
+                + "    <dl type='' id='dl2'><dt>a</dt><dd>b</dd></dl>\n"
+                + "    <dl type='blah' id='dl3'><dt>a</dt><dd>b</dd></dl>\n"
+                + "    <dl type='A' id='dl4'><dt>a</dt><dd>b</dd></dl>\n"
+                + "  </body>\n"
+                + "</html>";
+        loadPageWithAlerts2(html);
+    }
 }
--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLDirectoryElementTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLDirectoryElementTest.java
@@ -16,10 +16,16 @@ package com.gargoylesoftware.htmlunit.javascript.host.html;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import com.gargoylesoftware.htmlunit.BrowserRunner;
 import com.gargoylesoftware.htmlunit.BrowserRunner.Alerts;
+import com.gargoylesoftware.htmlunit.BrowserRunner.NotYetImplemented;
 import com.gargoylesoftware.htmlunit.WebDriverTestCase;
+import com.gargoylesoftware.htmlunit.html.HtmlDirectory;
 
 /**
  * Unit tests for {@link HTMLDirectoryElement}.
@@ -27,9 +33,34 @@ import com.gargoylesoftware.htmlunit.WebDriverTestCase;
  * @version $Revision$
  * @author Daniel Gredler
  * @author Frank Danek
+ * @author Ronald Brill
  */
 @RunWith(BrowserRunner.class)
 public class HTMLDirectoryElementTest extends WebDriverTestCase {
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts(DEFAULT = "[object HTMLDirectoryElement]",
+            IE8 = "[object]")
+    public void simpleScriptable() throws Exception {
+        final String html = "<html><head>\n"
+            + "<script>\n"
+            + "  function test() {\n"
+            + "    alert(document.getElementById('myId'));\n"
+            + "  }\n"
+            + "</script>\n"
+            + "</head><body onload='test()'>\n"
+            + "  <dir id='myId'/>\n"
+            + "</body></html>";
+
+        final WebDriver driver = loadPageWithAlerts2(html);
+        if (driver instanceof HtmlUnitDriver) {
+            final WebElement element = driver.findElement(By.id("myId"));
+            assertTrue(toHtmlElement(element) instanceof HtmlDirectory);
+        }
+    }
 
     /**
      * @throws Exception if an error occurs
@@ -40,35 +71,96 @@ public class HTMLDirectoryElementTest extends WebDriverTestCase {
             IE8 = { "false", "true", "true", "true", "false", "true", "true", "true",
                 "true", "false", "true", "false", "true", "false", "true", "false" })
     public void compact() throws Exception {
-        final String html = "<html><body>\n"
-            + "<dir id='dir1'><li>a</li><li>b</li></dir>\n"
-            + "<dir compact='' id='dir2'><li>a</li><li>b</li></dir>\n"
-            + "<dir compact='blah' id='dir3'><li>a</li><li>b</li></dir>\n"
-            + "<dir compact='2' id='dir4'><li>a</li><li>b</li></dir>\n"
-            + "<script>\n"
-            + "alert(document.getElementById('dir1').compact);\n"
-            + "alert(document.getElementById('dir2').compact);\n"
-            + "alert(document.getElementById('dir3').compact);\n"
-            + "alert(document.getElementById('dir4').compact);\n"
-            + "alert(document.getElementById('dir1').getAttribute('compact'));\n"
-            + "alert(document.getElementById('dir2').getAttribute('compact'));\n"
-            + "alert(document.getElementById('dir3').getAttribute('compact'));\n"
-            + "alert(document.getElementById('dir4').getAttribute('compact'));\n"
-            + "document.getElementById('dir1').compact = true;\n"
-            + "document.getElementById('dir2').compact = false;\n"
-            + "document.getElementById('dir3').compact = 'xyz';\n"
-            + "document.getElementById('dir4').compact = null;\n"
-            + "alert(document.getElementById('dir1').compact);\n"
-            + "alert(document.getElementById('dir2').compact);\n"
-            + "alert(document.getElementById('dir3').compact);\n"
-            + "alert(document.getElementById('dir4').compact);\n"
-            + "alert(document.getElementById('dir1').getAttribute('compact'));\n"
-            + "alert(document.getElementById('dir2').getAttribute('compact'));\n"
-            + "alert(document.getElementById('dir3').getAttribute('compact'));\n"
-            + "alert(document.getElementById('dir4').getAttribute('compact'));\n"
-            + "</script>\n"
-            + "</body></html>";
+        final String html =
+                "<html>\n"
+                + "  <head>\n"
+                + "    <script>\n"
+                + "      function test() {\n"
+                + "        alert(document.getElementById('dir1').compact);\n"
+                + "        alert(document.getElementById('dir2').compact);\n"
+                + "        alert(document.getElementById('dir3').compact);\n"
+                + "        alert(document.getElementById('dir4').compact);\n"
+                + "        alert(document.getElementById('dir1').getAttribute('compact'));\n"
+                + "        alert(document.getElementById('dir2').getAttribute('compact'));\n"
+                + "        alert(document.getElementById('dir3').getAttribute('compact'));\n"
+                + "        alert(document.getElementById('dir4').getAttribute('compact'));\n"
+
+                + "        document.getElementById('dir1').compact = true;\n"
+                + "        document.getElementById('dir2').compact = false;\n"
+                + "        document.getElementById('dir3').compact = 'xyz';\n"
+                + "        document.getElementById('dir4').compact = null;\n"
+                + "        alert(document.getElementById('dir1').compact);\n"
+                + "        alert(document.getElementById('dir2').compact);\n"
+                + "        alert(document.getElementById('dir3').compact);\n"
+                + "        alert(document.getElementById('dir4').compact);\n"
+                + "        alert(document.getElementById('dir1').getAttribute('compact'));\n"
+                + "        alert(document.getElementById('dir2').getAttribute('compact'));\n"
+                + "        alert(document.getElementById('dir3').getAttribute('compact'));\n"
+                + "        alert(document.getElementById('dir4').getAttribute('compact'));\n"
+                + "      }\n"
+                + "    </script>\n"
+                + "  </head>\n"
+                + "  <body onload='test()'>\n"
+                + "    <dir id='dir1'><li>a</li><li>b</li></dir>\n"
+                + "    <dir compact='' id='dir2'><li>a</li><li>b</li></dir>\n"
+                + "    <dir compact='blah' id='dir3'><li>a</li><li>b</li></dir>\n"
+                + "    <dir compact='2' id='dir4'><li>a</li><li>b</li></dir>\n"
+                + "  </body>\n"
+                + "</html>";
         loadPageWithAlerts2(html);
     }
 
+    /**
+     * @throws Exception if an error occurs
+     */
+    @Test
+    @Alerts(DEFAULT = { "undefined", "undefined", "undefined", "undefined",
+                        "null", "", "blah", "A", "1", "a", "A", "i", "I", "u" },
+            IE8 = { "", "", "", "A", "", "", "blah", "A", "1", "a", "A", "i", "I", "exception", "I" },
+            IE11 = { "", "", "", "A", "null", "", "blah", "A", "1", "a", "A", "i", "I", "exception", "I" })
+    @NotYetImplemented
+    public void type() throws Exception {
+        final String html =
+                "<html>\n"
+                + "  <head>\n"
+                + "    <script>\n"
+                + "      function test() {\n"
+                + "        alert(document.getElementById('dir1').type);\n"
+                + "        alert(document.getElementById('dir2').type);\n"
+                + "        alert(document.getElementById('dir3').type);\n"
+                + "        alert(document.getElementById('dir4').type);\n"
+                + "        alert(document.getElementById('dir1').getAttribute('type'));\n"
+                + "        alert(document.getElementById('dir2').getAttribute('type'));\n"
+                + "        alert(document.getElementById('dir3').getAttribute('type'));\n"
+                + "        alert(document.getElementById('dir4').getAttribute('type'));\n"
+
+                + "        document.getElementById('dir1').type = '1';\n"
+                + "        alert(document.getElementById('dir1').type);\n"
+
+                + "        document.getElementById('dir1').type = 'a';\n"
+                + "        alert(document.getElementById('dir1').type);\n"
+
+                + "        document.getElementById('dir1').type = 'A';\n"
+                + "        alert(document.getElementById('dir1').type);\n"
+
+                + "        document.getElementById('dir1').type = 'i';\n"
+                + "        alert(document.getElementById('dir1').type);\n"
+
+                + "        document.getElementById('dir1').type = 'I';\n"
+                + "        alert(document.getElementById('dir1').type);\n"
+
+                + "        try { document.getElementById('dir1').type = 'u' } catch(e) {alert('exception');};\n"
+                + "        alert(document.getElementById('dir1').type);\n"
+                + "      }\n"
+                + "    </script>\n"
+                + "  </head>\n"
+                + "  <body onload='test()'>\n"
+                + "    <dir id='dir1'><li>a</li><li>b</li></dir>\n"
+                + "    <dir type='' id='dir2'><li>a</li><li>b</li></dir>\n"
+                + "    <dir type='blah' id='dir3'><li>a</li><li>b</li></dir>\n"
+                + "    <dir type='A' id='dir4'><li>a</li><li>b</li></dir>\n"
+                + "  </body>\n"
+                + "</html>";
+        loadPageWithAlerts2(html);
+    }
 }
