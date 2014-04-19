bc75d612d1b7887e406837af6a289642b824dff0
rbri <rbri@rbri.de>
2014-04-19 18:34:12 +0000

    IE11 progress
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@9288 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/html/HtmlIsIndex2Test.java
+++ src/test/java/com/gargoylesoftware/htmlunit/html/HtmlIsIndex2Test.java
@@ -16,6 +16,7 @@ package com.gargoylesoftware.htmlunit.html;
 
 import static com.gargoylesoftware.htmlunit.BrowserRunner.Browser.FF17;
 import static com.gargoylesoftware.htmlunit.BrowserRunner.Browser.FF24;
+import static com.gargoylesoftware.htmlunit.BrowserRunner.Browser.IE11;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -45,7 +46,7 @@ public class HtmlIsIndex2Test extends WebDriverTestCase {
     @Test
     @Alerts(DEFAULT = "null",
             IE8 = "[object]")
-    @NotYetImplemented({ FF17, FF24 })
+    @NotYetImplemented({ FF17, FF24, IE11 })
     public void simpleScriptable() throws Exception {
         final String html = "<html><head>\n"
             + "<script>\n"
