85d428792ab836b2bf46cabe719cb8f395bf2926
asashour <asashour@yahoo.com>
2014-09-24 07:24:25 +0000

    cruisecontrol: maven 3.2.3
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@9690 5f5364db-9458-4db8-a492-e30667be6df6

--- cruise/mavenrunner.sh
+++ cruise/mavenrunner.sh
@@ -2,6 +2,6 @@
 
 export JAVA_HOME=/usr/java/jdk1.6.0_45
 
-export M2_HOME=/opt/cruisecontrol/maven/apache-maven-3.1.1
+export M2_HOME=/opt/cruisecontrol/maven/apache-maven-3.2.3
 
 $M2_HOME/bin/mvn $*
