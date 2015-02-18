d06af72a8a99a76a34e7d433c609ca3feeb628af
rbri <rbri@rbri.de>
2015-02-18 17:39:55 +0000

    more cookie mgnt tests; ignored at the moment because they require a special setup (wip)
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@9991 5f5364db-9458-4db8-a492-e30667be6df6

---
+++ src/test/java/com/gargoylesoftware/htmlunit/CookieManager4Test.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2002-2015 Gargoyle Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.gargoylesoftware.htmlunit;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.gargoylesoftware.htmlunit.BrowserRunner.Alerts;
+import com.gargoylesoftware.htmlunit.util.NameValuePair;
+
+/**
+ * Unit tests for {@link CookieManager}.
+ * These test requirering a special setup in you etc/hosts
+ * 127.0.0.1       host1.htmlunit.org
+ * 127.0.0.1       host2.htmlunit.org
+ *
+ * @version $Revision$
+ * @author Ronald Brill
+ */
+@RunWith(BrowserRunner.class)
+@Ignore("Requires a special configuration; see class comment for more")
+public class CookieManager4Test extends WebDriverTestCase {
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts(DEFAULT = "cross-domain=1")
+    public void differentHostsSameDomain() throws Exception {
+        final List<NameValuePair> responseHeader1 = new ArrayList<>();
+        responseHeader1.add(new NameValuePair("Set-Cookie", "cross-domain=1; Domain=.htmlunit.org; Path=/"));
+
+        final String html = "<html>\n"
+            + "<head></head>\n"
+            + "<body>\n"
+            + "<p>Cookie Domain Test</p>\n"
+            + "<script>\n"
+            + "  location.replace('http://host2.htmlunit.org:" + PORT + "/" + "');\n"
+            + "</script>\n"
+            + "</body>\n"
+            + "</html>";
+
+        getMockWebConnection().setDefaultResponse(CookieManagerTest.HTML_ALERT_COOKIE);
+        final URL firstUrl = new URL("http://host1.htmlunit.org:" + PORT + "/");
+        getMockWebConnection().setResponse(firstUrl, html, 200, "Ok", "text/html", responseHeader1);
+
+        loadPageWithAlerts2(firstUrl);
+    }
+}
