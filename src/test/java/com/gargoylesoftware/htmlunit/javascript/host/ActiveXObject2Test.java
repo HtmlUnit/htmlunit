de5f6459cb7b9765824d83ac27c1eee1631acfe3
rbri <rbri@rbri.de>
2015-03-10 19:37:59 +0000

    one more ActiveX test added
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@10146 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/ActiveXObject2Test.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/ActiveXObject2Test.java
@@ -37,9 +37,11 @@ public class ActiveXObject2Test extends WebDriverTestCase {
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts(DEFAULT = { "undefined", "undefined", "NaN", "false", "No", "No" },
-            IE11 = {"undefined", "\nfunction ActiveXObject() {\n    [native code]\n}\n", "NaN", "false", "No", "No" },
-            IE8 = {"function", "\nfunction ActiveXObject() {\n    [native code]\n}\n", "NaN", "true", "Yes", "Yes" })
+    @Alerts(DEFAULT = { "undefined", "undefined", "NaN", "false", "No", "No", "No", "No" },
+            IE11 = {"undefined", "\nfunction ActiveXObject() {\n    [native code]\n}\n",
+                    "NaN", "false", "No", "No", "Yes", "Yes" },
+            IE8 = {"function", "\nfunction ActiveXObject() {\n    [native code]\n}\n",
+                    "NaN", "true", "Yes", "Yes", "Yes", "Yes" })
     public void browserDetection() throws Exception {
         final String html = "<html>\n"
             + "<head><title>foo</title>\n"
@@ -51,6 +53,8 @@ public class ActiveXObject2Test extends WebDriverTestCase {
             + "    alert(Boolean(window.ActiveXObject));\n"
             + "    alert(window.ActiveXObject ? 'Yes' : 'No');\n"
             + "    if (window.ActiveXObject) { alert('Yes') } else { alert('No') }\n"
+            + "    alert(('ActiveXObject' in window) ? 'Yes' : 'No');\n"
+            + "    if ('ActiveXObject' in window) { alert('Yes') } else { alert('No') }\n"
             + "  }\n"
             + "</script>\n"
             + "</head>\n"
