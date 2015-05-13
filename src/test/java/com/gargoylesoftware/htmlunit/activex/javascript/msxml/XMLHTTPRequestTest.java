d8b0ac2dfd79c6edbf4742f2e1abd3b9d9b0990e
asashour <asashour@yahoo.com>
2015-05-13 09:35:49 +0000

    Fixing build, the real expectations of IE11 needs verification.
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@10454 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/activex/javascript/msxml/XMLHTTPRequestTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/activex/javascript/msxml/XMLHTTPRequestTest.java
@@ -627,7 +627,7 @@ public class XMLHTTPRequestTest extends WebDriverTestCase {
             IE = { "exception-created",
                    "exception-opened",
                    "Date XYZ GMT\r\n"
-                   + "Content-Type: text/xml; charset=ISO-8859-1\r\n"
+                   + "Content-Type: text/xml;charset=iso-8859-1\r\n"
                    + "Transfer-Encoding: chunked\r\nServer: Jetty(XXX)\r\n\r\n" })
     public void getAllResponseHeaders() throws Exception {
         final String test = ""
@@ -657,11 +657,11 @@ public class XMLHTTPRequestTest extends WebDriverTestCase {
     @Alerts(DEFAULT = "no ActiveX",
             IE = { "exception-created",
                    "exception-opened",
-                   "text/xml; charset=ISO-8859-1",
+                   "text/xml;charset=iso-8859-1",
                    "exception-getNull",
                    "exception-getEmpty",
                    "",
-                   "text/xml; charset=ISO-8859-1" })
+                   "text/xml;charset=iso-8859-1" })
     public void getResponseHeader() throws Exception {
         final String test = ""
             + "try {\n"
