8a16d7187a89b9beca0f6d9f9b2fac24bf717b68
asashour <asashour@yahoo.com>
2015-05-19 11:44:33 +0000

    checkstyle
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@10519 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLInputElementTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLInputElementTest.java
@@ -633,7 +633,8 @@ public class HTMLInputElementTest extends WebDriverTestCase {
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts(DEFAULT = { "undefined", "undefined", "undefined","30", "30", "30", "undefined", "40", "string", "number" },
+    @Alerts(DEFAULT = { "undefined", "undefined", "undefined", "30", "30", "30",
+                "undefined", "40", "string", "number" },
             CHROME = {"undefined", "30", "undefined", "30", "30", "30", "40", "50", "string", "number" },
             IE8 = { "30", "undefined", "undefined", "30", "30", "30", "undefined", "30", "string", "number" })
     @NotYetImplemented(IE8)
