3a824c82764f510254bd9d6109cf462cb39ca3a6
asashour <asashour@yahoo.com>
2015-05-30 08:00:13 +0000

    Initial support for HTML5 Input Types
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@10597 5f5364db-9458-4db8-a492-e30667be6df6

--- src/main/java/com/gargoylesoftware/htmlunit/html/InputElementFactory.java
+++ src/main/java/com/gargoylesoftware/htmlunit/html/InputElementFactory.java
@@ -95,47 +95,94 @@ public final class InputElementFactory implements ElementFactory {
         }
 
         final HtmlInput result;
-        if (type.isEmpty()) {
-            // This not an illegal value, as it defaults to "text"
-            // cf http://www.w3.org/TR/REC-html40/interact/forms.html#adef-type-INPUT
-            // and the common browsers seem to treat it as a "text" input so we will as well.
-            final DomAttr newAttr = new DomAttr(page, null, "type", "text", true);
-            attributeMap.put("type", newAttr);
-            result = new HtmlTextInput(qualifiedName, page, attributeMap);
-        }
-        else if ("submit".equals(type)) {
-            result = new HtmlSubmitInput(qualifiedName, page, attributeMap);
-        }
-        else if ("checkbox".equals(type)) {
-            result = new HtmlCheckBoxInput(qualifiedName, page, attributeMap);
-        }
-        else if ("radio".equals(type)) {
-            result = new HtmlRadioButtonInput(qualifiedName, page, attributeMap);
-        }
-        else if ("text".equals(type)) {
-            result = new HtmlTextInput(qualifiedName, page, attributeMap);
-        }
-        else if ("hidden".equals(type)) {
-            result = new HtmlHiddenInput(qualifiedName, page, attributeMap);
-        }
-        else if ("password".equals(type)) {
-            result = new HtmlPasswordInput(qualifiedName, page, attributeMap);
-        }
-        else if ("image".equals(type)) {
-            result = new HtmlImageInput(qualifiedName, page, attributeMap);
-        }
-        else if ("reset".equals(type)) {
-            result = new HtmlResetInput(qualifiedName, page, attributeMap);
-        }
-        else if ("button".equals(type)) {
-            result = new HtmlButtonInput(qualifiedName, page, attributeMap);
-        }
-        else if ("file".equals(type)) {
-            result = new HtmlFileInput(qualifiedName, page, attributeMap);
-        }
-        else {
-            LOG.info("Bad input type: \"" + type + "\", creating a text input");
-            result = new HtmlTextInput(qualifiedName, page, attributeMap);
+        switch (type) {
+            case "":
+                // This not an illegal value, as it defaults to "text"
+                // cf http://www.w3.org/TR/REC-html40/interact/forms.html#adef-type-INPUT
+                // and the common browsers seem to treat it as a "text" input so we will as well.
+                final DomAttr newAttr = new DomAttr(page, null, "type", "text", true);
+                attributeMap.put("type", newAttr);
+
+            case "text":
+                result = new HtmlTextInput(qualifiedName, page, attributeMap);
+                break;
+
+            case "submit":
+                result = new HtmlSubmitInput(qualifiedName, page, attributeMap);
+                break;
+
+            case "checkbox":
+                result = new HtmlCheckBoxInput(qualifiedName, page, attributeMap);
+                break;
+
+            case "radio":
+                result = new HtmlRadioButtonInput(qualifiedName, page, attributeMap);
+                break;
+
+            case "hidden":
+                result = new HtmlHiddenInput(qualifiedName, page, attributeMap);
+                break;
+
+            case "password":
+                result = new HtmlPasswordInput(qualifiedName, page, attributeMap);
+                break;
+
+            case "image":
+                result = new HtmlImageInput(qualifiedName, page, attributeMap);
+                break;
+
+            case "reset":
+                result = new HtmlResetInput(qualifiedName, page, attributeMap);
+                break;
+
+            case "button":
+                result = new HtmlButtonInput(qualifiedName, page, attributeMap);
+                break;
+
+            case "file":
+                result = new HtmlFileInput(qualifiedName, page, attributeMap);
+                break;
+
+            case "color":
+                result = new HtmlColorInput(qualifiedName, page, attributeMap);
+                break;
+
+            case "date":
+                result = new HtmlDateInput(qualifiedName, page, attributeMap);
+                break;
+
+            case "datetime-local":
+                result = new HtmlDateTimeLocalInput(qualifiedName, page, attributeMap);
+                break;
+
+            case "month":
+                result = new HtmlMonthInput(qualifiedName, page, attributeMap);
+                break;
+
+            case "number":
+                result = new HtmlNumberInput(qualifiedName, page, attributeMap);
+                break;
+
+            case "range":
+                result = new HtmlRangeInput(qualifiedName, page, attributeMap);
+                break;
+
+            case "search":
+                result = new HtmlSearchInput(qualifiedName, page, attributeMap);
+                break;
+
+            case "time":
+                result = new HtmlTimeInput(qualifiedName, page, attributeMap);
+                break;
+
+            case "week":
+                result = new HtmlWeekInput(qualifiedName, page, attributeMap);
+                break;
+
+            default:
+                LOG.info("Bad input type: \"" + type + "\", creating a text input");
+                result = new HtmlTextInput(qualifiedName, page, attributeMap);
+                break;
         }
         return result;
     }
