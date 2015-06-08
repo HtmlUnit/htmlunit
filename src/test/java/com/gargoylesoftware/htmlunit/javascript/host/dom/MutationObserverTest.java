053a3a3db3244b70bbf1321a6bc4328b4d64bcdd
asashour <asashour@yahoo.com>
2015-06-08 00:53:12 +0000

    Add MutationObserverTest.characterData() as @NYI Issue 1691
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@10649 5f5364db-9458-4db8-a492-e30667be6df6

---
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/dom/MutationObserverTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2002-2015 Gargoyle Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.gargoylesoftware.htmlunit.javascript.host.dom;
+
+import static com.gargoylesoftware.htmlunit.BrowserRunner.Browser.CHROME;
+import static com.gargoylesoftware.htmlunit.BrowserRunner.Browser.FF;
+import static com.gargoylesoftware.htmlunit.BrowserRunner.Browser.IE11;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.gargoylesoftware.htmlunit.BrowserRunner;
+import com.gargoylesoftware.htmlunit.BrowserRunner.Alerts;
+import com.gargoylesoftware.htmlunit.BrowserRunner.NotYetImplemented;
+import com.gargoylesoftware.htmlunit.WebDriverTestCase;
+
+/**
+ * Tests for {@link MutationObserver}.
+ *
+ * @version $Revision$
+ * @author Ahmed Ashour
+ */
+@RunWith(BrowserRunner.class)
+public class MutationObserverTest extends WebDriverTestCase {
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts(DEFAULT = "old, new",
+            IE8 = "")
+    @NotYetImplemented({ CHROME, FF, IE11 })
+    public void characterData() throws Exception {
+        final String html
+            = "<html><head><script>\n"
+            + "function test(){\n"
+            + "    var div = document.getElementById('myDiv');\n"
+            + "    if (!window.MutationObserver) return;\n"
+            + "    var observer = new MutationObserver(function(mutations) {\n"
+            + "      mutations.forEach(function(mutation) {\n"
+            + "        alert(mutation.oldValue);\n"
+            + "        alert(mutation.target.textContent);\n"
+            + "      });\n"
+            + "    });\n"
+            + "\n"
+            + "    observer.observe(div, {\n"
+            + "      attributes: true,\n"
+            + "      childList: true,\n"
+            + "      characterData: true,\n"
+            + "      characterDataOldValue: true,\n"
+            + "      subtree: true\n"
+            + "    });\n"
+            + "\n"
+            + "    div.firstChild.textContent = 'new';\n"
+            + "}\n"
+            + "</script></head>"
+            + "<body onload='test()'>\n"
+            + "  <div id='myDiv'>old</div>\n"
+            + "</body></html>";
+        loadPageWithAlerts2(html);
+    }
+
+}
