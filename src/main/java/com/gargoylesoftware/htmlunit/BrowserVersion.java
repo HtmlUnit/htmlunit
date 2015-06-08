6e825b242c07df1418e904cc150d503306dd3f5b
rbri <rbri@rbri.de>
2015-06-08 15:54:51 +0000

    retested with latest ff38
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@10655 5f5364db-9458-4db8-a492-e30667be6df6

--- src/main/java/com/gargoylesoftware/htmlunit/BrowserVersion.java
+++ src/main/java/com/gargoylesoftware/htmlunit/BrowserVersion.java
@@ -197,7 +197,7 @@ public class BrowserVersion implements Serializable, Cloneable {
 
         FIREFOX_38.setBrowserLanguage("en-US");
         FIREFOX_38.setVendor("");
-        FIREFOX_38.buildId_ = "20150505103531";
+        FIREFOX_38.buildId_ = "20150514102509";
         FIREFOX_38.setHeaderNamesOrdered(new String[] {
             "Host", "User-Agent", "Accept", "Accept-Language", "Accept-Encoding", "Referer", "Cookie", "Connection" });
         FIREFOX_38.setHtmlAcceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
