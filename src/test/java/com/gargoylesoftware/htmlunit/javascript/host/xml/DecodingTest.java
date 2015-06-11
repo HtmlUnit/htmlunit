3cf603b562df8b99da3b9f669e7e2e9c51d04220
asashour <asashour@yahoo.com>
2015-06-11 17:40:14 +0000

    ...
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@10698 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/xml/DecodingTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/xml/DecodingTest.java
@@ -39,8 +39,9 @@ public class DecodingTest {
      */
     @Test
     public void test() throws Exception {
+        System.out.println("test()");
         final String string = "'\u9ec4'";
-        final GBK charset = new GBK();
+        final Charset charset = new GBK();
         System.out.println(charset.getClass().getName());
         CharsetDecoder decoder = charset.newDecoder().onMalformedInput(CodingErrorAction.REPLACE)
                 .onUnmappableCharacter(CodingErrorAction.REPLACE);
@@ -73,6 +74,51 @@ public class DecodingTest {
         System.out.println("10 - " + cb.position() + " " + cb.limit());
         decoder.decode(bb, cb, false);
         System.out.println("11 - " + bb.position() + " " + bb.limit());
+        System.out.println("12 - " + cb.position() + " " + cb.limit());
+    }
+
+    /**
+     * Helper method Bug #1623.
+     *
+     * @throws Exception if the test fails
+     */
+    @Test
+    public void test2() throws Exception {
+        System.out.println("test2()");
+        final String string = "'\u9ec4'";
+        final Charset charset = Charset.forName("GBK");
+        System.out.println(charset.getClass().getName());
+        CharsetDecoder decoder = charset.newDecoder().onMalformedInput(CodingErrorAction.REPLACE)
+                .onUnmappableCharacter(CodingErrorAction.REPLACE);
+        byte[] bytes = string.getBytes("UTF-8");
+        final InputStream in = new ByteArrayInputStream(bytes);
+        ByteBuffer bb = ByteBuffer.allocate(8192);
+        bb.flip();
+
+        CharBuffer cb = CharBuffer.wrap(new char[2]);
+        System.out.println("01 - " + bb.position() + " " + bb.limit());
         System.out.println("02 - " + cb.position() + " " + cb.limit());
+        decoder.decode(bb, cb, false);
+        System.out.println("03 - " + bb.position() + " " + bb.limit());
+        System.out.println("04 - " + cb.position() + " " + cb.limit());
+
+        bb.compact();
+        
+        in.read(bb.array(), 0, 8192);
+        bb.position(5);
+        bb.flip();
+
+        System.out.println("05 - " + bb.position() + " " + bb.limit());
+        System.out.println("06 - " + cb.position() + " " + cb.limit());
+        decoder.decode(bb, cb, false);
+        System.out.println("07 - " + bb.position() + " " + bb.limit());
+        System.out.println("08 - " + cb.position() + " " + cb.limit());
+
+        cb = CharBuffer.wrap(new char[2]);
+        System.out.println("09 - " + bb.position() + " " + bb.limit());
+        System.out.println("10 - " + cb.position() + " " + cb.limit());
+        decoder.decode(bb, cb, false);
+        System.out.println("11 - " + bb.position() + " " + bb.limit());
+        System.out.println("12 - " + cb.position() + " " + cb.limit());
     }
 }
