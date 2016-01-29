061f7f4d9affad605d1a0f1f8278f50c0353cdb5
rbri <rbri@rbri.de>
2016-01-29 18:57:45 +0000

    works like real chrome now
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@11789 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/ArgumentsTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/ArgumentsTest.java
@@ -14,14 +14,11 @@
  */
 package com.gargoylesoftware.htmlunit.javascript;
 
-import static com.gargoylesoftware.htmlunit.BrowserRunner.Browser.CHROME;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.gargoylesoftware.htmlunit.BrowserRunner;
 import com.gargoylesoftware.htmlunit.BrowserRunner.Alerts;
-import com.gargoylesoftware.htmlunit.BrowserRunner.NotYetImplemented;
 import com.gargoylesoftware.htmlunit.WebDriverTestCase;
 
 /**
@@ -146,8 +143,8 @@ public class ArgumentsTest extends WebDriverTestCase {
      */
     @Test
     @Alerts(DEFAULT = "true",
+            CHROME = "false",
             FF = "false")
-    @NotYetImplemented(CHROME)
     public void argumentsEqualsFnArguments() throws Exception {
         final String html = "<html><body><script>\n"
             + "function test1() {\n"
