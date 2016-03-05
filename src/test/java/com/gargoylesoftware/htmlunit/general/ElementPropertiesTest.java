84aa0e9bf06dad43ca790307ce7b40a2042434dd
rbri <rbri@rbri.de>
2016-03-05 13:24:37 +0000

    add value property support for HTMLDataElement (FF only)
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@12016 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/general/ElementPropertiesTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/general/ElementPropertiesTest.java
@@ -2682,7 +2682,6 @@ public class ElementPropertiesTest extends WebDriverTestCase {
      */
     @Test
     @Alerts(FF = "value")
-    @NotYetImplemented(FF)
     public void data() throws Exception {
         test("data");
     }
---
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLDataElementTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2002-2016 Gargoyle Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.gargoylesoftware.htmlunit.javascript.host.html;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.gargoylesoftware.htmlunit.BrowserRunner;
+import com.gargoylesoftware.htmlunit.BrowserRunner.Alerts;
+import com.gargoylesoftware.htmlunit.WebDriverTestCase;
+
+/**
+ * Tests for {@link HTMLDataElement}.
+ * @author Ronald Brill
+ */
+@RunWith(BrowserRunner.class)
+public class HTMLDataElementTest extends WebDriverTestCase {
+
+    /**
+     * @throws Exception if an error occurs
+     */
+    @Test
+    @Alerts(DEFAULT = "[object HTMLUnknownElement]",
+            FF = "[object HTMLDataElement]")
+    public void tag() throws Exception {
+        final String html = "<html><body>\n"
+            + "  <data id='it' value='1234'>onetwothreefour</data>\n"
+            + "<script>\n"
+            + "  alert(document.getElementById('it'));\n"
+            + "</script></body></html>";
+        loadPageWithAlerts2(html);
+    }
+
+    /**
+     * @throws Exception if an error occurs
+     */
+    @Test
+    @Alerts(DEFAULT = { "undefined", "#12o", "undefined", "#12o" },
+            FF = { "1234", "#12o", "", "#12o" })
+    public void value() throws Exception {
+        final String html = "<html><body>\n"
+            + "  <data id='d1' value='1234'>onetwothreefour</data>\n"
+            + "  <data id='d2' >onetwothreefour</data>\n"
+            + "<script>\n"
+            + "  var dat = document.getElementById('d1');\n"
+            + "  alert(dat.value);\n"
+            + "  dat.value='#12o';\n"
+            + "  alert(dat.value);\n"
+
+            + "  dat = document.getElementById('d2');\n"
+            + "  alert(dat.value);\n"
+            + "  dat.value='#12o';\n"
+            + "  alert(dat.value);\n"
+            + "</script></body></html>";
+        loadPageWithAlerts2(html);
+    }
+}
