d11fb7ab924d267b0d4fca0fcd25c5df83bb3fd0
asashour <asashour@yahoo.com>
2016-03-25 15:27:13 +0000

    Fix FF45 expectations
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@12163 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/intl/DateTimeFormatTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/intl/DateTimeFormatTest.java
@@ -318,7 +318,8 @@ public class DateTimeFormatTest extends WebDriverTestCase {
      */
     @Test
     @Alerts(DEFAULT = "\u0662\u0660\u200F/\u0661\u0662\u200F/\u0662\u0660\u0661\u0662",
-            IE = "\u200F\u0660\u0667\u200F/\u200F\u0660\u0662\u200F/\u200F\u0661\u0664\u0663\u0664")
+            IE = "\u200F\u0660\u0667\u200F/\u200F\u0660\u0662\u200F/\u200F\u0661\u0664\u0663\u0664",
+            FF45 = "\u0667\u200F/\u0662\u200F/\u0661\u0664\u0663\u0664 \u0647\u0640")
     @NotYetImplemented
     public void format_ar_sa() throws Exception {
         test("new Intl.DateTimeFormat('ar-SA').format(date)");
