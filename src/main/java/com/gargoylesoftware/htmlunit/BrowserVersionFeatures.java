ba5c02808e38505e7a8e96d6ef4becc2c577caf3
rbri <rbri@rbri.de>
2016-03-29 11:38:45 +0000

    order fix
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@12218 5f5364db-9458-4db8-a492-e30667be6df6

--- src/main/java/com/gargoylesoftware/htmlunit/BrowserVersionFeatures.java
+++ src/main/java/com/gargoylesoftware/htmlunit/BrowserVersionFeatures.java
@@ -591,13 +591,13 @@ public enum BrowserVersionFeatures {
     @BrowserFeature(@WebBrowser(FF))
     JS_CSS_MOZ_CSS_KEYFRAMES_RULE,
 
-    /** <code>Date.toLocaleDateString()</code> returns a short form (d.M.yyyy). */
-    @BrowserFeature({ @WebBrowser(CHROME), @WebBrowser(FF) })
-    JS_DATE_LOCALE_DATE_SHORT,
-
     /** {@link DateTimeFormat} uses the Ascii digits for {@code ar-DZ} locale. */
     @BrowserFeature({ @WebBrowser(FF), @WebBrowser(IE) })
     JS_DATE_AR_DZ_ASCII_DIGITS,
+
+    /** <code>Date.toLocaleDateString()</code> returns a short form (d.M.yyyy). */
+    @BrowserFeature({ @WebBrowser(CHROME), @WebBrowser(FF) })
+    JS_DATE_LOCALE_DATE_SHORT,
 
     /** {@link DateTimeFormat} uses the Unicode Character {@code 'LEFT-TO-RIGHT MARK'}. */
     @BrowserFeature(@WebBrowser(IE))
