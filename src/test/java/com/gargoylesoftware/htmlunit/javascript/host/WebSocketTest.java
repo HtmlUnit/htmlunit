a9a3a0d4ef2ee376af32f5c09afbdae33d4ab645
rbri <rbri@rbri.de>
2016-03-31 14:54:56 +0000

    unit test for events added (NYI)
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@12262 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/WebSocketTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/WebSocketTest.java
@@ -14,7 +14,10 @@
  */
 package com.gargoylesoftware.htmlunit.javascript.host;
 
+import static java.nio.charset.StandardCharsets.UTF_16LE;
+
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
@@ -34,12 +37,15 @@ import org.openqa.selenium.WebElement;
 
 import com.gargoylesoftware.htmlunit.BrowserRunner;
 import com.gargoylesoftware.htmlunit.BrowserRunner.Alerts;
+import com.gargoylesoftware.htmlunit.BrowserRunner.NotYetImplemented;
 import com.gargoylesoftware.htmlunit.WebDriverTestCase;
 
 /**
  * Tests for {@link WebSocket}.
  *
  * @author Ahmed Ashour
+ * @author Ronald Brill
+ * @author Madis Pärn
  */
 @RunWith(BrowserRunner.class)
 public class WebSocketTest extends WebDriverTestCase {
@@ -50,35 +56,35 @@ public class WebSocketTest extends WebDriverTestCase {
      */
     @Test
     public void chat() throws Exception {
-        //TODO: compatibility of FF and Chrome versions.
-        if (getBrowserVersion().isFirefox()) {
-            final String firstResponse = "Browser: has joined!";
-            final String secondResponse = "Browser: Hope you are fine!";
+        final String firstResponse = "Browser: has joined!";
+        final String secondResponse = "Browser: Hope you are fine!";
 
-            startWebServer("src/test/resources/com/gargoylesoftware/htmlunit/javascript/host",
-                null, null, new ChatWebSocketHandler());
-            final WebDriver driver = getWebDriver();
-            driver.get("http://localhost:" + PORT + "/WebSocketTest_chat.html");
+        startWebServer("src/test/resources/com/gargoylesoftware/htmlunit/javascript/host",
+            null, null, new ChatWebSocketHandler());
+        final WebDriver driver = getWebDriver();
+        driver.get("http://localhost:" + PORT + "/WebSocketTest_chat.html");
 
-            driver.findElement(By.id("username")).sendKeys("Browser");
-            driver.findElement(By.id("joinB")).click();
-            final WebElement chatE = driver.findElement(By.id("chat"));
-            int counter = 0;
-            do {
-                Thread.sleep(100);
-            } while (chatE.getText().isEmpty() && counter++ < 10);
+        driver.findElement(By.id("username")).sendKeys("Browser");
+        driver.findElement(By.id("joinB")).click();
 
-            assertEquals(firstResponse, chatE.getText());
+        assertVisible("joined", driver);
 
-            driver.findElement(By.id("phrase")).sendKeys("Hope you are fine!");
-            driver.findElement(By.id("sendB")).click();
-            counter = 0;
-            do {
-                Thread.sleep(100);
-            } while (!chatE.getText().contains(secondResponse) && counter++ < 10);
+        final WebElement chatE = driver.findElement(By.id("chat"));
+        int counter = 0;
+        do {
+            Thread.sleep(100);
+        } while (chatE.getText().isEmpty() && counter++ < 10);
 
-            assertEquals(firstResponse + "\n" + secondResponse, chatE.getText());
-        }
+        assertEquals(firstResponse, chatE.getText());
+
+        driver.findElement(By.id("phrase")).sendKeys("Hope you are fine!");
+        driver.findElement(By.id("sendB")).click();
+        counter = 0;
+        do {
+            Thread.sleep(100);
+        } while (!chatE.getText().contains(secondResponse) && counter++ < 10);
+
+        assertEquals(firstResponse + "\n" + secondResponse, chatE.getText());
     }
 
     private static class ChatWebSocketHandler extends WebSocketHandler {
@@ -171,21 +177,21 @@ public class WebSocketTest extends WebDriverTestCase {
 
     private static class CookiesWebSocketHandler extends WebSocketHandler {
 
-        private final Set<ChatWebSocket> webSockets_ = new CopyOnWriteArraySet<>();
+        private final Set<CookiesWebSocket> webSockets_ = new CopyOnWriteArraySet<>();
 
         @Override
         public void configure(final WebSocketServletFactory factory) {
-            factory.register(ChatWebSocket.class);
+            factory.register(CookiesWebSocket.class);
             factory.setCreator(new WebSocketCreator() {
                 @Override
                 public Object createWebSocket(final ServletUpgradeRequest servletUpgradeRequest,
                         final ServletUpgradeResponse servletUpgradeResponse) {
-                    return new ChatWebSocket();
+                    return new CookiesWebSocket();
                 }
             });
         }
 
-        private class ChatWebSocket extends WebSocketAdapter {
+        private class CookiesWebSocket extends WebSocketAdapter {
             private Session session_;
             private int counter_ = 1;
 
@@ -199,7 +205,7 @@ public class WebSocketTest extends WebDriverTestCase {
             public void onWebSocketText(final String data) {
                 try {
                     final String cookie = session_.getUpgradeRequest().getHeaders().get("Cookie").get(0) + counter_++;
-                    for (final ChatWebSocket webSocket : webSockets_) {
+                    for (final CookiesWebSocket webSocket : webSockets_) {
                         webSocket.session_.getRemote().sendString(cookie);
                     }
                 }
@@ -211,6 +217,85 @@ public class WebSocketTest extends WebDriverTestCase {
             @Override
             public void onWebSocketClose(final int closeCode, final String message) {
                 webSockets_.remove(this);
+            }
+        }
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @NotYetImplemented
+    public void events() throws Exception {
+        startWebServer("src/test/resources/com/gargoylesoftware/htmlunit/javascript/host",
+            null, null, new EventsWebSocketHandler());
+        final WebDriver driver = getWebDriver();
+        driver.get("http://localhost:" + PORT + "/WebSocketTest_events.html");
+
+        assertVisible("onOpen", driver);
+        assertVisible("onMessageText", driver);
+        assertVisible("onMessageBinary", driver);
+        assertVisible("onClose", driver);
+    }
+
+    private void assertVisible(final String domId, final WebDriver driver) throws Exception {
+        final WebElement domE = driver.findElement(By.id(domId));
+        int counter = 0;
+        do {
+            Thread.sleep(100);
+        } while (!domE.isDisplayed() && counter++ < 10);
+
+        assertEquals("Node should be visible, domId: " + domId, true, domE.isDisplayed());
+    }
+
+    private static class EventsWebSocketHandler extends WebSocketHandler {
+
+        @Override
+        public void configure(final WebSocketServletFactory factory) {
+            factory.register(EventsWebSocket.class);
+            factory.setCreator(new WebSocketCreator() {
+                @Override
+                public EventsWebSocket createWebSocket(final ServletUpgradeRequest servletUpgradeRequest,
+                        final ServletUpgradeResponse servletUpgradeResponse) {
+                    return new EventsWebSocket();
+                }
+            });
+        }
+
+        private static class EventsWebSocket extends WebSocketAdapter {
+            @Override
+            public void onWebSocketText(final String data) {
+                if ("text".equals(data)) {
+                    try {
+                        getRemote().sendString("server_text");
+                    }
+                    catch (final IOException e) {
+                        throw new IllegalStateException(e.getMessage(), e);
+                    }
+                }
+                else if ("close".equals(data)) {
+                    getSession().close();
+                }
+                else {
+                    throw new IllegalArgumentException("Unknown request: " + data);
+                }
+            }
+
+            @Override
+            public void onWebSocketBinary(final byte[] payload, final int offset, final int len) {
+                final String data = new String(payload, offset, len, UTF_16LE);
+                if ("binary".equals(data)) {
+                    final ByteBuffer response = ByteBuffer.wrap("server_binary".getBytes(UTF_16LE));
+                    try {
+                        getRemote().sendBytes(response);
+                    }
+                    catch (final IOException e) {
+                        throw new IllegalStateException(e.getMessage(), e);
+                    }
+                }
+                else {
+                    throw new IllegalArgumentException("Unknown request: " + data);
+                }
             }
         }
     }
