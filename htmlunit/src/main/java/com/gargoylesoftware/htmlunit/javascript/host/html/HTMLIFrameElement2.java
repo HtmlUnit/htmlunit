6943256d549b82c1e39636f328b0901d081ef0dc
asashour <asashour@yahoo.com>
2016-04-25 17:15:24 +0000

    hu3: HTMLIFrameElement
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/branches/htmlunit3@12489 5f5364db-9458-4db8-a492-e30667be6df6

---
+++ htmlunit/src/main/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLIFrameElement2.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2016 Gargoyle Software Inc.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (http://www.gnu.org/licenses/).
+ */
+package com.gargoylesoftware.htmlunit.javascript.host.html;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
+import com.gargoylesoftware.js.nashorn.ScriptUtils;
+import com.gargoylesoftware.js.nashorn.internal.runtime.Context;
+import com.gargoylesoftware.js.nashorn.internal.runtime.PrototypeObject;
+import com.gargoylesoftware.js.nashorn.internal.runtime.ScriptFunction;
+
+public class HTMLIFrameElement2 extends HTMLElement2 {
+
+    public static HTMLIFrameElement2 constructor(final boolean newObj, final Object self) {
+        final HTMLIFrameElement2 host = new HTMLIFrameElement2();
+        host.setProto(Context.getGlobal().getPrototype(host.getClass()));
+        return host;
+    }
+
+    private static MethodHandle staticHandle(final String name, final Class<?> rtype, final Class<?>... ptypes) {
+        try {
+            return MethodHandles.lookup().findStatic(HTMLIFrameElement2.class,
+                    name, MethodType.methodType(rtype, ptypes));
+        }
+        catch (final ReflectiveOperationException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static final class FunctionConstructor extends ScriptFunction {
+        public FunctionConstructor() {
+            super("HTMLIFrameElement", 
+                    staticHandle("constructor", HTMLIFrameElement2.class, boolean.class, Object.class),
+                    null);
+            final Prototype prototype = new Prototype();
+            PrototypeObject.setConstructor(prototype, this);
+            setPrototype(prototype);
+        }
+    }
+
+    public static final class Prototype extends PrototypeObject {
+        Prototype() {
+            ScriptUtils.initialize(this);
+        }
+
+        public String getClassName() {
+            return "HTMLIFrameElement";
+        }
+    }
+}
