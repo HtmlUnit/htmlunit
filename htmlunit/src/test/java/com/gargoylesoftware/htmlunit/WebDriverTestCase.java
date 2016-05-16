bb6cd67b31bdfac9430efa574587634ebc1d2054
asashour <asashour@yahoo.com>
2016-05-16 06:12:36 +0000

    hu3: run Chrome only for now
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/branches/htmlunit3@12634 5f5364db-9458-4db8-a492-e30667be6df6

--- htmlunit/src/test/java/com/gargoylesoftware/htmlunit/WebDriverTestCase.java
+++ htmlunit/src/test/java/com/gargoylesoftware/htmlunit/WebDriverTestCase.java
@@ -130,8 +130,7 @@ public abstract class WebDriverTestCase extends WebTestCase {
     /**
      * Browsers which run by default.
      */
-    public static BrowserVersion[] DEFAULT_RUNNING_BROWSERS_ = {BrowserVersion.CHROME, BrowserVersion.FIREFOX_38,
-        BrowserVersion.FIREFOX_45, BrowserVersion.INTERNET_EXPLORER};
+    public static BrowserVersion[] DEFAULT_RUNNING_BROWSERS_ = {BrowserVersion.CHROME};
 
     private static final Log LOG = LogFactory.getLog(WebDriverTestCase.class);
 
