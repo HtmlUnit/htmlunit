dc55972d7d56e26fe82d8b2e756e0231bc05541c
asashour <asashour@yahoo.com>
2016-06-13 16:30:04 +0000

    Add test case Issue 1799
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@12809 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/NativeObjectTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/NativeObjectTest.java
@@ -19,6 +19,7 @@ import org.junit.runner.RunWith;
 
 import com.gargoylesoftware.htmlunit.BrowserRunner;
 import com.gargoylesoftware.htmlunit.BrowserRunner.Alerts;
+import com.gargoylesoftware.htmlunit.BrowserRunner.NotYetImplemented;
 import com.gargoylesoftware.htmlunit.WebDriverTestCase;
 
 /**
@@ -108,6 +109,33 @@ public class NativeObjectTest extends WebDriverTestCase {
             + "</script></head><body onload='test()'>\n"
             + "</body></html>";
 
+        loadPageWithAlerts2(html);
+    }
+
+    /**
+     * Test for bug #1799.
+     *
+     * @throws Exception if an error occurs
+     */
+    @Test
+    @Alerts("exception")
+    @NotYetImplemented
+    public void setPrototype() throws Exception {
+        final String html = "<html><head><script>\n"
+            + "  var v = function() {\n"
+            + "  }\n"
+            + "  function test() {\n"
+            + "    try {\n"
+            + "      var h = window.XMLHttpRequest;\n"
+            + "      window.XMLHttpRequest = v;\n"
+            + "      window.XMLHttpRequest.prototype = Object.create(h.prototype);\n"
+            + "      var b = 'onerror';\n"
+            + "      window.XMLHttpRequest.prototype[b] = h.prototype[b];\n"
+            + "    } catch(e) {alert('exception')}\n"
+            + "  }\n"
+            + "</script></head>\n"
+            + "<body onload='test()'></body>\n"
+            + "</html>\n";
         loadPageWithAlerts2(html);
     }
 }
