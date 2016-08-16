5fae63fdbddbec9e230653f925559ba17b7529ff
rbri <rbri@rbri.de>
2016-08-16 09:49:35 +0000

    latest chrome
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@12913 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/crypto/SubtleCryptoTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/crypto/SubtleCryptoTest.java
@@ -43,6 +43,12 @@ public class SubtleCryptoTest extends WebDriverTestCase {
                         "private", "false", "sign",
                         "name RSASSA-PKCS1-v1_5", "hash [object Object]", "modulusLength 2048",
                         "publicExponent [object Uint8Array]"},
+            CHROME = {"[object Crypto]", "public", "true", "verify",
+                        "name RSASSA-PKCS1-v1_5", "hash [object Object]", "modulusLength 2048",
+                        "publicExponent 1,0,1",
+                        "private", "false", "sign",
+                        "name RSASSA-PKCS1-v1_5", "hash [object Object]", "modulusLength 2048",
+                        "publicExponent 1,0,1"},
             IE = "undefined")
     @NotYetImplemented({CHROME, FF})
     public void rsassa() throws Exception {
