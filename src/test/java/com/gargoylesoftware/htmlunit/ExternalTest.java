7f44d3c0a662c357ae215d677f6739091df66a56
rbri <rbri@rbri.de>
2016-08-16 05:47:48 +0000

    latest chrome driver
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@12900 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/ExternalTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/ExternalTest.java
@@ -69,7 +69,7 @@ public class ExternalTest {
                 }
             }
             assertVersion("org.sonatype.oss", "oss-parent", "9");
-            assertChromeDriver("2.22");
+            assertChromeDriver("2.23");
         }
     }
 
