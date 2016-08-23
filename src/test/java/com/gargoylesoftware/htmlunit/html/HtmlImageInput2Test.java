ab614421c64257dca882d90d26f522a8079db826
rbri <rbri@rbri.de>
2016-08-23 18:41:12 +0000

    switch content of the two test files to have similar test in the first file like we have for password and hidden
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@12938 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/html/HtmlImageInput2Test.java
+++ src/test/java/com/gargoylesoftware/htmlunit/html/HtmlImageInput2Test.java
@@ -14,24 +14,16 @@
  */
 package com.gargoylesoftware.htmlunit.html;
 
-import static com.gargoylesoftware.htmlunit.BrowserRunner.Browser.CHROME;
-import static com.gargoylesoftware.htmlunit.BrowserRunner.Browser.IE;
-
-import java.io.InputStream;
-import java.net.URL;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 
 import com.gargoylesoftware.htmlunit.BrowserRunner;
-import com.gargoylesoftware.htmlunit.BrowserRunner.Alerts;
-import com.gargoylesoftware.htmlunit.BrowserRunner.NotYetImplemented;
-import com.gargoylesoftware.htmlunit.WebDriverTestCase;
+import com.gargoylesoftware.htmlunit.MockWebConnection;
+import com.gargoylesoftware.htmlunit.SimpleWebTestCase;
 import com.gargoylesoftware.htmlunit.util.NameValuePair;
 
 /**
@@ -40,323 +32,68 @@ import com.gargoylesoftware.htmlunit.util.NameValuePair;
  * @author <a href="mailto:mbowler@GargoyleSoftware.com">Mike Bowler</a>
  * @author Marc Guillemot
  * @author Ahmed Ashour
- * @author Ronald Brill
  */
 @RunWith(BrowserRunner.class)
-public class HtmlImageInput2Test extends WebDriverTestCase {
+public class HtmlImageInput2Test extends SimpleWebTestCase {
 
     /**
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts(DEFAULT = "§§URL§§?button.x=0&button.y=0",
-            CHROME = "§§URL§§?button.x=9&button.y=7&button=foo",
-            IE = "§§URL§§?button.x=14&button.y=15")
-    @NotYetImplemented({CHROME, IE})
-    public void click_NoPosition() throws Exception {
+    public void click_WithPosition() throws Exception {
         final String html
             = "<html><head><title>foo</title></head><body>\n"
-            + "<form id='form1'>\n"
+            + "<form id='form1' method='post'>\n"
             + "<input type='image' name='aButton' value='foo'/>\n"
             + "<input type='image' name='button' value='foo'/>\n"
             + "<input type='image' name='anotherButton' value='foo'/>\n"
             + "</form></body></html>";
-        final WebDriver webDriver = loadPage2(html);
-        webDriver.findElement(By.name("button")).click();
+        final HtmlPage page = loadPageWithAlerts(html);
+        final MockWebConnection webConnection = getMockConnection(page);
 
-        expandExpectedAlertsVariables(URL_FIRST);
-        assertEquals(getExpectedAlerts()[0], webDriver.getCurrentUrl());
-    }
+        final HtmlForm form = page.getHtmlElementById("form1");
 
-    /**
-     * @throws Exception if the test fails
-     */
-    @Test
-    @Alerts(DEFAULT = "§§URL§§?button.x=0&button.y=0",
-            CHROME = "§§URL§§?button.x=22&button.y=7",
-            IE = "§§URL§§?button.x=14&button.y=15")
-    @NotYetImplemented({CHROME, IE})
-    public void click_NoPosition_NoValue() throws Exception {
-        final String html
-            = "<html><head><title>foo</title></head><body>\n"
-            + "<form id='form1'>\n"
-            + "<input type='image' name='button'>\n"
-            + "</form></body></html>";
-        final WebDriver webDriver = loadPage2(html);
-        webDriver.findElement(By.name("button")).click();
+        final HtmlImageInput imageInput = form.getInputByName("button");
+        final HtmlPage secondPage = imageInput.click(100, 200);
+        assertNotNull(secondPage);
 
-        expandExpectedAlertsVariables(URL_FIRST);
-        assertEquals(getExpectedAlerts()[0], webDriver.getCurrentUrl());
-    }
+        final List<NameValuePair> expectedPairs = new ArrayList<>();
+        expectedPairs.add(new NameValuePair("button.x", "100"));
+        expectedPairs.add(new NameValuePair("button.y", "200"));
 
-    /**
-     * @throws Exception if the test fails
-     */
-    @Test
-    @Alerts({"-", "-", "-"})
-    public void defaultValues() throws Exception {
-        final String html = "<html><head><title>foo</title>\n"
-            + "<script>\n"
-            + "  function test() {\n"
-            + "    var input = document.getElementById('image1');\n"
-            + "    alert(input.value + '-' + input.defaultValue);\n"
-
-            + "    input = document.createElement('input');\n"
-            + "    input.type = 'image';\n"
-            + "    alert(input.value + '-' + input.defaultValue);\n"
-
-            + "    var builder = document.createElement('div');\n"
-            + "    builder.innerHTML = '<input type=\"image\">';\n"
-            + "    input = builder.firstChild;\n"
-            + "    alert(input.value + '-' + input.defaultValue);\n"
-            + "  }\n"
-            + "</script>\n"
-            + "</head><body onload='test()'>\n"
-            + "<form>\n"
-            + "  <input type='image' id='image1'>\n"
-            + "</form>\n"
-            + "</body></html>";
-
-        loadPageWithAlerts2(html);
-    }
-
-    /**
-     * @throws Exception if the test fails
-     */
-    @Test
-    @Alerts({"-", "-", "-"})
-    public void defaultValuesAfterclone() throws Exception {
-        final String html = "<html><head><title>foo</title>\n"
-            + "<script>\n"
-            + "  function test() {\n"
-            + "    var input = document.getElementById('image1');\n"
-            + "    input = input.cloneNode(false);\n"
-            + "    alert(input.value + '-' + input.defaultValue);\n"
-
-            + "    input = document.createElement('input');\n"
-            + "    input.type = 'image';\n"
-            + "    input = input.cloneNode(false);\n"
-            + "    alert(input.value + '-' + input.defaultValue);\n"
-
-            + "    var builder = document.createElement('div');\n"
-            + "    builder.innerHTML = '<input type=\"image\">';\n"
-            + "    input = builder.firstChild;\n"
-            + "    input = input.cloneNode(false);\n"
-            + "    alert(input.value + '-' + input.defaultValue);\n"
-            + "  }\n"
-            + "</script>\n"
-            + "</head><body onload='test()'>\n"
-            + "<form>\n"
-            + "  <input type='image' id='image1'>\n"
-            + "</form>\n"
-            + "</body></html>";
-
-        loadPageWithAlerts2(html);
-    }
-
-    /**
-     * @throws Exception if the test fails
-     */
-    @Test
-    @Alerts({"initial-initial", "initial-initial", "newValue-newValue", "newValue-newValue",
-                "newDefault-newDefault", "newDefault-newDefault"})
-    public void resetByClick() throws Exception {
-        final String html = "<html><head><title>foo</title>\n"
-            + "<script>\n"
-            + "  function test() {\n"
-            + "    var image = document.getElementById('testId');\n"
-            + "    alert(image.value + '-' + image.defaultValue);\n"
-
-            + "    document.getElementById('testReset').click;\n"
-            + "    alert(image.value + '-' + image.defaultValue);\n"
-
-            + "    image.value = 'newValue';\n"
-            + "    alert(image.value + '-' + image.defaultValue);\n"
-
-            + "    document.getElementById('testReset').click;\n"
-            + "    alert(image.value + '-' + image.defaultValue);\n"
-
-            + "    image.defaultValue = 'newDefault';\n"
-            + "    alert(image.value + '-' + image.defaultValue);\n"
-
-            + "    document.forms[0].reset;\n"
-            + "    alert(image.value + '-' + image.defaultValue);\n"
-            + "  }\n"
-            + "</script>\n"
-            + "</head><body onload='test()'>\n"
-            + "<form>\n"
-            + "  <input type='image' id='testId' value='initial'>\n"
-            + "  <input type='reset' id='testReset'>\n"
-            + "</form>\n"
-            + "</body></html>";
-
-        loadPageWithAlerts2(html);
-    }
-
-    /**
-     * @throws Exception if the test fails
-     */
-    @Test
-    @Alerts({"initial-initial", "initial-initial", "newValue-newValue", "newValue-newValue",
-                "newDefault-newDefault", "newDefault-newDefault"})
-    public void resetByJS() throws Exception {
-        final String html = "<html><head><title>foo</title>\n"
-            + "<script>\n"
-            + "  function test() {\n"
-            + "    var image = document.getElementById('testId');\n"
-            + "    alert(image.value + '-' + image.defaultValue);\n"
-
-            + "    document.forms[0].reset;\n"
-            + "    alert(image.value + '-' + image.defaultValue);\n"
-
-            + "    image.value = 'newValue';\n"
-            + "    alert(image.value + '-' + image.defaultValue);\n"
-
-            + "    document.forms[0].reset;\n"
-            + "    alert(image.value + '-' + image.defaultValue);\n"
-
-            + "    image.defaultValue = 'newDefault';\n"
-            + "    alert(image.value + '-' + image.defaultValue);\n"
-
-            + "    document.forms[0].reset;\n"
-            + "    alert(image.value + '-' + image.defaultValue);\n"
-            + "  }\n"
-            + "</script>\n"
-            + "</head><body onload='test()'>\n"
-            + "<form>\n"
-            + "  <input type='image' id='testId' value='initial'>\n"
-            + "</form>\n"
-            + "</body></html>";
-
-        loadPageWithAlerts2(html);
-    }
-
-    /**
-     * @throws Exception if the test fails
-     */
-    @Test
-    @Alerts({"initial-initial", "default-default", "newValue-newValue", "newDefault-newDefault"})
-    public void defaultValue() throws Exception {
-        final String html = "<html><head><title>foo</title>\n"
-            + "<script>\n"
-            + "  function test() {\n"
-            + "    var image = document.getElementById('testId');\n"
-            + "    alert(image.value + '-' + image.defaultValue);\n"
-
-            + "    image.defaultValue = 'default';\n"
-            + "    alert(image.value + '-' + image.defaultValue);\n"
-
-            + "    image.value = 'newValue';\n"
-            + "    alert(image.value + '-' + image.defaultValue);\n"
-            + "    image.defaultValue = 'newDefault';\n"
-            + "    alert(image.value + '-' + image.defaultValue);\n"
-            + "  }\n"
-            + "</script>\n"
-            + "</head><body onload='test()'>\n"
-            + "<form>\n"
-            + "  <input type='image' id='testId' value='initial'>\n"
-            + "</form>\n"
-            + "</body></html>";
-
-        loadPageWithAlerts2(html);
-    }
-
-    /**
-     * @throws Exception if the test fails
-     */
-    @Test
-    @Alerts("§§URL§§?imageInput.x=0&imageInput.y=0")
-    public void javascriptClick() throws Exception {
-        final String html = "<html><head><title>foo</title>\n"
-                + "</head><body>\n"
-                + "<form>\n"
-                + "  <input type='image' name='imageInput'>\n"
-                + "  <input type='button' id='submit' value='submit' "
-                        + "onclick='document.getElementsByName(\"imageInput\")[0].click()'>\n"
-                + "</form>\n"
-                + "</body></html>";
-
-        final WebDriver webDriver = loadPage2(html);
-        webDriver.findElement(By.id("submit")).click();
-
-        expandExpectedAlertsVariables(URL_FIRST);
-        assertEquals(getExpectedAlerts()[0], webDriver.getCurrentUrl());
-    }
-
-    /**
-     * Test for bug #646.
-     * @throws Exception if an error occurs
-     */
-    @Test
-    @Alerts("1")
-    public void clickFiresOnMouseDown() throws Exception {
-        final String html = "<html><body><input type='image' src='x.png' id='i' onmousedown='alert(1)'></body></html>";
-
-        final WebDriver webDriver = loadPage2(html);
-        webDriver.findElement(By.id("i")).click();
-
-        verifyAlerts(webDriver, getExpectedAlerts());
-    }
-
-    /**
-     * Test for bug #646.
-     * @throws Exception if an error occurs
-     */
-    @Test
-    @Alerts("1")
-    public void clickFiresOnMouseUp() throws Exception {
-        final String html = "<html><body><input type='image' src='x.png' id='i' onmouseup='alert(1)'></body></html>";
-        final WebDriver webDriver = loadPage2(html);
-        webDriver.findElement(By.id("i")).click();
-
-        verifyAlerts(webDriver, getExpectedAlerts());
-    }
-
-    /**
-     * @throws Exception if the test fails
-     */
-    @Test
-    @Alerts("1")
-    public void outsideForm() throws Exception {
-        final String html =
-            "<html><head></head>\n"
-            + "<body>\n"
-            + "<input id='myInput' type='image' src='test.png' onclick='alert(1)'>\n"
-            + "</body></html>";
-        final WebDriver webDriver = loadPage2(html);
-        webDriver.findElement(By.id("myInput")).click();
-
-        verifyAlerts(webDriver, getExpectedAlerts());
-    }
-
-    /**
-     * @throws Exception if the test fails
-     */
-    @Test
-    @Alerts("§§URL§§abcd/img.gif")
-    public void lineBreaksInUrl() throws Exception {
-        try (final InputStream is = getClass().getClassLoader().getResourceAsStream("testfiles/tiny-gif.img")) {
-            final byte[] directBytes = IOUtils.toByteArray(is);
-            final URL urlImage = new URL(URL_SECOND, "abcd/img.gif");
-            final List<NameValuePair> emptyList = Collections.emptyList();
-            getMockWebConnection().setResponse(urlImage, directBytes, 200, "ok", "image/gif", emptyList);
+        if ("Chrome".equals(getBrowserVersion().getNickname())) {
+            expectedPairs.add(new NameValuePair("button", "foo"));
         }
 
-        final String html
-            = "<html><head>\n"
-            + "<script>\n"
-            + "  function test() {\n"
-            + "    var input = document.getElementById('myInput');\n"
-            + "    alert(input.src);\n"
-            + "  }\n"
-            + "</script>\n"
-            + "</head>\n"
-            + "<body onload='test()'>\n"
-            + "  <input id='myInput' type='image' src='" + URL_SECOND + "a\rb\nc\r\nd/img.gif'>\n"
-            + "</body>\n"
-            + "</html>";
+        assertEquals(expectedPairs, webConnection.getLastParameters());
+    }
 
-        expandExpectedAlertsVariables(URL_SECOND);
-        loadPageWithAlerts2(html);
+    /**
+     * If an image button without name is clicked, it should send only "x" and "y" parameters.
+     * Regression test for bug #217.
+     * @throws Exception if the test fails
+     */
+    @Test
+    public void noNameClick_WithPosition() throws Exception {
+        final String html
+            = "<html><head><title>foo</title></head><body>\n"
+            + "<form id='form1' method='post'>\n"
+            + "<input type='image' value='foo'/>\n"
+            + "</form></body></html>";
+        final HtmlPage page = loadPageWithAlerts(html);
+        final MockWebConnection webConnection = getMockConnection(page);
+
+        final HtmlForm form = page.getHtmlElementById("form1");
+
+        final HtmlImageInput imageInput = form.getInputByValue("foo");
+        final HtmlPage secondPage = imageInput.click(100, 200);
+        assertNotNull(secondPage);
+
+        final List<NameValuePair> expectedPairs = Arrays.asList(new NameValuePair[]{
+            new NameValuePair("x", "100"),
+            new NameValuePair("y", "200")
+        });
+
+        assertEquals(expectedPairs, webConnection.getLastParameters());
     }
 }
