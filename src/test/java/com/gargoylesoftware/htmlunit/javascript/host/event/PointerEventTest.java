c21008e7b5082baeb64681d04c45aa55d068e47b
rbri <rbri@rbri.de>
2016-12-02 20:45:27 +0000

    latest CHROME
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@13111 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/event/PointerEventTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/event/PointerEventTest.java
@@ -53,7 +53,9 @@ public class PointerEventTest extends WebDriverTestCase {
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts("exception")
+    @Alerts(DEFAULT = "exception",
+            CHROME = {"[object PointerEvent]", "click", "false", "false",
+                            "0", "1", "1", "0", "0", "0", "", "false"})
     public void create_ctor() throws Exception {
         final String html = HtmlPageTest.STANDARDS_MODE_PREFIX_
             + "<html><head><title>foo</title><script>\n"
@@ -74,7 +76,9 @@ public class PointerEventTest extends WebDriverTestCase {
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts("exception")
+    @Alerts(DEFAULT = "exception",
+            CHROME = {"[object PointerEvent]", "click", "true", "false",
+                            "2", "1", "1", "0", "0", "0", "mouse", "false"})
     public void create_ctorWithDetails() throws Exception {
         final String html = HtmlPageTest.STANDARDS_MODE_PREFIX_
             + "<html><head><title>foo</title><script>\n"
