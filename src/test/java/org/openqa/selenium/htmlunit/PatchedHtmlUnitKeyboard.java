4d72928b55928b0d11d5fd07ffcb5f835f0a804b
asashour <asashour@yahoo.com>
2017-01-25 02:29:17 +0000

    remove unneeded parts of PatchedHtmlUnitKeyboard
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@13417 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/org/openqa/selenium/htmlunit/PatchedHtmlUnitKeyboard.java
+++ src/test/java/org/openqa/selenium/htmlunit/PatchedHtmlUnitKeyboard.java
@@ -15,152 +15,54 @@
 
 package org.openqa.selenium.htmlunit;
 
-import java.io.IOException;
-
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriverException;
-import org.openqa.selenium.WebElement;
+import java.lang.reflect.Field;
 
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlFileInput;
-import com.gargoylesoftware.htmlunit.html.Keyboard;
 
 /**
  * Temporary work around for HtmlUnitDriver.
  *
  * @author Ronald Brill
+ * @author Ahmed Ashour
  */
 public final class PatchedHtmlUnitKeyboard extends HtmlUnitKeyboard {
 
-    private KeyboardModifiersState modifiersState_ = new KeyboardModifiersState();
-    private final HtmlUnitDriver parent_;
-    private HtmlElement lastElement_;
-
     /**
-     * Ctor.
+     * Constructor.
+     *
      * @param parent the driver
      */
     public PatchedHtmlUnitKeyboard(final HtmlUnitDriver parent) {
         super(parent);
-        this.parent_ = parent;
     }
 
-    private HtmlUnitWebElement getElementToSend(final WebElement toElement) {
-        WebElement sendToElement = toElement;
-        if (sendToElement == null) {
-            sendToElement = parent_.switchTo().activeElement();
-        }
-
-        return (HtmlUnitWebElement) sendToElement;
-    }
-
-    @Override
-    public void sendKeys(final CharSequence... keysToSend) {
-        final WebElement toElement = parent_.switchTo().activeElement();
-
-        final HtmlUnitWebElement htmlElem = getElementToSend(toElement);
-        htmlElem.sendKeys(false, keysToSend);
-    }
-
+    /**
+     * {@inheritDoc}
+     * Overridden because .setValueAttribute moved from HtmlFileInput to HtmlInput
+     */
     @Override
     public void sendKeys(final HtmlElement element, final String currentValue, final InputKeysContainer keysToSend,
-        final boolean releaseAllAtEnd) {
-        keysToSend.setCapitalization(modifiersState_.isShiftPressed());
-        final String keysSequence = keysToSend.toString();
-
+            final boolean releaseAllAtEnd) {
+        final KeyboardModifiersState modifiersState = (KeyboardModifiersState) getPrivateField("modifiersState");
+        keysToSend.setCapitalization(modifiersState.isShiftPressed());
+        String keysSequence = keysToSend.toString();
         if (element instanceof HtmlFileInput) {
-            ((HtmlFileInput) element).setValueAttribute(keysSequence);
+            HtmlFileInput fileInput = (HtmlFileInput) element;
+            fileInput.setValueAttribute(keysSequence);
+            fileInput.fireEvent("change");
+            return;
         }
-        else {
-            try {
-                final Keyboard keyboard = asHtmlUnitKeyboard(lastElement_ != element, keysSequence, true);
-                if (releaseAllAtEnd) {
-                    if (isShiftPressed()) {
-                        addToKeyboard(keyboard, Keys.SHIFT.charAt(0), false);
-                    }
-                    if (isAltPressed()) {
-                        addToKeyboard(keyboard, Keys.ALT.charAt(0), false);
-                    }
-                    if (isCtrlPressed()) {
-                        addToKeyboard(keyboard, Keys.CONTROL.charAt(0), false);
-                    }
-                }
-                element.type(keyboard);
-            }
-            catch (final IOException e) {
-                throw new WebDriverException(e);
-            }
-        }
-        lastElement_ = element;
+        super.sendKeys(element, currentValue, keysToSend, releaseAllAtEnd);
     }
 
-    private Keyboard asHtmlUnitKeyboard(final boolean startAtEnd, final CharSequence keysSequence,
-        final boolean isPress) {
-        final Keyboard keyboard = new Keyboard(startAtEnd);
-        for (int i = 0; i < keysSequence.length(); i++) {
-            final char ch = keysSequence.charAt(i);
-            addToKeyboard(keyboard, ch, isPress);
-        }
-        return keyboard;
-    }
-
-    private void addToKeyboard(final Keyboard keyboard, final char ch, final boolean isPress) {
-        if (HtmlUnitKeyboardMapping.isSpecialKey(ch)) {
-            final int keyCode = HtmlUnitKeyboardMapping.getKeysMapping(ch);
-            if (isPress) {
-                keyboard.press(keyCode);
-                modifiersState_.storeKeyDown(ch);
-            }
-            else {
-                keyboard.release(keyCode);
-                modifiersState_.storeKeyUp(ch);
-            }
-        }
-        else {
-            keyboard.type(ch);
-        }
-    }
-
-    @Override
-    public void pressKey(final CharSequence keyToPress) {
-        final WebElement toElement = parent_.switchTo().activeElement();
-
-        final HtmlUnitWebElement htmlElement = getElementToSend(toElement);
-        final HtmlElement element = (HtmlElement) htmlElement.element;
+    private Object getPrivateField(final String fieldName) {
         try {
-            element.type(asHtmlUnitKeyboard(lastElement_ != element, keyToPress, true));
+            final Field f = getClass().getSuperclass().getDeclaredField(fieldName);
+            f.setAccessible(true);
+            return f.get(this);
+        } catch (Exception e) {
+            return null;
         }
-        catch (final IOException e) {
-            throw new WebDriverException(e);
-        }
-    }
-
-    @Override
-    public void releaseKey(final CharSequence keyToRelease) {
-        final WebElement toElement = parent_.switchTo().activeElement();
-
-        final HtmlUnitWebElement htmlElement = getElementToSend(toElement);
-        final HtmlElement element = (HtmlElement) htmlElement.element;
-        try {
-            element.type(asHtmlUnitKeyboard(lastElement_ != element, keyToRelease, false));
-        }
-        catch (final IOException e) {
-            throw new WebDriverException(e);
-        }
-    }
-
-    @Override
-    public boolean isShiftPressed() {
-        return modifiersState_.isShiftPressed();
-    }
-
-    @Override
-    public boolean isCtrlPressed() {
-        return modifiersState_.isCtrlPressed();
-    }
-
-    @Override
-    public boolean isAltPressed() {
-        return modifiersState_.isAltPressed();
     }
 }
