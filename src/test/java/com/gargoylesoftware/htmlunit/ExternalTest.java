eebe574750eee1c93bdea94a53e226f91eed5628
asashour <asashour@yahoo.com>
2017-01-30 07:01:03 +0000

    Exclude checkstyle 7.5
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@13479 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/ExternalTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/ExternalTest.java
@@ -195,6 +195,8 @@ public class ExternalTest {
 
     private static boolean isIgnored(final String groupId, final String artifactId, final String version) {
         return groupId.startsWith("org.eclipse.jetty") && version.startsWith("9.4.")
+                // https://github.com/checkstyle/checkstyle/issues/3764
+                || artifactId.equals("checkstyle") && version.equals("7.5")
                 // 1.8.13: missing many dependencies
                 // 1.8.14: https://issues.tmatesoft.com/issue/SVNKIT-692
                 || groupId.startsWith("org.tmatesoft.svnkit")
