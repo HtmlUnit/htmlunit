098452fd27c32d72c5f993ec6f99b0e53a6aef47
asashour <asashour@yahoo.com>
2017-03-01 14:45:09 +0000

    Tests pass
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@13668 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/html/BaseFrameElement2Test.java
+++ src/test/java/com/gargoylesoftware/htmlunit/html/BaseFrameElement2Test.java
@@ -31,6 +31,14 @@ import com.gargoylesoftware.htmlunit.WebDriverTestCase;
 public class BaseFrameElement2Test extends WebDriverTestCase {
 
     /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected boolean supportsWebDriver() {
+        return true;
+    }
+
+    /**
      * @throws Exception if the test fails
      */
     @Test
--- src/test/java/com/gargoylesoftware/htmlunit/html/ClickableElement2Test.java
+++ src/test/java/com/gargoylesoftware/htmlunit/html/ClickableElement2Test.java
@@ -41,6 +41,14 @@ import com.gargoylesoftware.htmlunit.WebDriverTestCase;
 public class ClickableElement2Test extends WebDriverTestCase {
 
     /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected boolean supportsWebDriver() {
+        return true;
+    }
+
+    /**
      * @throws Exception if the test fails
      */
     @Test
