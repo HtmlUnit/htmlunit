a36598c594924f0ed1607b58e71d27698dabc7f2
rbri <rbri@rbri.de>
2017-03-13 19:27:23 +0000

    latest chrome
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@13722 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLKeygenElementTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLKeygenElementTest.java
@@ -33,8 +33,7 @@ public class HTMLKeygenElementTest extends WebDriverTestCase {
      * @throws Exception if an error occurs
      */
     @Test
-    @Alerts(DEFAULT = {"undefined", "undefined", "undefined", "undefined", "undefined", "undefined"},
-            CHROME = {"0", "2", "1", "2", "1", "1"})
+    @Alerts({"undefined", "undefined", "undefined", "undefined", "undefined", "undefined"})
     public void labels() throws Exception {
         final String html =
             "<html><head>\n"
