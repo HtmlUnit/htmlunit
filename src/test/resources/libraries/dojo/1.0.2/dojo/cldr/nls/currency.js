f6eee45a4b5f7a2380da294a0d232c28fcf95850
rbri <rbri@rbri.de>
2017-03-15 19:15:49 +0000

    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@13733 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/resources/libraries/dojo/1.0.2/dojo/cldr/nls/currency.js
+++
@@ -1,1 +0,0 @@
-({"USD_symbol": "$", "EUR_displayName": "EUR", "GBP_displayName": "GBP", "JPY_displayName": "JPY", "GBP_symbol": "£", "JPY_symbol": "¥", "EUR_symbol": "€", "USD_displayName": "USD"})
--- src/test/resources/testJsError4.js
+++
@@ -1,8 +0,0 @@
-/**
-bla bla bla
-and bla bla
-*/
-
-// my wonderful code... with an error
-
-foo.bla = 456;
