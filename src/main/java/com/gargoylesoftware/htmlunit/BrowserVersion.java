2395bf3ccc6a18e94f87a8178f458cd316f82f8e
rbri <rbri@rbri.de>
2017-03-15 20:14:12 +0000

    Latest chrome
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@13751 5f5364db-9458-4db8-a492-e30667be6df6

--- src/main/java/com/gargoylesoftware/htmlunit/BrowserVersion.java
+++ src/main/java/com/gargoylesoftware/htmlunit/BrowserVersion.java
@@ -132,10 +132,10 @@ public class BrowserVersion implements Serializable, Cloneable {
     /** Latest Chrome. */
     public static final BrowserVersion CHROME = new BrowserVersion(
         NETSCAPE, "5.0 (Windows NT 6.1) AppleWebKit/537.36"
-        + " (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36",
+        + " (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36",
         "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36"
-        + " (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36",
-        56, "Chrome", null);
+        + " (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36",
+        57, "Chrome", null);
 
     /** Microsoft Edge. Work In Progress!!! */
     public static final BrowserVersion EDGE = new BrowserVersion(
