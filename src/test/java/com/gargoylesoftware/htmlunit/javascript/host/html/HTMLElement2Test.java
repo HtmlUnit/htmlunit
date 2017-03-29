22fe44628b8cab81a3d14c85644b944e4e1f6571
asashour <asashour@yahoo.com>
2017-03-29 14:58:27 +0000

    Add @NYI
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@13938 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLElement2Test.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLElement2Test.java
@@ -1360,7 +1360,8 @@ public class HTMLElement2Test extends WebDriverTestCase {
     @Alerts(DEFAULT = {"body1", "setActive not available"},
             IE = {"body1", "text1", "[object HTMLButtonElement]", "text2", "[object Window]", "onfocus text2"})
     @BuggyWebDriver(IE)
-    @NotYetImplemented(IE)
+    // alert conflicts with focus/blur 
+    @NotYetImplemented
     public void setActiveAndFocus() throws Exception {
         final String firstHtml =
             HtmlPageTest.STANDARDS_MODE_PREFIX_
