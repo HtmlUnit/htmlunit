99526dcf61eaabb0b8331337bb38f996b7718967
asashour <asashour@yahoo.com>
2017-04-03 07:49:30 +0000

    checkstyle
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@13971 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/event/KeyboardEventTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/event/KeyboardEventTest.java
@@ -131,7 +131,7 @@ public class KeyboardEventTest extends WebDriverTestCase {
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts({"32, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, "})
+    @Alerts("32, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, ")
     public void keyCodes_keyup() throws Exception {
         final String html = "<html><head>\n"
             + "<script>\n"
@@ -158,8 +158,8 @@ public class KeyboardEventTest extends WebDriverTestCase {
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts({"65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, "
-        + "82, 83, 84, 85, 86, 87, 88, 89, 90, "})
+    @Alerts("65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, "
+        + "82, 83, 84, 85, 86, 87, 88, 89, 90, ")
     public void keyCodes2_keyup() throws Exception {
         final String html = "<html><head>\n"
             + "<script>\n"
@@ -186,7 +186,7 @@ public class KeyboardEventTest extends WebDriverTestCase {
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts({"32, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, "})
+    @Alerts("32, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, ")
     public void keyCodes_keydown() throws Exception {
         final String html = "<html><head>\n"
             + "<script>\n"
@@ -213,8 +213,8 @@ public class KeyboardEventTest extends WebDriverTestCase {
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts({"65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, "
-                + "80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, "})
+    @Alerts("65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, "
+                + "80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, ")
     public void keyCodes2_keydown() throws Exception {
         final String html = "<html><head>\n"
             + "<script>\n"
@@ -241,7 +241,7 @@ public class KeyboardEventTest extends WebDriverTestCase {
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts({"32, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, "})
+    @Alerts("32, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, ")
     public void keyCodes_keypress() throws Exception {
         final String html = "<html><head>\n"
             + "<script>\n"
@@ -268,10 +268,10 @@ public class KeyboardEventTest extends WebDriverTestCase {
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts({"97, 98, 99, "
+    @Alerts("97, 98, 99, "
             + "100, 101, 102, 103, 104, 105, 106, 107, 108, 109, "
             + "110, 111, 112, 113, 114, 115, 116, 117, 118, 119, "
-            + "120, 121, 122, "})
+            + "120, 121, 122, ")
     public void keyCodes2_keypress() throws Exception {
         final String html = "<html><head>\n"
             + "<script>\n"
