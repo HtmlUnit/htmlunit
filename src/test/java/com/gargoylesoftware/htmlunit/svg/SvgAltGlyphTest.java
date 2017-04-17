27179db0dd32cfee1acf20370228b996b2c81ed0
rbri <rbri@rbri.de>
2017-04-17 08:28:32 +0000

    FF52
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@14130 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/svg/SvgAltGlyphTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/svg/SvgAltGlyphTest.java
@@ -39,7 +39,7 @@ public class SvgAltGlyphTest extends WebDriverTestCase {
      */
     @Test
     @Alerts(DEFAULT = "[object SVGElement]",
-            FF = "[object SVGAltGlyphElement]")
+            FF45 = "[object SVGAltGlyphElement]")
     public void simpleScriptable() throws Exception {
         final String html = HtmlPageTest.STANDARDS_MODE_PREFIX_
             + "<html><head>\n"
