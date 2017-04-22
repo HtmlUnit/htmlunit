9fbae117b0126768b4061b8a81c26026d9e53490
asashour <asashour@yahoo.com>
2017-04-22 12:13:47 +0000

    checkstyle
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@14171 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/SimpleWebTestCase.java
+++ src/test/java/com/gargoylesoftware/htmlunit/SimpleWebTestCase.java
@@ -16,9 +16,7 @@ package com.gargoylesoftware.htmlunit;
 
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.junit.After;
 import org.junit.Before;
