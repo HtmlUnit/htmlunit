fc43d9493b698f831e626d60dbd4fce3864ef086
rbri <rbri@rbri.de>
2017-04-28 13:08:36 +0000

    ff52 expectations
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@14293 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/libraries/MooTools121Test.java
+++ src/test/java/com/gargoylesoftware/htmlunit/libraries/MooTools121Test.java
@@ -68,12 +68,12 @@ public class MooTools121Test extends WebDriverTestCase {
     /**
      * @throws Exception if an error occurs
      */
-    @Alerts(CHROME = {"364", "1", "0",
+    @Alerts(DEFAULT = {"364", "1", "0",
                     "should return the function bound to an object with multiple arguments"},
             IE = {"364", "2", "0",
                     "should return the function bound to an object with multiple arguments",
                     "should return a CSS string representing the Element's styles"},
-            FF = {"364", "2", "0",
+            FF45 = {"364", "2", "0",
                     "should return true if the string constains the string and separator otherwise false",
                     "should return the function bound to an object with multiple arguments"})
     @Test
