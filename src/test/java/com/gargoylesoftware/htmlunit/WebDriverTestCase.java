a6e17fa39ec68d964d6f14b8179d5cf7579d8688
rbri <rbri@rbri.de>
2017-05-07 11:33:20 +0000

    start supporting FF52
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@14406 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/WebDriverTestCase.java
+++ src/test/java/com/gargoylesoftware/htmlunit/WebDriverTestCase.java
@@ -142,8 +142,8 @@ public abstract class WebDriverTestCase extends WebTestCase {
     /**
      * Browsers which run by default.
      */
-    public static BrowserVersion[] DEFAULT_RUNNING_BROWSERS_ = {BrowserVersion.CHROME, BrowserVersion.FIREFOX_45,
-        BrowserVersion.INTERNET_EXPLORER};
+    public static BrowserVersion[] DEFAULT_RUNNING_BROWSERS_ =
+        {BrowserVersion.CHROME, BrowserVersion.FIREFOX_45, BrowserVersion.FIREFOX_52, BrowserVersion.INTERNET_EXPLORER};
 
     private static final Log LOG = LogFactory.getLog(WebDriverTestCase.class);
 
