46e8689443ad876c79a72705c053d6834943d324
rbri <rbri@rbri.de>
2017-05-12 17:30:22 +0000

    revert last cleanup, ff is really strange with keygen
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@14441 5f5364db-9458-4db8-a492-e30667be6df6

--- src/main/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLSpanElement.java
+++ src/main/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLSpanElement.java
@@ -57,6 +57,7 @@ public class HTMLSpanElement extends HTMLElement {
         if (browser.hasFeature(HTMLBASEFONT_END_TAG_FORBIDDEN)) {
             switch (domNode.getLocalName().toLowerCase(Locale.ROOT)) {
                 case "basefont":
+                case "keygen":
                     endTagForbidden_ = true;
                     break;
                 default:
