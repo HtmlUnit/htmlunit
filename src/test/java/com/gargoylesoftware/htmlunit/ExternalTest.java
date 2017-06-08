520d08759d48d00c6deda6030312bfc6be58e55c
rbri <rbri@rbri.de>
2017-06-08 19:02:32 +0000

    chrome driver update
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@14580 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/ExternalTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/ExternalTest.java
@@ -48,7 +48,7 @@ import com.gargoylesoftware.htmlunit.xml.XmlPage;
 public class ExternalTest {
 
     /** Chrome driver. */
-    static String CHROME_DRIVER_ = "2.29";
+    static String CHROME_DRIVER_ = "2.30";
 
     private static final DateFormat TEAM_CITY_FORMAT_ = new SimpleDateFormat("dd MMM yy HH:mm", Locale.ROOT);
 
