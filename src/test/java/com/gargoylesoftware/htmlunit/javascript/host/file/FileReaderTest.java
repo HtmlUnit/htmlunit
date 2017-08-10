25cbec045ff72cb6855919e81e89b7cbfcadffca
rbri <rbri@rbri.de>
2017-08-10 18:16:25 +0000

    test case for missing reaAsDataUrl function
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@14780 5f5364db-9458-4db8-a492-e30667be6df6

---
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/file/FileReaderTest.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2002-2017 Gargoyle Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.gargoylesoftware.htmlunit.javascript.host.file;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.gargoylesoftware.htmlunit.BrowserRunner;
+import com.gargoylesoftware.htmlunit.WebDriverTestCase;
+import com.gargoylesoftware.htmlunit.BrowserRunner.Alerts;
+import com.gargoylesoftware.htmlunit.BrowserRunner.NotYetImplemented;
+import com.gargoylesoftware.htmlunit.html.HtmlPageTest;
+
+/**
+ * Tests for {@link FileReader}.
+ *
+ * @author Ronald Brill
+ */
+@RunWith(BrowserRunner.class)
+public class FileReaderTest extends WebDriverTestCase {
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts(DEFAULT = "data:;base64,SHRtbFVuaXQ=",
+            FF = "data:application/octet-stream;base64,SHRtbFVuaXQ=")
+    @NotYetImplemented
+    public void readAsDataURL() throws Exception {
+        final String html
+            = HtmlPageTest.STANDARDS_MODE_PREFIX_
+            + "<html>\n"
+            + "<head>\n"
+            + "  <script>\n"
+            + "    function test() {\n"
+            + "      var reader = new FileReader();\n"
+            + "      reader.onload = function() {\n"
+            + "        var dataURL = reader.result;\n"
+            + "        alert(dataURL);\n"
+            + "      };\n"
+            + "      var files = document.testForm.fileupload.files;\n"
+            + "      reader.readAsDataURL(files[0]);\n"
+            + "    }\n"
+            + "  </script>\n"
+            + "<head>\n"
+            + "<body>\n"
+            + "  <form name='testForm'>\n"
+            + "    <input type='file' id='fileupload' name='fileupload'>\n"
+            + "  </form>\n"
+            + "  <button id='testBtn' onclick='test()'>Tester</button>\n"
+            + "</body>\n"
+            + "</html>";
+
+        final WebDriver driver = loadPage2(html);
+
+        final File tstFile = File.createTempFile("HtmlUnitReadAsDataURLTest", ".txt");
+        try {
+            FileUtils.write(tstFile, "HtmlUnit", StandardCharsets.UTF_8);
+
+            final String path = tstFile.getCanonicalPath();
+            driver.findElement(By.name("fileupload")).sendKeys(path);
+
+            driver.findElement(By.id("testBtn")).click();
+
+            verifyAlerts(driver, getExpectedAlerts());
+        }
+        finally {
+            FileUtils.deleteQuietly(tstFile);
+        }
+    }
+}
