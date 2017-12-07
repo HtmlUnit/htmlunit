34c9d2b70c5532f808c78d41c135bffce0112cf5
rbri <rbri@rbri.de>
2017-12-07 16:34:47 +0000

    more tests for Number functions added
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@14992 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/NativeNumberTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/NativeNumberTest.java
@@ -19,6 +19,8 @@ import org.junit.runner.RunWith;
 
 import com.gargoylesoftware.htmlunit.BrowserRunner;
 import com.gargoylesoftware.htmlunit.BrowserRunner.Alerts;
+import com.gargoylesoftware.htmlunit.BrowserRunner.NotYetImplemented;
+import com.gargoylesoftware.htmlunit.BrowserRunner.TestedBrowser;
 import com.gargoylesoftware.htmlunit.WebDriverTestCase;
 
 /**
@@ -30,6 +32,199 @@ import com.gargoylesoftware.htmlunit.WebDriverTestCase;
  */
 @RunWith(BrowserRunner.class)
 public class NativeNumberTest extends WebDriverTestCase {
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts(DEFAULT = {"false", "false", "false", "true", "true", "false", "false"},
+            IE = "no Number.isFinite")
+    @NotYetImplemented(TestedBrowser.IE)
+    public void isFinite() throws Exception {
+        final String html =
+            "<html>\n"
+            + "<head>\n"
+            + "  <script>\n"
+            + "    if (Number.isFinite === undefined) {\n"
+            + "      alert('no Number.isFinite');\n"
+            + "    } else {\n"
+            + "      alert(Number.isFinite(Infinity));\n"
+            + "      alert(Number.isFinite(NaN));\n"
+            + "      alert(Number.isFinite(-Infinity));\n"
+            + "      alert(Number.isFinite(0));\n"
+            + "      alert(Number.isFinite(2e64));\n"
+            + "      alert(Number.isFinite('0'));\n"
+            + "      alert(Number.isFinite(null));\n"
+            + "    }\n"
+            + "  </script>\n"
+            + "</head>\n"
+            + "<body>\n"
+            + "</body>\n"
+            + "</html>";
+        loadPageWithAlerts2(html);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts(DEFAULT = {"true", "true", "true", "false", "false", "false", "false",
+                "false", "false", "false", "false", "false"},
+            IE = "no Number.isInteger")
+    @NotYetImplemented(TestedBrowser.IE)
+    public void isInteger() throws Exception {
+        final String html =
+            "<html>\n"
+            + "<head>\n"
+            + "  <script>\n"
+            + "    if (Number.isInteger === undefined) {\n"
+            + "      alert('no Number.isInteger');\n"
+            + "    } else {\n"
+            + "      alert(Number.isInteger(0));\n"
+            + "      alert(Number.isInteger(1));\n"
+            + "      alert(Number.isInteger(-100000));\n"
+
+            + "      alert(Number.isInteger(0.1));\n"
+            + "      alert(Number.isInteger(Math.PI));\n"
+
+            + "      alert(Number.isInteger(NaN));\n"
+            + "      alert(Number.isInteger(Infinity));\n"
+            + "      alert(Number.isInteger(-Infinity));\n"
+            + "      alert(Number.isInteger('10'));\n"
+            + "      alert(Number.isInteger(true));\n"
+            + "      alert(Number.isInteger(false));\n"
+            + "      alert(Number.isInteger([1]));\n"
+            + "    }\n"
+            + "  </script>\n"
+            + "</head>\n"
+            + "<body>\n"
+            + "</body>\n"
+            + "</html>";
+        loadPageWithAlerts2(html);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts(DEFAULT = {"true", "true", "true", "false", "false", "false", "false",
+                "false", "false", "false", "false", "false", "false", "false"},
+            IE = "no Number.isNaN")
+    @NotYetImplemented(TestedBrowser.IE)
+    public void isNaN() throws Exception {
+        final String html =
+            "<html>\n"
+            + "<head>\n"
+            + "  <script>\n"
+            + "    if (Number.isNaN === undefined) {\n"
+            + "      alert('no Number.isNaN');\n"
+            + "    } else {\n"
+            + "      alert(Number.isNaN(NaN));\n"
+            + "      alert(Number.isNaN(Number.NaN));\n"
+            + "      alert(Number.isNaN(0 / 0));\n"
+
+            + "      alert(Number.isNaN('NaN'));\n"
+            + "      alert(Number.isNaN(undefined));\n"
+            + "      alert(Number.isNaN({}));\n"
+            + "      alert(Number.isNaN('blabla'));\n"
+
+            + "      alert(Number.isNaN(true));\n"
+            + "      alert(Number.isNaN(null));\n"
+            + "      alert(Number.isNaN(37));\n"
+            + "      alert(Number.isNaN('37'));\n"
+            + "      alert(Number.isNaN('37.37'));\n"
+            + "      alert(Number.isNaN(''));\n"
+            + "      alert(Number.isNaN(' '));\n"
+            + "    }\n"
+            + "  </script>\n"
+            + "</head>\n"
+            + "<body>\n"
+            + "</body>\n"
+            + "</html>";
+        loadPageWithAlerts2(html);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts(DEFAULT = {"true", "false", "true", "false", "false", "false", "false", "true"},
+            IE = "no Number.isSafeInteger")
+    @NotYetImplemented(TestedBrowser.IE)
+    public void isSafeInteger() throws Exception {
+        final String html =
+            "<html>\n"
+            + "<head>\n"
+            + "  <script>\n"
+            + "    if (Number.isSafeInteger === undefined) {\n"
+            + "      alert('no Number.isSafeInteger');\n"
+            + "    } else {\n"
+            + "      alert(Number.isSafeInteger(3));\n"
+            + "      alert(Number.isSafeInteger(Math.pow(2, 53)));\n"
+            + "      alert(Number.isSafeInteger(Math.pow(2, 53) - 1));\n"
+            + "      alert(Number.isSafeInteger(NaN));\n"
+            + "      alert(Number.isSafeInteger(Infinity));\n"
+            + "      alert(Number.isSafeInteger('3'));\n"
+            + "      alert(Number.isSafeInteger(3.1));\n"
+            + "      alert(Number.isSafeInteger(3.0));\n"
+            + "    }\n"
+            + "  </script>\n"
+            + "</head>\n"
+            + "<body>\n"
+            + "</body>\n"
+            + "</html>";
+        loadPageWithAlerts2(html);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts(DEFAULT = "3.14",
+            IE = "no Number.parseFloat")
+    @NotYetImplemented(TestedBrowser.IE)
+    public void parseFloat() throws Exception {
+        final String html =
+            "<html>\n"
+            + "<head>\n"
+            + "  <script>\n"
+            + "    if (Number.parseFloat === undefined) {\n"
+            + "      alert('no Number.parseFloat');\n"
+            + "    } else {\n"
+            + "      alert(Number.parseFloat('3.14'));\n"
+            + "    }\n"
+            + "  </script>\n"
+            + "</head>\n"
+            + "<body>\n"
+            + "</body>\n"
+            + "</html>";
+        loadPageWithAlerts2(html);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts(DEFAULT = "4",
+            IE = "no Number.parseInt")
+    @NotYetImplemented(TestedBrowser.IE)
+    public void parseInt() throws Exception {
+        final String html =
+            "<html>\n"
+            + "<head>\n"
+            + "  <script>\n"
+            + "    if (Number.parseInt === undefined) {\n"
+            + "      alert('no Number.parseInt');\n"
+            + "    } else {\n"
+            + "      alert(Number.parseInt('4'));\n"
+            + "    }\n"
+            + "  </script>\n"
+            + "</head>\n"
+            + "<body>\n"
+            + "</body>\n"
+            + "</html>";
+        loadPageWithAlerts2(html);
+    }
 
     /**
      * Test for the methods with the same expectations for all browsers.
