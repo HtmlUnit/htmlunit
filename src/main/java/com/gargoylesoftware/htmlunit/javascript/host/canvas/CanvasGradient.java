8d64258be42c2a2731c3df59490e6a00d8d321ff
rbri <rbri@rbri.de>
2017-12-19 21:01:12 +0000

    fix javadoc
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@15026 5f5364db-9458-4db8-a492-e30667be6df6

--- src/main/java/com/gargoylesoftware/htmlunit/javascript/host/canvas/CanvasGradient.java
+++ src/main/java/com/gargoylesoftware/htmlunit/javascript/host/canvas/CanvasGradient.java
@@ -43,8 +43,8 @@ public class CanvasGradient extends SimpleScriptable {
      * Adds a new stop, defined by an offset and a color, to the gradient.
      * @param offset A number between 0 and 1. An INDEX_SIZE_ERR is raised,
      * if the number is not in that range.
-     * @param color A CSS <color>. A SYNTAX_ERR is raised, if the value
-     * can not be parsed as a CSS <color> value.
+     * @param color A CSS color. A SYNTAX_ERR is raised, if the value
+     * can not be parsed as a CSS color value.
      */
     @JsxFunction
     public void addColorStop(final double offset, final String color) {
