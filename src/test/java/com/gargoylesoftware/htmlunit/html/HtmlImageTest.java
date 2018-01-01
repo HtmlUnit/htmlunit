f09d5b57f5bbae5c2e8ca46235a07f924cfcf69f
rbri <rbri@rbri.de>
2018-01-01 12:07:11 +0000

    Happy New Year!
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk/htmlunit@15055 5f5364db-9458-4db8-a492-e30667be6df6

--- src/test/java/com/gargoylesoftware/htmlunit/html/HtmlImageTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/html/HtmlImageTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002-2017 Gargoyle Software Inc.
+ * Copyright (c) 2002-2018 Gargoyle Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
