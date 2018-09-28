573c8dca78535d7b28fc23b51b73e8afc1c3c5dd
rbri <rbri@rbri.de>
2018-09-28 17:23:48 +0200

    and again bug hunting for strange effects on the server

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/xml/XMLHttpRequest3Test.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/xml/XMLHttpRequest3Test.java
@@ -393,6 +393,7 @@ public class XMLHttpRequest3Test extends WebServerTestCase {
         startWebServer("./", null, servlets);
 
         collectedHeaders_.clear();
+System.out.println("------------------------------------------");
         final WebClient client = getWebClient();
 
         final List<String> collectedAlerts = Collections.synchronizedList(new ArrayList<String>());
@@ -404,6 +405,7 @@ public class XMLHttpRequest3Test extends WebServerTestCase {
 
         assertEquals(0, client.waitForBackgroundJavaScript(DEFAULT_WAIT_TIME));
 
+System.out.println("------------------------------------------ " + collectedHeaders_.toString());
         assertEquals(collectedHeaders_.toString(), 2, collectedHeaders_.size());
 
         String headers = collectedHeaders_.get(0);
@@ -470,7 +472,9 @@ public class XMLHttpRequest3Test extends WebServerTestCase {
          */
         @Override
         protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
+System.out.println("AjaxHeaderServlet 1");
             final String header = headers(request);
+System.out.println("AjaxHeaderServlet 2" + header);
             try {
                 // do not return before the form request is also sent
                 Thread.sleep(666);
@@ -479,10 +483,13 @@ public class XMLHttpRequest3Test extends WebServerTestCase {
                 e.printStackTrace();
             }
 
+System.out.println("AjaxHeaderServlet 3");
             collectedHeaders_.add("Ajax: " + header);
             response.setContentType("text/plain");
             final Writer writer = response.getWriter();
             writer.write(header);
+            writer.flush();
+System.out.println("AjaxHeaderServlet 4");
         }
     }
 
@@ -503,7 +510,9 @@ public class XMLHttpRequest3Test extends WebServerTestCase {
          */
         @Override
         protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
+System.out.println("FormHeaderServlet 1");
             final String header = headers(request);
+System.out.println("FormHeaderServlet 2" + header);
 
             final String html = "<html><head></head>\n"
                     + "<body>\n"
@@ -514,6 +523,8 @@ public class XMLHttpRequest3Test extends WebServerTestCase {
             response.setContentType("text/html");
             final Writer writer = response.getWriter();
             writer.write(html);
+            writer.flush();
+System.out.println("FormHeaderServlet 3");
         }
     }
 
