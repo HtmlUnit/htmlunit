98f4dbcdf3f6421cb482b26d574999f96f27eeb2
rbri <rbri@rbri.de>
2019-03-23 16:50:52 +0100

    cleanup

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/media/OfflineAudioContextTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/media/OfflineAudioContextTest.java
@@ -14,15 +14,11 @@
  */
 package com.gargoylesoftware.htmlunit.javascript.host.media;
 
-import static com.gargoylesoftware.htmlunit.BrowserRunner.TestedBrowser.CHROME;
-import static com.gargoylesoftware.htmlunit.BrowserRunner.TestedBrowser.FF;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.gargoylesoftware.htmlunit.BrowserRunner;
 import com.gargoylesoftware.htmlunit.BrowserRunner.Alerts;
-import com.gargoylesoftware.htmlunit.BrowserRunner.NotYetImplemented;
 import com.gargoylesoftware.htmlunit.WebDriverTestCase;
 
 /**
