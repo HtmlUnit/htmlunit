8df8faab7cd276984dd2bbf1e3ad032273c34039
rbri <rbri@rbri.de>
2019-07-21 12:25:50 +0200

    fix refactoring mistake

--- src/main/java/com/gargoylesoftware/htmlunit/html/xpath/XPathHelper.java
+++ src/main/java/com/gargoylesoftware/htmlunit/html/xpath/XPathHelper.java
@@ -56,12 +56,12 @@ public final class XPathHelper {
      * @param node the node to start searching from
      * @param xpathExpr the XPath expression
      * @param resolver the prefix resolver to use for resolving namespace prefixes, or null
-     * @param caseSensitive true if the browser handles tag names case sensitive
+     * @param caseSensitiveXpathAttributes true if the browser handles xpath attribute names case sensitive
      * @return the list of objects found
      */
     @SuppressWarnings("unchecked")
     public static <T> List<T> getByXPath(final DomNode node, final String xpathExpr,
-            final PrefixResolver resolver, final boolean caseSensitive) {
+            final PrefixResolver resolver, final boolean caseSensitiveXpathAttributes) {
         if (xpathExpr == null) {
             throw new IllegalArgumentException("Null is not a valid XPath expression");
         }
@@ -69,7 +69,7 @@ public final class XPathHelper {
         PROCESS_XPATH_.set(Boolean.TRUE);
         final List<T> list = new ArrayList<>();
         try {
-            final XObject result = evaluateXPath(node, xpathExpr, resolver, caseSensitive);
+            final XObject result = evaluateXPath(node, xpathExpr, resolver, caseSensitiveXpathAttributes);
 
             if (result instanceof XNodeSet) {
                 final NodeList nodelist = ((XNodeSet) result).nodelist();
@@ -112,13 +112,13 @@ public final class XPathHelper {
      * @param contextNode the node to start searching from
      * @param str a valid XPath string
      * @param a prefix resolver to use for resolving namespace prefixes, or null
-     * @param caseSensitive true if the browser handles tag names case sensitive
+     * @param caseSensitiveXpathAttributes true if the browser handles xpath attribute names case sensitive
      * @return an XObject, which can be used to obtain a string, number, nodelist, etc (should never be {@code null})
      * @throws TransformerException if a syntax or other error occurs
      */
     private static XObject evaluateXPath(final DomNode contextNode,
             final String str, final PrefixResolver prefixResolver,
-            final boolean caseSensitive) throws TransformerException {
+            final boolean caseSensitiveXpathAttributes) throws TransformerException {
         final XPathContext xpathSupport = new XPathContext();
         final Node xpathExpressionContext;
         if (contextNode.getNodeType() == Node.DOCUMENT_NODE) {
@@ -133,7 +133,9 @@ public final class XPathHelper {
             resolver = new HtmlUnitPrefixResolver(xpathExpressionContext);
         }
 
-        final boolean attributeCaseSensitive = contextNode.getPage().hasCaseSensitiveTagNames() || caseSensitive;
+        final boolean caseSensitive = contextNode.getPage().hasCaseSensitiveTagNames();
+        final boolean attributeCaseSensitive = caseSensitive || caseSensitiveXpathAttributes;
+
         final XPathAdapter xpath = new XPathAdapter(str, null, resolver, null, caseSensitive, attributeCaseSensitive);
         final int ctxtNode = xpathSupport.getDTMHandleFromNode(contextNode);
         return xpath.execute(xpathSupport, ctxtNode, prefixResolver);
