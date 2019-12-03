87eed93791533d79091074b151addecaf9fdc92a
rbri <rbri@rbri.de>
2019-12-03 17:21:37 +0100

    No longer use annotations for the implementation of isReadOnlySettable to make the code simpler and faster - fixes #107

--- src/main/java/com/gargoylesoftware/htmlunit/javascript/host/dom/Document.java
+++ src/main/java/com/gargoylesoftware/htmlunit/javascript/host/dom/Document.java
@@ -4149,4 +4149,12 @@ public class Document extends Node {
         }
         return fonts_;
     }
+
+    @Override
+    protected boolean isReadOnlySettable(final String name, final Object value) {
+        if ("body".equals(name)) {
+            throw ScriptRuntime.typeError3("msg.set.prop.no.setter", name, getClassName(), Context.toString(value));
+        }
+        return super.isReadOnlySettable(name, value);
+    }
 }
