f8eb2761a26e1c5df50cca1a7c99254035e1941b
rbri <rbri@rbri.de>
2019-12-14 16:30:16 +0100

    code style

--- src/main/java/com/gargoylesoftware/htmlunit/BrowserVersionFeatures.java
+++ src/main/java/com/gargoylesoftware/htmlunit/BrowserVersionFeatures.java
@@ -555,13 +555,13 @@ public enum BrowserVersionFeatures {
     @BrowserFeature({CHROME, FF})
     HTTP_COOKIE_START_DATE_1970,
 
-    /** Browser sends Upgrade-Insecure-Requests header. */
-    @BrowserFeature({CHROME, FF})
-    HTTP_HEADER_UPGRADE_INSECURE_REQUEST,
-
     /** Browser sends Sec-Fetch headers. */
     @BrowserFeature(CHROME)
     HTTP_HEADER_SEC_FETCH,
+
+    /** Browser sends Upgrade-Insecure-Requests header. */
+    @BrowserFeature({CHROME, FF})
+    HTTP_HEADER_UPGRADE_INSECURE_REQUEST,
 
     /** Supports redirect via 308 code. */
     @BrowserFeature({CHROME, FF})
