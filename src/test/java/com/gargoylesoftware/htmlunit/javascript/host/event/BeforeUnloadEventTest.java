0bf891162d5e6f61145c0dea01e2aebff858dc4a
rbri <rbri@rbri.de>
2020-02-03 11:54:45 +0100

    code style fixes

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/event/BeforeUnloadEventTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/event/BeforeUnloadEventTest.java
@@ -140,8 +140,8 @@ public class BeforeUnloadEventTest extends WebDriverTestCase {
     @Test
     @Alerts(DEFAULT = {"-[object Event]", "-beforeunload", "-true", "-false", "-undefined"},
             CHROME = {"-[object Event]", "-beforeunload", "-true", "-false", "-true"},
-            FF68 = {"-[object Event]", "-beforeunload", "-true", "-false", "-true"},
-            FF = {"-[object Event]", "-beforeunload", "-true", "-false", "-true"})
+            FF = {"-[object Event]", "-beforeunload", "-true", "-false", "-true"},
+            FF68 = {"-[object Event]", "-beforeunload", "-true", "-false", "-true"})
     public void dispatchEvent_event() throws Exception {
         final String html = HtmlPageTest.STANDARDS_MODE_PREFIX_
             + "<html><head><script>\n"
