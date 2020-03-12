4aba5a90637d0b0107417c26d8815de50f395126
rbri <rbri@rbri.de>
2020-03-12 12:24:12 +0100

    retested with all supported browsers

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/css/property/ElementOffsetWidthTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/css/property/ElementOffsetWidthTest.java
@@ -15,6 +15,7 @@
 package com.gargoylesoftware.htmlunit.javascript.host.css.property;
 
 import static com.gargoylesoftware.htmlunit.BrowserRunner.TestedBrowser.CHROME;
+import static com.gargoylesoftware.htmlunit.BrowserRunner.TestedBrowser.FF;
 import static com.gargoylesoftware.htmlunit.BrowserRunner.TestedBrowser.FF60;
 import static com.gargoylesoftware.htmlunit.BrowserRunner.TestedBrowser.FF68;
 import static com.gargoylesoftware.htmlunit.BrowserRunner.TestedBrowser.IE;
@@ -480,7 +481,7 @@ public class ElementOffsetWidthTest extends WebDriverTestCase {
     @Alerts(DEFAULT = "-56",
             FF68 = "0",
             FF60 = "0")
-    @NotYetImplemented({CHROME, IE})
+    @NotYetImplemented({CHROME, FF, IE})
     public void dd() throws Exception {
         loadPageWithAlerts2(test("dd"));
     }
@@ -986,7 +987,7 @@ public class ElementOffsetWidthTest extends WebDriverTestCase {
      */
     @Test
     @Alerts("-16")
-    @NotYetImplemented({CHROME, FF68, FF60})
+    @NotYetImplemented({CHROME, FF, FF68, FF60})
     public void marquee() throws Exception {
         loadPageWithAlerts2(test("marquee"));
     }
@@ -1043,7 +1044,7 @@ public class ElementOffsetWidthTest extends WebDriverTestCase {
     @Test
     @Alerts(DEFAULT = "80",
             IE = "0")
-    @NotYetImplemented({CHROME, FF68, FF60})
+    @NotYetImplemented({CHROME, FF, FF68, FF60})
     public void meter() throws Exception {
         loadPageWithAlerts2(test("meter"));
     }
@@ -1251,6 +1252,8 @@ public class ElementOffsetWidthTest extends WebDriverTestCase {
     @Test
     @Alerts(DEFAULT = "160",
             FF = "162",
+            FF68 = "162",
+            FF60 = "162",
             IE = "280")
     @NotYetImplemented
     public void progress() throws Exception {
