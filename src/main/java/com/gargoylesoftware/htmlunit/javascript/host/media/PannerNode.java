0bdca286d2a5e06a9cf43aafbc834e0c87288577
rbri <rbri@rbri.de>
2020-03-12 08:03:08 +0100

    working on FF-latest support

--- src/main/java/com/gargoylesoftware/htmlunit/javascript/host/media/PannerNode.java
+++ src/main/java/com/gargoylesoftware/htmlunit/javascript/host/media/PannerNode.java
@@ -15,6 +15,7 @@
 package com.gargoylesoftware.htmlunit.javascript.host.media;
 
 import static com.gargoylesoftware.htmlunit.javascript.configuration.SupportedBrowser.CHROME;
+import static com.gargoylesoftware.htmlunit.javascript.configuration.SupportedBrowser.FF;
 import static com.gargoylesoftware.htmlunit.javascript.configuration.SupportedBrowser.FF60;
 import static com.gargoylesoftware.htmlunit.javascript.configuration.SupportedBrowser.FF68;
 
@@ -25,6 +26,7 @@ import com.gargoylesoftware.htmlunit.javascript.configuration.JsxConstructor;
  * A JavaScript object for {@code PannerNode}.
  *
  * @author Ahmed Ashour
+ * @author Ronald Brill
  */
 @JsxClass({CHROME, FF, FF68, FF60})
 public class PannerNode extends AudioNode {
