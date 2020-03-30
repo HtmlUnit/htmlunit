3931f473e5ad400dfe8b704be78a0b65ad139278
rbri <rbri@rbri.de>
2020-03-30 20:30:40 +0200

    test cases for #150

---
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/worker/DedicatedWorkerGlobalScopeTest.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2002-2020 Gargoyle Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.gargoylesoftware.htmlunit.javascript.host.worker;
+
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.gargoylesoftware.htmlunit.BrowserRunner;
+import com.gargoylesoftware.htmlunit.BrowserRunner.Alerts;
+import com.gargoylesoftware.htmlunit.WebDriverTestCase;
+
+/**
+ * Unit tests for {@code DedicatedWorkerGlobalScope}.
+ *
+ * @author Ronald Brill
+ */
+@RunWith(BrowserRunner.class)
+public class DedicatedWorkerGlobalScopeTest extends WebDriverTestCase {
+
+    /**
+     * Closes the real ie because clearing all cookies seem to be not working
+     * at the moment.
+     */
+    @After
+    public void shutDownRealBrowsersAfter() {
+        shutDownRealIE();
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts("Received: Result = 15")
+    public void onmessage() throws Exception {
+        final String html = "<html><body>"
+            + "<script>\n"
+            + "try {\n"
+            + "  var myWorker = new Worker('worker.js');\n"
+            + "  myWorker.onmessage = function(e) {\n"
+            + "    alert('Received: ' + e.data);\n"
+            + "  };\n"
+            + "  setTimeout(function() { myWorker.postMessage([5, 3]);}, 10);\n"
+            + "} catch(e) { alert('exception'); }\n"
+            + "</script></body></html>\n";
+
+        final String workerJs = "onmessage = function(e) {\n"
+                + "  var workerResult = 'Result = ' + (e.data[0] * e.data[1]);\n"
+                + "  postMessage(workerResult);\n"
+                + "}\n";
+
+        getMockWebConnection().setResponse(new URL(URL_FIRST, "worker.js"), workerJs);
+
+        loadPageWithAlerts2(html, 2000);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts("Received: Result = 15")
+    public void selfOnmessage() throws Exception {
+        final String html = "<html><body><script>\n"
+            + "try {\n"
+            + "  var myWorker = new Worker('worker.js');\n"
+            + "  myWorker.onmessage = function(e) {\n"
+            + "    alert('Received: ' + e.data);\n"
+            + "  };\n"
+            + "  setTimeout(function() { myWorker.postMessage([5, 3]);}, 10);\n"
+            + "} catch(e) { alert('exception'); }\n"
+            + "</script></body></html>\n";
+
+        final String workerJs = "self.onmessage = function(e) {\n"
+                + "  var workerResult = 'Result = ' + (e.data[0] * e.data[1]);\n"
+                + "  postMessage(workerResult);\n"
+                + "}\n";
+
+        getMockWebConnection().setResponse(new URL(URL_FIRST, "worker.js"), workerJs);
+
+        loadPageWithAlerts2(html, 2000);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts("Received: Result = 15")
+    public void selfAddEventListener() throws Exception {
+        final String html = "<html><body><script>\n"
+            + "try {\n"
+            + "  var myWorker = new Worker('worker.js');\n"
+            + "  myWorker.onmessage = function(e) {\n"
+            + "    alert('Received: ' + e.data);\n"
+            + "  };\n"
+            + "  setTimeout(function() { myWorker.postMessage([5, 3]);}, 10);\n"
+            + "} catch(e) { alert('exception'); }\n"
+            + "</script></body></html>\n";
+
+        final String workerJs = "self.addEventListener('message', (e) => {\n"
+                + "  var workerResult = 'Result = ' + (e.data[0] * e.data[1]);\n"
+                + "  postMessage(workerResult);\n"
+                + "});\n";
+
+        getMockWebConnection().setResponse(new URL(URL_FIRST, "worker.js"), workerJs);
+
+        loadPageWithAlerts2(html, 2000);
+    }
+}
