2a58b8107c2aa543ad8bad9d5ed1b2bb89222e89
Ronald Brill <rbri@rbri.de>
2020-07-30 18:57:07 +0200

    lib update

--- src/test/java/com/gargoylesoftware/htmlunit/ExternalTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/ExternalTest.java
@@ -58,7 +58,7 @@ public class ExternalTest {
     static String EDGE_DRIVER_URL_ = "https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/";
 
     /** Gecko driver. */
-    static String GECKO_DRIVER_ = "0.26.0";
+    static String GECKO_DRIVER_ = "0.27.0";
     /** IE driver. */
     static String IE_DRIVER_ = "3.150.1.0";
 
@@ -175,7 +175,7 @@ public class ExternalTest {
             try {
                 final HtmlPage page = webClient.getPage("https://github.com/mozilla/geckodriver/releases/latest");
                 final DomNodeList<DomNode> divs = page.querySelectorAll(".release-header div");
-                assertEquals("Gecko Driver", divs.get(0).asText(), "v" + GECKO_DRIVER_);
+                assertEquals("Gecko Driver", divs.get(0).asText(), GECKO_DRIVER_);
             }
             catch (final FailingHttpStatusCodeException e) {
                 // ignore
