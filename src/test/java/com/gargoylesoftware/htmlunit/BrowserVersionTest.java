3bb4b9e130f74e9b7bcbecfd260db7b78f2e723a
Ronald Brill <rbri@rbri.de>
2020-10-27 13:26:19 +0100

    fix build

--- src/test/java/com/gargoylesoftware/htmlunit/BrowserVersionTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/BrowserVersionTest.java
@@ -31,7 +31,7 @@ public class BrowserVersionTest extends WebTestCase {
      */
     @Test
     public void getBrowserVersionNumeric() {
-        assertEquals(81, BrowserVersion.FIREFOX.getBrowserVersionNumeric());
+        assertEquals(82, BrowserVersion.FIREFOX.getBrowserVersionNumeric());
         assertEquals(78, BrowserVersion.FIREFOX_78.getBrowserVersionNumeric());
         assertEquals(11, BrowserVersion.INTERNET_EXPLORER.getBrowserVersionNumeric());
         assertEquals(86, BrowserVersion.CHROME.getBrowserVersionNumeric());
