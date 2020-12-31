e251e2b9bdddc2a3f2cf8cc4d5af8370ec8ac322
Ronald Brill <rbri@rbri.de>
2020-12-31 13:43:16 +0100

    better version checking and some pom fixes

--- src/test/java/com/gargoylesoftware/htmlunit/ExternalTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/ExternalTest.java
@@ -277,14 +277,8 @@ public class ExternalTest {
             if (i == values2.length) {
                 return true;
             }
-            int i1 = Integer.parseInt(values1[i]);
-            int i2 = Integer.parseInt(values2[i]);
-            if (i1 > 2000 && i2 < 2000) {
-                i1 = 0;
-            }
-            if (i2 > 2000 && i1 < 2000) {
-                i2 = 0;
-            }
+            final int i1 = Integer.parseInt(values1[i]);
+            final int i2 = Integer.parseInt(values2[i]);
             if (i1 < i2) {
                 return false;
             }
@@ -301,6 +295,15 @@ public class ExternalTest {
                 && (version.startsWith("11.") || version.startsWith("10."))) {
             return true;
         }
+
+        // really old common versions
+        if ("commons-io".equals(artifactId) && (version.startsWith("2003"))) {
+            return true;
+        }
+        if ("commons-net".equals(artifactId) && (version.startsWith("2003"))) {
+            return true;
+        }
+
         return false;
     }
 
