cc3a08149b58b973c3272c68f8c909d815000f4e
Ronald Brill <rbri@rbri.de>
2021-01-11 17:56:52 +0100

    start working on more architecture tests

--- src/test/java/com/gargoylesoftware/htmlunit/archunit/ArchitectureTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/archunit/ArchitectureTest.java
@@ -15,9 +15,14 @@
 package com.gargoylesoftware.htmlunit.archunit;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
 
 import org.junit.runner.RunWith;
 
+import com.gargoylesoftware.htmlunit.javascript.configuration.JsxClass;
+import com.gargoylesoftware.htmlunit.javascript.configuration.JsxFunction;
+import com.gargoylesoftware.htmlunit.javascript.configuration.JsxGetter;
+import com.gargoylesoftware.htmlunit.javascript.configuration.JsxSetter;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.junit.ArchUnitRunner;
@@ -39,4 +44,33 @@ public class ArchitectureTest {
     public static final ArchRule utilsPackageRule = classes()
         .that().haveNameMatching(".*Util.?")
         .should().resideInAPackage("com.gargoylesoftware.htmlunit.util");
+
+    /**
+     * JsxClasses are always in the javascript package.
+     */
+    @ArchTest
+    public static final ArchRule jsxClassAnnotationPackages = classes()
+            .that().areAnnotatedWith(JsxClass.class)
+            .should().resideInAPackage("..javascript..");
+
+    /**
+     * JsxGetter/Setter/Functions are always in the javascript package.
+     */
+    @ArchTest
+    public static final ArchRule jsxGetterAnnotationPackages = methods()
+            .that().areAnnotatedWith(JsxGetter.class)
+                    .or().areAnnotatedWith(JsxSetter.class)
+                    .or().areAnnotatedWith(JsxFunction.class)
+            .should().beDeclaredInClassesThat().resideInAPackage("..javascript..");
+
+// seems to be broken https://github.com/TNG/ArchUnit/issues/507
+//    /**
+//     * JsxGetter/Setter/Functions only valid in classes annotated as JsxClass.
+//     */
+//    @ArchTest
+//    public static final ArchRule jsxGetterAnnotationJsxClass = methods()
+//            .that().areAnnotatedWith(JsxGetter.class)
+//                    .or().areAnnotatedWith(JsxSetter.class)
+//                    .or().areAnnotatedWith(JsxFunction.class)
+//            .should().beDeclaredInClassesThat().areAnnotatedWith(JsxClass.class);
 }
