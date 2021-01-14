f81a01ee31fa37a47de5db1527f529999df5a948
Ronald Brill <rbri@rbri.de>
2021-01-14 20:17:41 +0100

    code style

--- src/main/java/com/gargoylesoftware/htmlunit/javascript/host/Location.java
+++ src/main/java/com/gargoylesoftware/htmlunit/javascript/host/Location.java
@@ -205,7 +205,7 @@ public class Location extends SimpleScriptable {
         }
         catch (final MalformedURLException e) {
             if (LOG.isErrorEnabled()) {
-            	LOG.error(e.getMessage(), e);
+                LOG.error(e.getMessage(), e);
             }
             return page.getUrl().toExternalForm();
         }
