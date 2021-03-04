d9e1e556692f728e487027c1ecc752d123cec090
Ronald Brill <rbri@rbri.de>
2021-03-04 11:49:51 +0100

    Chrome 89, Firefox 86

--- src/test/java/com/gargoylesoftware/htmlunit/BrowserVersionTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/BrowserVersionTest.java
@@ -31,7 +31,7 @@ public class BrowserVersionTest extends WebTestCase {
      */
     @Test
     public void getBrowserVersionNumeric() {
-        assertEquals(85, BrowserVersion.FIREFOX.getBrowserVersionNumeric());
+        assertEquals(86, BrowserVersion.FIREFOX.getBrowserVersionNumeric());
         assertEquals(78, BrowserVersion.FIREFOX_78.getBrowserVersionNumeric());
         assertEquals(11, BrowserVersion.INTERNET_EXPLORER.getBrowserVersionNumeric());
         assertEquals(89, BrowserVersion.CHROME.getBrowserVersionNumeric());
