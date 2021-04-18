f6989ddd25b7a6a87f6083384ba0d43dd50f1a13
Ronald Brill <rbri@rbri.de>
2021-04-18 09:40:40 +0200

    Chrome/Edge 90

--- src/test/java/com/gargoylesoftware/htmlunit/html/HtmlOption2Test.java
+++ src/test/java/com/gargoylesoftware/htmlunit/html/HtmlOption2Test.java
@@ -14,10 +14,6 @@
  */
 package com.gargoylesoftware.htmlunit.html;
 
-import static com.gargoylesoftware.htmlunit.BrowserRunner.TestedBrowser.CHROME;
-import static com.gargoylesoftware.htmlunit.BrowserRunner.TestedBrowser.EDGE;
-import static com.gargoylesoftware.htmlunit.BrowserRunner.TestedBrowser.IE;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -31,7 +27,7 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import com.gargoylesoftware.htmlunit.BrowserRunner;
 import com.gargoylesoftware.htmlunit.BrowserRunner.Alerts;
 import com.gargoylesoftware.htmlunit.BrowserRunner.BuggyWebDriver;
-import com.gargoylesoftware.htmlunit.BrowserRunner.NotYetImplemented;
+import com.gargoylesoftware.htmlunit.BrowserRunner.HtmlUnitNYI;
 import com.gargoylesoftware.htmlunit.WebDriverTestCase;
 
 /**
@@ -53,10 +49,15 @@ public class HtmlOption2Test extends WebDriverTestCase {
             CHROME = {"option1", "", "Number Three", "Number 4",
                 "      option1\n       Number Three\n      Number 4\n    "},
             EDGE = {"option1", "", "Number Three", "Number 4",
-                "      option1\n      \n      Number Three\n      Number 4\n    "},
+                "      option1\n       Number Three\n      Number 4\n    "},
             IE = {"option1", "", "Number Three", "Number 4",
                 "option1 Number Three Number 4"})
-    @NotYetImplemented({CHROME, EDGE, IE})
+    @HtmlUnitNYI(CHROME = {"option1", "", "Number Three", "Number 4",
+                "option1\nNumber Three\nNumber 4"},
+            EDGE = {"option1", "", "Number Three", "Number 4",
+                "option1\nNumber Three\nNumber 4"},
+            IE = {"option1", "", "Number Three", "Number 4",
+                "option1\nNumber Three\nNumber 4"})
     public void getVisibleText() throws Exception {
         final String htmlContent
             = "<html>\n"
