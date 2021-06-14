1661029bb8a71852eadd699afd5027fd57b9998a
Ronald Brill <rbri@rbri.de>
2021-06-14 19:27:59 +0200

    the usual updates

--- src/test/java/com/gargoylesoftware/htmlunit/ExternalTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/ExternalTest.java
@@ -55,10 +55,10 @@ public class ExternalTest {
     static String MAVEN_REPO_URL_ = "https://repo1.maven.org/maven2/";
 
     /** Chrome driver. */
-    static String CHROME_DRIVER_ = "91.0.4472.19";
+    static String CHROME_DRIVER_ = "91.0.4472.101";
     static String CHROME_DRIVER_URL_ = "https://chromedriver.chromium.org/downloads";
 
-    static String EDGE_DRIVER_ = "91.0.864.37";
+    static String EDGE_DRIVER_ = "91.0.864.48";
     static String EDGE_DRIVER_URL_ = "https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/";
 
     /** Gecko driver. */
