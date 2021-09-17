8b50b7b739685d9f685a4a4262cedc6d0e71ab63
Ronald Brill <rbri@rbri.de>
2021-09-17 16:49:14 +0200

    faster tests

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLDivElementTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/html/HTMLDivElementTest.java
@@ -47,21 +47,23 @@ public class HTMLDivElementTest extends WebDriverTestCase {
             "<html>\n"
             + "  <head>\n"
             + "    <script>\n"
+            + LOG_TITLE_FUNCTION
             + "      function test() {\n"
             + "        var div = document.getElementById('d');\n"
             + "        if(div.doScroll) {\n"
-            + "          alert('yes');\n"
+            + "          log('yes');\n"
             + "          div.doScroll();\n"
             + "          div.doScroll('down');\n"
             + "        } else {\n"
-            + "          alert('no');\n"
+            + "          log('no');\n"
             + "        }\n"
             + "      }\n"
             + "    </script>\n"
             + "  </head>\n"
             + "  <body onload='test()'><div id='d'>abc</div></body>\n"
             + "</html>";
-        loadPageWithAlerts2(html);
+
+        loadPageVerifyTitle2(html);
     }
 
     /**
@@ -84,12 +86,14 @@ public class HTMLDivElementTest extends WebDriverTestCase {
             + "  </table>\n"
 
             + "<script>\n"
+            + LOG_TITLE_FUNCTION
             + "  for (var i = 1; i <= 6; i++) {\n"
-            + "    alert(document.getElementById('d' + i).align);\n"
+            + "    log(document.getElementById('d' + i).align);\n"
             + "  }\n"
             + "</script>\n"
             + "</body></html>";
-        loadPageWithAlerts2(html);
+
+        loadPageVerifyTitle2(html);
     }
 
     /**
@@ -106,11 +110,12 @@ public class HTMLDivElementTest extends WebDriverTestCase {
             + "  </table>\n"
 
             + "<script>\n"
+            + LOG_TITLE_FUNCTION
             + "  function setAlign(elem, value) {\n"
             + "    try {\n"
             + "      elem.align = value;\n"
-            + "    } catch (e) { alert('error'); }\n"
-            + "    alert(elem.align);\n"
+            + "    } catch (e) { log('error'); }\n"
+            + "    log(elem.align);\n"
             + "  }\n"
 
             + "  var elem = document.getElementById('d1');\n"
@@ -125,7 +130,8 @@ public class HTMLDivElementTest extends WebDriverTestCase {
             + "  setAlign(elem, 'center');\n"
             + "</script>\n"
             + "</body></html>";
-        loadPageWithAlerts2(html);
+
+        loadPageVerifyTitle2(html);
     }
 
     /**
@@ -139,14 +145,16 @@ public class HTMLDivElementTest extends WebDriverTestCase {
             = "<html><body>\n"
             + "<div id='d1'></div>\n"
             + "<script>\n"
+            + LOG_TITLE_FUNCTION
             + "  var d = document.getElementById('d1');\n"
-            + "  alert(d.onchange);\n"
-            + "  alert('onchange' in d);\n"
-            + "  alert(d.onsubmit);\n"
-            + "  alert('onsubmit' in d);\n"
+            + "  log(d.onchange);\n"
+            + "  log('onchange' in d);\n"
+            + "  log(d.onsubmit);\n"
+            + "  log('onsubmit' in d);\n"
             + "</script>\n"
             + "</body></html>";
-        loadPageWithAlerts2(html);
+
+        loadPageVerifyTitle2(html);
     }
 
     /**
@@ -160,24 +168,25 @@ public class HTMLDivElementTest extends WebDriverTestCase {
             + "<html>\n"
             + "<head>\n"
             + "  <script>\n"
+            + LOG_TITLE_FUNCTION
             + "    function test() {\n"
             + "      var elt = document.getElementById('emptyDiv');\n"
-            + "      alert(elt.clientHeight == 0);\n"
+            + "      log(elt.clientHeight == 0);\n"
 
             + "      elt = document.getElementById('textDiv');\n"
-            + "      alert(elt.clientHeight > 15);\n"
+            + "      log(elt.clientHeight > 15);\n"
 
             + "      elt = document.getElementById('styleDiv0');\n"
-            + "      alert(elt.clientHeight == 0);\n"
+            + "      log(elt.clientHeight == 0);\n"
 
             + "      elt = document.getElementById('styleDiv10');\n"
-            + "      alert(elt.clientHeight > 5);\n"
+            + "      log(elt.clientHeight > 5);\n"
 
             + "      elt = document.getElementById('styleDivAuto');\n"
-            + "      alert(elt.clientHeight > 15);\n"
+            + "      log(elt.clientHeight > 15);\n"
 
             + "      elt = document.getElementById('styleDivAutoEmpty');\n"
-            + "      alert(elt.clientHeight == 0);\n"
+            + "      log(elt.clientHeight == 0);\n"
             + "    }\n"
             + "  </script>\n"
             + "</head>\n"
@@ -190,7 +199,7 @@ public class HTMLDivElementTest extends WebDriverTestCase {
             + "  <div id='styleDivAutoEmpty' style='height: auto'></div>\n"
             + "</body></html>";
 
-        loadPageWithAlerts2(html);
+        loadPageVerifyTitle2(html);
     }
 
     /**
@@ -204,24 +213,25 @@ public class HTMLDivElementTest extends WebDriverTestCase {
             + "<html>\n"
             + "<head>\n"
             + "  <script>\n"
+            + LOG_TITLE_FUNCTION
             + "    function test() {\n"
             + "      var elt = document.getElementById('emptyDiv');\n"
-            + "      alert(elt.clientWidth > 500);\n"
+            + "      log(elt.clientWidth > 500);\n"
 
             + "      elt = document.getElementById('textDiv');\n"
-            + "      alert(elt.clientWidth > 500);\n"
+            + "      log(elt.clientWidth > 500);\n"
 
             + "      elt = document.getElementById('styleDiv0');\n"
-            + "      alert(elt.clientWidth == 0);\n"
+            + "      log(elt.clientWidth == 0);\n"
 
             + "      elt = document.getElementById('styleDiv10');\n"
-            + "      alert(elt.clientWidth > 8);\n"
+            + "      log(elt.clientWidth > 8);\n"
 
             + "      elt = document.getElementById('styleDivAuto');\n"
-            + "      alert(elt.clientWidth > 500);\n"
+            + "      log(elt.clientWidth > 500);\n"
 
             + "      elt = document.getElementById('styleDivAutoEmpty');\n"
-            + "      alert(elt.clientWidth > 500);\n"
+            + "      log(elt.clientWidth > 500);\n"
             + "    }\n"
             + "  </script>\n"
             + "</head>\n"
@@ -234,7 +244,7 @@ public class HTMLDivElementTest extends WebDriverTestCase {
             + "  <div id='styleDivAutoEmpty' style='width: auto'></div>\n"
             + "</body></html>";
 
-        loadPageWithAlerts2(html);
+        loadPageVerifyTitle2(html);
     }
 
     /**
@@ -248,24 +258,25 @@ public class HTMLDivElementTest extends WebDriverTestCase {
             + "<html>\n"
             + "<head>\n"
             + "  <script>\n"
+            + LOG_TITLE_FUNCTION
             + "    function test() {\n"
             + "      var elt = document.getElementById('emptyDiv');\n"
-            + "      alert(elt.clientWidth > 500);\n"
+            + "      log(elt.clientWidth > 500);\n"
 
             + "      elt = document.getElementById('textDiv');\n"
-            + "      alert(elt.clientWidth > 500);\n"
+            + "      log(elt.clientWidth > 500);\n"
 
             + "      elt = document.getElementById('styleDiv0');\n"
-            + "      alert(elt.clientWidth == 0);\n"
+            + "      log(elt.clientWidth == 0);\n"
 
             + "      elt = document.getElementById('styleDiv10');\n"
-            + "      alert(elt.clientWidth > 8);\n"
+            + "      log(elt.clientWidth > 8);\n"
 
             + "      elt = document.getElementById('styleDivAuto');\n"
-            + "      alert(elt.clientWidth > 500);\n"
+            + "      log(elt.clientWidth > 500);\n"
 
             + "      elt = document.getElementById('styleDivAutoEmpty');\n"
-            + "      alert(elt.clientWidth > 500);\n"
+            + "      log(elt.clientWidth > 500);\n"
             + "    }\n"
             + "  </script>\n"
             + "</head>\n"
@@ -278,7 +289,7 @@ public class HTMLDivElementTest extends WebDriverTestCase {
             + "  <div id='styleDivAutoEmpty' style='width: auto'><div></div></div>\n"
             + "</body></html>";
 
-        loadPageWithAlerts2(html);
+        loadPageVerifyTitle2(html);
     }
 
     /**
@@ -292,22 +303,23 @@ public class HTMLDivElementTest extends WebDriverTestCase {
             "<html>\n"
             + "  <head>\n"
             + "    <script>\n"
+            + LOG_TITLE_FUNCTION
             + "      function test() {\n"
             + "        var div = document.getElementById('test');\n"
-            + "        alert(div.noWrap);\n"
-            + "        alert(div.getAttribute('noWrap'));\n"
+            + "        log(div.noWrap);\n"
+            + "        log(div.getAttribute('noWrap'));\n"
             + "        div.noWrap = 'nowrap';\n"
-            + "        alert(div.noWrap);\n"
-            + "        alert(div.getAttribute('noWrap'));\n"
+            + "        log(div.noWrap);\n"
+            + "        log(div.getAttribute('noWrap'));\n"
             + "        div.noWrap = 'x';\n"
-            + "        alert(div.noWrap);\n"
-            + "        alert(div.getAttribute('noWrap'));\n"
+            + "        log(div.noWrap);\n"
+            + "        log(div.getAttribute('noWrap'));\n"
             + "        div.setAttribute('noWrap', 'blah');\n"
-            + "        alert(div.noWrap);\n"
-            + "        alert(div.getAttribute('noWrap'));\n"
+            + "        log(div.noWrap);\n"
+            + "        log(div.getAttribute('noWrap'));\n"
             + "        div.noWrap = '';\n"
-            + "        alert(div.noWrap);\n"
-            + "        alert(div.getAttribute('noWrap'));\n"
+            + "        log(div.noWrap);\n"
+            + "        log(div.getAttribute('noWrap'));\n"
             + "      }\n"
             + "    </script>\n"
             + "  </head>\n"
@@ -315,6 +327,7 @@ public class HTMLDivElementTest extends WebDriverTestCase {
             + "  <div id='test'>div</div>\n"
             + "  </body>\n"
             + "</html>";
-        loadPageWithAlerts2(html);
+
+        loadPageVerifyTitle2(html);
     }
 }
