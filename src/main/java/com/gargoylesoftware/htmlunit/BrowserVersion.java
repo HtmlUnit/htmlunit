3743944bfe7a6885ed81491fbb170f40573a0415
Ronald Brill <rbri@rbri.de>
2021-10-30 14:07:20 +0200

    Chrome/Edge 95

--- src/main/java/com/gargoylesoftware/htmlunit/BrowserVersion.java
+++ src/main/java/com/gargoylesoftware/htmlunit/BrowserVersion.java
@@ -103,10 +103,10 @@ public final class BrowserVersion implements Serializable {
     public static final BrowserVersion INTERNET_EXPLORER = new BrowserVersion(11, "IE");
 
     /** Latest Edge */
-    public static final BrowserVersion EDGE = new BrowserVersion(94, "Edge");
+    public static final BrowserVersion EDGE = new BrowserVersion(95, "Edge");
 
     /** Latest Chrome. */
-    public static final BrowserVersion CHROME = new BrowserVersion(94, "Chrome");
+    public static final BrowserVersion CHROME = new BrowserVersion(95, "Chrome");
 
     /**
      * Array with all supported browsers
--- src/test/java/com/gargoylesoftware/htmlunit/ExternalTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/ExternalTest.java
@@ -55,10 +55,10 @@ public class ExternalTest {
     static String MAVEN_REPO_URL_ = "https://repo1.maven.org/maven2/";
 
     /** Chrome driver. */
-    static String CHROME_DRIVER_ = "94.0.4606.61";
+    static String CHROME_DRIVER_ = "95.0.4638.54";
     static String CHROME_DRIVER_URL_ = "https://chromedriver.chromium.org/downloads";
 
-    static String EDGE_DRIVER_ = "94.0.992.50";
+    static String EDGE_DRIVER_ = "95.0.1020.40";
     static String EDGE_DRIVER_URL_ = "https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/";
 
     /** Gecko driver. */
