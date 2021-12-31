96dac37706f51f48f78d4cd826c9519be7bd1215
Ronald Brill <rbri@rbri.de>
2021-12-31 13:29:51 +0100

    code style

--- src/test/java/com/gargoylesoftware/htmlunit/html/HtmlButton2Test.java
+++ src/test/java/com/gargoylesoftware/htmlunit/html/HtmlButton2Test.java
@@ -26,7 +26,6 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import com.gargoylesoftware.htmlunit.WebDriverTestCase;
 import com.gargoylesoftware.htmlunit.junit.BrowserRunner;
 import com.gargoylesoftware.htmlunit.junit.BrowserRunner.Alerts;
-import com.gargoylesoftware.htmlunit.junit.BrowserRunner.HtmlUnitNYI;
 import com.gargoylesoftware.htmlunit.util.NameValuePair;
 
 /**
