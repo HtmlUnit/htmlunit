cb6727d52fbb71a85d3239c8cfbedd59cdb9c037
Ronald Brill <rbri@rbri.de>
2022-03-08 13:13:10 +0100

    unify tests impl

--- src/test/java/com/gargoylesoftware/htmlunit/javascript/host/event/MessageEventTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/javascript/host/event/MessageEventTest.java
@@ -18,7 +18,6 @@ import static com.gargoylesoftware.htmlunit.junit.BrowserRunner.TestedBrowser.IE
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.WebDriver;
 
 import com.gargoylesoftware.htmlunit.WebDriverTestCase;
 import com.gargoylesoftware.htmlunit.html.HtmlPageTest;
@@ -38,17 +37,17 @@ public class MessageEventTest extends WebDriverTestCase {
 
     private static final String DUMP_EVENT_FUNCTION = "  function dump(event) {\n"
             + "    if (event) {\n"
-            + "      document.title += ' -' + event;\n"
-            + "      document.title += ' -' + event.type;\n"
-            + "      document.title += ' -' + event.bubbles;\n"
-            + "      document.title += ' -' + event.cancelable;\n"
-            + "      document.title += ' -' + event.composed;\n"
-            + "      document.title += ' -' + event.data;\n"
-            + "      document.title += ' -' + event.origin;\n"
-            + "      document.title += ' -' + event.lastEventId;\n"
-            + "      document.title += ' -' + event.source;\n"
+            + "      log(event);\n"
+            + "      log(event.type);\n"
+            + "      log(event.bubbles);\n"
+            + "      log(event.cancelable);\n"
+            + "      log(event.composed);\n"
+            + "      log(event.data);\n"
+            + "      log(event.origin);\n"
+            + "      log(event.lastEventId);\n"
+            + "      log(event.source);\n"
             + "    } else {\n"
-            + "      document.title += ' ' + 'no event';\n"
+            + "      log('no event');\n"
             + "    }\n"
             + "  }\n";
 
@@ -56,36 +55,37 @@ public class MessageEventTest extends WebDriverTestCase {
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts(DEFAULT = {"-[object MessageEvent]", "-type-message", "-false", "-false", "-false",
-                       "-null", "-", "-", "-null"},
+    @Alerts(DEFAULT = {"[object MessageEvent]", "type-message", "false", "false", "false",
+                       "null", "", "", "null"},
             IE = "exception")
     public void create_ctor() throws Exception {
         final String html = HtmlPageTest.STANDARDS_MODE_PREFIX_
             + "<html><head><script>\n"
+            + LOG_TITLE_FUNCTION
             + "  function test() {\n"
             + "    try {\n"
             + "      var event = new MessageEvent('type-message');\n"
             + "      dump(event);\n"
-            + "    } catch (e) { document.title += 'exception'; }\n"
+            + "    } catch (e) { log('exception'); }\n"
             + "  }\n"
             + DUMP_EVENT_FUNCTION
             + "</script></head><body onload='test()'>\n"
             + "</body></html>";
 
-        final WebDriver driver = loadPage2(html);
-        assertTitle(driver, String.join(" ", getExpectedAlerts()));
+        loadPageVerifyTitle2(html);
     }
 
     /**
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts(DEFAULT = {"-[object MessageEvent]", "-type-message", "-false", "-false", "-false",
-                       "-test-data", "-test-origin", "-42", "-[object Window]"},
+    @Alerts(DEFAULT = {"[object MessageEvent]", "type-message", "false", "false", "false",
+                       "test-data", "test-origin", "42", "[object Window]"},
             IE = "exception")
     public void create_ctorWithDetails() throws Exception {
         final String html = HtmlPageTest.STANDARDS_MODE_PREFIX_
             + "<html><head><script>\n"
+            + LOG_TITLE_FUNCTION
             + "  function test() {\n"
             + "    try {\n"
             + "      var event = new MessageEvent('type-message', {\n"
@@ -95,14 +95,13 @@ public class MessageEventTest extends WebDriverTestCase {
             + "        'source': window\n"
             + "      });\n"
             + "      dump(event);\n"
-            + "    } catch (e) { document.title += 'exception'; }\n"
+            + "    } catch (e) { log('exception'); }\n"
             + "  }\n"
             + DUMP_EVENT_FUNCTION
             + "</script></head><body onload='test()'>\n"
             + "</body></html>";
 
-        final WebDriver driver = loadPage2(html);
-        assertTitle(driver, String.join(" ", getExpectedAlerts()));
+        loadPageVerifyTitle2(html);
     }
 
     /**
@@ -150,25 +149,24 @@ public class MessageEventTest extends WebDriverTestCase {
             + "  try {\n"
             + "    e.initMessageEvent('message', true, true, 'hello', '" + origin + "', 2, window, null);\n"
             + "    dump(e);\n"
-            + "  } catch (e) { document.title += 'exception '; }\n"
+            + "  } catch (e) { log('exception'); }\n"
             + "} else {\n"
-            + "  document.title += 'no initMessageEvent';\n"
+            + "  log('no initMessageEvent');\n"
             + "}\n"
             + DUMP_EVENT_FUNCTION
             + "</script></body></html>";
 
-        final WebDriver driver = loadPage2(html);
-        assertTitle(driver, String.join(" ", getExpectedAlerts()));
+        loadPageVerifyTitle2(html);
     }
 
     /**
      * @throws Exception if an error occurs
      */
     @Test
-    @Alerts(DEFAULT = {"-[object MessageEvent]", "-message", "-true", "-true", "-false", "-hello",
-                       "-http://localhost:", "-2", "-[object Window]"},
-            IE = {"-[object MessageEvent]", "-message", "-true", "-true", "-undefined", "-hello",
-                  "-http://localhost:", "-undefined", "-[object Window]"})
+    @Alerts(DEFAULT = {"[object MessageEvent]", "message", "true", "true", "false", "hello",
+                       "http://localhost:", "2", "[object Window]"},
+            IE = {"[object MessageEvent]", "message", "true", "true", "undefined", "hello",
+                  "http://localhost:", "undefined", "[object Window]"})
     public void initMessageEventPortsUndefined() throws Exception {
         final String[] expectedAlerts = getExpectedAlerts();
         if (expectedAlerts.length > 4) {
@@ -178,30 +176,30 @@ public class MessageEventTest extends WebDriverTestCase {
 
         final String origin = "http://localhost:" + PORT;
         final String html = "<html><body><script>\n"
+            + LOG_TITLE_FUNCTION
             + "var e = document.createEvent('MessageEvent');\n"
             + "if (e.initMessageEvent) {\n"
             + "  try {\n"
             + "    e.initMessageEvent('message', true, true, 'hello', '" + origin + "', 2, window, undefined);\n"
             + "    dump(e);\n"
-            + "  } catch (e) { document.title += 'exception '; }\n"
+            + "  } catch (e) { log('exception'); }\n"
             + "} else {\n"
-            + "  document.title += 'no initMessageEvent';\n"
+            + "  log('no initMessageEvent');\n"
             + "}\n"
             + DUMP_EVENT_FUNCTION
             + "</script></body></html>";
 
-        final WebDriver driver = loadPage2(html);
-        assertTitle(driver, String.join(" ", getExpectedAlerts()));
+        loadPageVerifyTitle2(html);
     }
 
     /**
      * @throws Exception if an error occurs
      */
     @Test
-    @Alerts(DEFAULT = {"-[object MessageEvent]", "-message", "-true", "-true", "-false", "-hello",
-                       "-http://localhost:", "-2", "-[object Window]"},
-            IE = {"-[object MessageEvent]", "-message", "-true", "-true", "-undefined", "-hello",
-                  "-http://localhost:", "-undefined", "-[object Window]"})
+    @Alerts(DEFAULT = {"[object MessageEvent]", "message", "true", "true", "false", "hello",
+                       "http://localhost:", "2", "[object Window]"},
+            IE = {"[object MessageEvent]", "message", "true", "true", "undefined", "hello",
+                  "http://localhost:", "undefined", "[object Window]"})
     public void initMessageEvent() throws Exception {
         final String[] expectedAlerts = getExpectedAlerts();
         expectedAlerts[6] += PORT;
@@ -209,19 +207,19 @@ public class MessageEventTest extends WebDriverTestCase {
 
         final String origin = "http://localhost:" + PORT;
         final String html = "<html><body><script>\n"
+            + LOG_TITLE_FUNCTION
             + "var e = document.createEvent('MessageEvent');\n"
             + "if (e.initMessageEvent) {\n"
             + "  try {\n"
             + "    e.initMessageEvent('message', true, true, 'hello', '" + origin + "', 2, window, []);\n"
             + "    dump(e);\n"
-            + "  } catch (e) { document.title += 'exception' + e; }\n"
+            + "  } catch (e) { log('exception ' + e); }\n"
             + "} else {\n"
-            + "  document.title += 'no initMessageEvent';\n"
+            + "  log('no initMessageEvent');\n"
             + "}\n"
             + DUMP_EVENT_FUNCTION
             + "</script></body></html>";
 
-        final WebDriver driver = loadPage2(html);
-        assertTitle(driver, String.join(" ", getExpectedAlerts()));
+        loadPageVerifyTitle2(html);
     }
 }
