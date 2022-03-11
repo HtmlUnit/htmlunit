72c2eb19c2d4c905c13645ba993473d3303fdb4e
Ronald Brill <rbri@rbri.de>
2022-03-11 08:55:31 +0100

    HtmlFileInput.setValueAttribute(String) now handles an empty input as no file (issue #383)

--- src/test/java/com/gargoylesoftware/htmlunit/html/HtmlFileInput2Test.java
+++ src/test/java/com/gargoylesoftware/htmlunit/html/HtmlFileInput2Test.java
@@ -701,4 +701,29 @@ public class HtmlFileInput2Test extends WebServerTestCase {
         final HtmlFileInput file = page.<HtmlFileInput>getHtmlElementById("fileId");
         assertEquals(0, file.getFiles().length);
     }
+
+    /**
+     * @throws Exception if an error occurs
+     */
+    @Test
+    public void clearFromJava() throws Exception {
+        final String html =
+                "<html><head>\n"
+                + "  <script>\n"
+                + "    function test() {\n"
+                + "      var f =  document.createElement('input');\n"
+                + "      f.type='file';\n"
+                + "      f.id='fileId';\n"
+                + "      document.body.appendChild(f);"
+                + "    }\n"
+                + "  </script>\n"
+                + "</head>\n"
+                + "<body onload='test()'>\n"
+                + "</body></html>";
+
+        final HtmlPage page = loadPage(html);
+        final HtmlFileInput file = page.<HtmlFileInput>getHtmlElementById("fileId");
+        file.setValueAttribute("");
+        assertEquals(0, file.getFiles().length);
+    }
 }
