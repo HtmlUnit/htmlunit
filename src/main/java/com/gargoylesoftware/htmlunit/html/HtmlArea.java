5dbf31191196aaec45625b147d518d0406cf07da
Bharatwaaj <bharatwaaj.1996@gmail.com>
2022-03-26 12:18:44 -0300

    Update HtmlArea.java

--- src/main/java/com/gargoylesoftware/htmlunit/html/HtmlArea.java
+++ src/main/java/com/gargoylesoftware/htmlunit/html/HtmlArea.java
@@ -95,15 +95,22 @@ public class HtmlArea extends HtmlElement {
                 throw new IllegalStateException(
                         "Not a valid url: " + getHrefAttribute(), e);
             }
-            final WebRequest request = new WebRequest(url);
-            request.setCharset(page.getCharset());
-            request.setRefererlHeader(page.getUrl());
-            final WebWindow webWindow = enclosingPage.getEnclosingWindow();
-
-            final String target = enclosingPage.getResolvedTarget(getTargetAttribute());
-            webClient.getPage(webClient.openTargetWindow(webWindow, target, WebClient.TARGET_SELF), request);
+            setupWebClientPage(enclosingPage, webClient, page, url);
         }
         return false;
+    }
+    
+    /**
+     * Sets up a Web Client Page
+     */
+    private void setupWebClientPage(HtmlPage enclosingPage, WebClient webClient, HtmlPage page, URL url) throws IOException {
+        final WebRequest request = new WebRequest(url);
+        request.setCharset(page.getCharset());
+        request.setRefererlHeader(page.getUrl());
+        final WebWindow webWindow = enclosingPage.getEnclosingWindow();
+
+        final String target = enclosingPage.getResolvedTarget(getTargetAttribute());
+        webClient.getPage(webClient.openTargetWindow(webWindow, target, WebClient.TARGET_SELF), request);
     }
 
     /**
