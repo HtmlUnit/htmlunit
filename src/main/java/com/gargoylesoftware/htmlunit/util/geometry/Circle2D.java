76e3fc3d07ba5b999fdbfa38565016fc66490f3c
Bharatwaaj <bharatwaaj.1996@gmail.com>
2022-03-27 18:43:33 -0300

    Update Circle2D.java

--- src/main/java/com/gargoylesoftware/htmlunit/util/geometry/Circle2D.java
+++ src/main/java/com/gargoylesoftware/htmlunit/util/geometry/Circle2D.java
@@ -14,6 +14,11 @@
  */
 package com.gargoylesoftware.htmlunit.util.geometry;
 
+import com.gargoylesoftware.htmlunit.html.HtmlArea;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+
 /**
  * Simple 2D shape circle.
  *
@@ -53,6 +58,33 @@ public class Circle2D implements Shape2D {
     @Override
     public boolean isEmpty() {
         return radius_ < epsilon;
+    }
+
+    public static Shape2D parse(HtmlArea htmlArea) {
+        // browsers seem to support comma and blank
+        final String[] coords = StringUtils.split(htmlArea.getCoordsAttribute(), ", ");
+
+        double centerX = 0;
+        double centerY = 0;
+        double radius = 0;
+
+        try {
+            if (coords.length > 0) {
+                centerX = Double.parseDouble(coords[0].trim());
+            }
+            if (coords.length > 1) {
+                centerY = Double.parseDouble(coords[1].trim());
+            }
+            if (coords.length > 2) {
+                radius = Double.parseDouble(coords[2].trim());
+            }
+
+        }
+        catch (final NumberFormatException e) {
+            htmlArea.getLOG().warn("Invalid circle coords '" + Arrays.toString(coords) + "'", e);
+        }
+
+        return new Circle2D(centerX, centerY, radius);
     }
 
     @Override
