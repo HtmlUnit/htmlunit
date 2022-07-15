ade1eb92661e9f57c6a27f8b36c4eb55d20e5783
Ronald Brill <rbri@rbri.de>
2022-07-15 19:31:01 +0200

    felix and adge driver update

--- src/test/java/com/gargoylesoftware/htmlunit/ExternalTest.java
+++ src/test/java/com/gargoylesoftware/htmlunit/ExternalTest.java
@@ -58,7 +58,7 @@ public class ExternalTest {
     static String CHROME_DRIVER_ = "103.0.5060.53";
     static String CHROME_DRIVER_URL_ = "https://chromedriver.chromium.org/downloads";
 
-    static String EDGE_DRIVER_ = "103.0.1264.37";
+    static String EDGE_DRIVER_ = "103.0.1264.62";
     static String EDGE_DRIVER_URL_ = "https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/";
 
     /** Gecko driver. */
