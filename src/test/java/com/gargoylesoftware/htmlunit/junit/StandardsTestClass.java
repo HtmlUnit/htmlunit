50c0d3852df612839a4fae615081d7dbef1d5019
Ronald Brill <rbri@rbri.de>
2022-08-03 19:42:41 +0200

    format

--- src/test/java/com/gargoylesoftware/htmlunit/junit/StandardsTestClass.java
+++ src/test/java/com/gargoylesoftware/htmlunit/junit/StandardsTestClass.java
@@ -28,6 +28,7 @@ import org.junit.runners.model.TestClass;
 
 /**
  * Test class annotated with {@link com.gargoylesoftware.htmlunit.annotations.StandardsMode}.
+ *
  * @author Ahmed Ashour
  */
 public class StandardsTestClass extends TestClass {
