2f03ab75a96db82ee49dc25f9fac18f26310a520
Ronald Brill <rbri@rbri.de>
2024-04-14 16:19:49 +0200

    more tests (see https://github.com/HtmlUnit/htmlunit-rhino-fork/pull/16)

--- src/test/java/org/htmlunit/javascript/NativeTypedArrayTest.java
+++ src/test/java/org/htmlunit/javascript/NativeTypedArrayTest.java
@@ -17,6 +17,7 @@ package org.htmlunit.javascript;
 import org.htmlunit.WebDriverTestCase;
 import org.htmlunit.junit.BrowserRunner;
 import org.htmlunit.junit.BrowserRunner.Alerts;
+import org.htmlunit.junit.BrowserRunner.HtmlUnitNYI;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -32,161 +33,255 @@ public class NativeTypedArrayTest extends WebDriverTestCase {
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts("Float32Array")
+    @Alerts({"Float32Array", "false", "true",
+             "undefined", "false", "true",
+             "undefined", "true", "true",
+             "Symbol(Symbol.iterator),Symbol(Symbol.toStringTag)"})
+    @HtmlUnitNYI(CHROME = {"Float32Array", "false", "false",
+                           "Float32Array", "false", "false",
+                           "undefined", "false", "false", ""},
+        EDGE = {"Float32Array", "false", "false",
+                "Float32Array", "false", "false",
+                "undefined", "false", "false", ""},
+        FF = {"Float32Array", "false", "false",
+              "Float32Array", "false", "false",
+              "undefined", "false", "false", ""},
+        FF_ESR = {"Float32Array", "false", "false",
+                  "Float32Array", "false", "false",
+                  "undefined", "false", "false", ""})
     public void toStringFloat32Array() throws Exception {
-        final String html
-            = "<html></head>\n"
-            + "<body>"
-            + "<script>\n"
-            + LOG_TITLE_FUNCTION
-            + "  log((new Float32Array(1))[Symbol.toStringTag]);\n"
-            + "</script>\n"
-            + "</body></html>";
-
-        loadPageVerifyTitle2(html);
+        loadPageVerifyTitle2(toStringTagTest("new Float32Array(1)"));
     }
 
     /**
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts("Float64Array")
+    @Alerts({"Float64Array", "false", "true",
+             "undefined", "false", "true",
+             "undefined", "true", "true",
+             "Symbol(Symbol.iterator),Symbol(Symbol.toStringTag)"})
+    @HtmlUnitNYI(CHROME = {"Float64Array", "false", "false",
+                           "Float64Array", "false", "false",
+                           "undefined", "false", "false", ""},
+        EDGE = {"Float64Array", "false", "false",
+                "Float64Array", "false", "false",
+                "undefined", "false", "false", ""},
+        FF = {"Float64Array", "false", "false",
+              "Float64Array", "false", "false",
+              "undefined", "false", "false", ""},
+        FF_ESR = {"Float64Array", "false", "false",
+                  "Float64Array", "false", "false",
+                  "undefined", "false", "false", ""})
     public void toStringFloat64Array() throws Exception {
-        final String html
-            = "<html></head>\n"
-            + "<body>"
-            + "<script>\n"
-            + LOG_TITLE_FUNCTION
-            + "  log((new Float64Array(1))[Symbol.toStringTag]);\n"
-            + "</script>\n"
-            + "</body></html>";
-
-        loadPageVerifyTitle2(html);
+        loadPageVerifyTitle2(toStringTagTest("new Float64Array(1)"));
     }
 
     /**
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts("Int8Array")
+    @Alerts({"Int8Array", "false", "true",
+             "undefined", "false", "true",
+             "undefined", "true", "true",
+             "Symbol(Symbol.iterator),Symbol(Symbol.toStringTag)"})
+    @HtmlUnitNYI(CHROME = {"Int8Array", "false", "false",
+                           "Int8Array", "false", "false",
+                           "undefined", "false", "false", ""},
+        EDGE = {"Int8Array", "false", "false",
+                "Int8Array", "false", "false",
+                "undefined", "false", "false", ""},
+        FF = {"Int8Array", "false", "false",
+              "Int8Array", "false", "false",
+              "undefined", "false", "false", ""},
+        FF_ESR = {"Int8Array", "false", "false",
+                  "Int8Array", "false", "false",
+                  "undefined", "false", "false", ""})
     public void toStringInt8Array() throws Exception {
-        final String html
-            = "<html></head>\n"
-            + "<body>"
-            + "<script>\n"
-            + LOG_TITLE_FUNCTION
-            + "  log((new Int8Array(1))[Symbol.toStringTag]);\n"
-            + "</script>\n"
-            + "</body></html>";
-
-        loadPageVerifyTitle2(html);
+        loadPageVerifyTitle2(toStringTagTest("new Int8Array(1)"));
     }
 
     /**
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts("Int16Array")
+    @Alerts({"Int16Array", "false", "true",
+             "undefined", "false", "true",
+             "undefined", "true", "true",
+             "Symbol(Symbol.iterator),Symbol(Symbol.toStringTag)"})
+    @HtmlUnitNYI(CHROME = {"Int16Array", "false", "false",
+                           "Int16Array", "false", "false",
+                           "undefined", "false", "false", ""},
+        EDGE = {"Int16Array", "false", "false",
+                "Int16Array", "false", "false",
+                "undefined", "false", "false", ""},
+        FF = {"Int16Array", "false", "false",
+              "Int16Array", "false", "false",
+              "undefined", "false", "false", ""},
+        FF_ESR = {"Int16Array", "false", "false",
+                  "Int16Array", "false", "false",
+                  "undefined", "false", "false", ""})
     public void toStringInt16Array() throws Exception {
-        final String html
-            = "<html></head>\n"
-            + "<body>"
-            + "<script>\n"
-            + LOG_TITLE_FUNCTION
-            + "  log((new Int16Array(1))[Symbol.toStringTag]);\n"
-            + "</script>\n"
-            + "</body></html>";
-
-        loadPageVerifyTitle2(html);
+        loadPageVerifyTitle2(toStringTagTest("new Int16Array(1)"));
     }
 
     /**
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts("Int32Array")
+    @Alerts({"Int32Array", "false", "true",
+             "undefined", "false", "true",
+             "undefined", "true", "true",
+             "Symbol(Symbol.iterator),Symbol(Symbol.toStringTag)"})
+    @HtmlUnitNYI(CHROME = {"Int32Array", "false", "false",
+                           "Int32Array", "false", "false",
+                           "undefined", "false", "false", ""},
+        EDGE = {"Int32Array", "false", "false",
+                "Int32Array", "false", "false",
+                "undefined", "false", "false", ""},
+        FF = {"Int32Array", "false", "false",
+              "Int32Array", "false", "false",
+              "undefined", "false", "false", ""},
+        FF_ESR = {"Int32Array", "false", "false",
+                  "Int32Array", "false", "false",
+                  "undefined", "false", "false", ""})
     public void toStringInt32Array() throws Exception {
-        final String html
-            = "<html></head>\n"
-            + "<body>"
-            + "<script>\n"
-            + LOG_TITLE_FUNCTION
-            + "  log((new Int32Array(1))[Symbol.toStringTag]);\n"
-            + "</script>\n"
-            + "</body></html>";
-
-        loadPageVerifyTitle2(html);
+        loadPageVerifyTitle2(toStringTagTest("new Int32Array(1)"));
     }
 
     /**
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts("Uint8Array")
+    @Alerts({"Uint8Array", "false", "true",
+             "undefined", "false", "true",
+             "undefined", "true", "true",
+             "Symbol(Symbol.iterator),Symbol(Symbol.toStringTag)"})
+    @HtmlUnitNYI(CHROME = {"Uint8Array", "false", "false",
+                           "Uint8Array", "false", "false",
+                           "undefined", "false", "false", ""},
+        EDGE = {"Uint8Array", "false", "false",
+                "Uint8Array", "false", "false",
+                "undefined", "false", "false", ""},
+        FF = {"Uint8Array", "false", "false",
+              "Uint8Array", "false", "false",
+              "undefined", "false", "false", ""},
+        FF_ESR = {"Uint8Array", "false", "false",
+                  "Uint8Array", "false", "false",
+                  "undefined", "false", "false", ""})
     public void toStringUint8Array() throws Exception {
-        final String html
-            = "<html></head>\n"
-            + "<body>"
-            + "<script>\n"
-            + LOG_TITLE_FUNCTION
-            + "  log((new Uint8Array(1))[Symbol.toStringTag]);\n"
-            + "</script>\n"
-            + "</body></html>";
-
-        loadPageVerifyTitle2(html);
+        loadPageVerifyTitle2(toStringTagTest("new Uint8Array(1)"));
     }
 
     /**
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts("Uint16Array")
+    @Alerts({"Uint16Array", "false", "true",
+             "undefined", "false", "true",
+             "undefined", "true", "true",
+             "Symbol(Symbol.iterator),Symbol(Symbol.toStringTag)"})
+    @HtmlUnitNYI(CHROME = {"Uint16Array", "false", "false",
+                           "Uint16Array", "false", "false",
+                           "undefined", "false", "false", ""},
+        EDGE = {"Uint16Array", "false", "false",
+                "Uint16Array", "false", "false",
+                "undefined", "false", "false", ""},
+        FF = {"Uint16Array", "false", "false",
+              "Uint16Array", "false", "false",
+              "undefined", "false", "false", ""},
+        FF_ESR = {"Uint16Array", "false", "false",
+                  "Uint16Array", "false", "false",
+                  "undefined", "false", "false", ""})
     public void toStringUint16Array() throws Exception {
-        final String html
-            = "<html></head>\n"
-            + "<body>"
-            + "<script>\n"
-            + LOG_TITLE_FUNCTION
-            + "  log((new Uint16Array(1))[Symbol.toStringTag]);\n"
-            + "</script>\n"
-            + "</body></html>";
-
-        loadPageVerifyTitle2(html);
+        loadPageVerifyTitle2(toStringTagTest("new Uint16Array(1)"));
     }
 
     /**
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts("Uint32Array")
+    @Alerts({"Uint32Array", "false", "true",
+             "undefined", "false", "true",
+             "undefined", "true", "true",
+             "Symbol(Symbol.iterator),Symbol(Symbol.toStringTag)"})
+    @HtmlUnitNYI(CHROME = {"Uint32Array", "false", "false",
+                           "Uint32Array", "false", "false",
+                           "undefined", "false", "false", ""},
+        EDGE = {"Uint32Array", "false", "false",
+                "Uint32Array", "false", "false",
+                "undefined", "false", "false", ""},
+        FF = {"Uint32Array", "false", "false",
+              "Uint32Array", "false", "false",
+              "undefined", "false", "false", ""},
+        FF_ESR = {"Uint32Array", "false", "false",
+                  "Uint32Array", "false", "false",
+                  "undefined", "false", "false", ""})
     public void toStringUint32Array() throws Exception {
-        final String html
-            = "<html></head>\n"
-            + "<body>"
-            + "<script>\n"
-            + LOG_TITLE_FUNCTION
-            + "  log((new Uint32Array(1))[Symbol.toStringTag]);\n"
-            + "</script>\n"
-            + "</body></html>";
-
-        loadPageVerifyTitle2(html);
+        loadPageVerifyTitle2(toStringTagTest("new Uint32Array(1)"));
     }
 
     /**
      * @throws Exception if the test fails
      */
     @Test
-    @Alerts("Uint8ClampedArray")
+    @Alerts({"Uint8ClampedArray", "false", "true",
+             "undefined", "false", "true",
+             "undefined", "true", "true",
+             "Symbol(Symbol.iterator),Symbol(Symbol.toStringTag)"})
+    @HtmlUnitNYI(CHROME = {"Uint8ClampedArray", "false", "false",
+                           "Uint8ClampedArray", "false", "false",
+                           "undefined", "false", "false", ""},
+        EDGE = {"Uint8ClampedArray", "false", "false",
+                "Uint8ClampedArray", "false", "false",
+                "undefined", "false", "false", ""},
+        FF = {"Uint8ClampedArray", "false", "false",
+              "Uint8ClampedArray", "false", "false",
+              "undefined", "false", "false", ""},
+        FF_ESR = {"Uint8ClampedArray", "false", "false",
+                  "Uint8ClampedArray", "false", "false",
+                  "undefined", "false", "false", ""})
     public void toStringUint8ClampedArray() throws Exception {
-        final String html
-            = "<html></head>\n"
+        loadPageVerifyTitle2(toStringTagTest("new Uint8ClampedArray(1)"));
+    }
+
+    private static String toStringTagTest(final String init) {
+        return
+            "<html></head>\n"
             + "<body>"
             + "<script>\n"
             + LOG_TITLE_FUNCTION
-            + "  log((new Uint8ClampedArray(1))[Symbol.toStringTag]);\n"
+            + "  function sortFunction(s1, s2) {\n"
+            + "    var s1lc = s1.toLowerCase();\n"
+            + "    var s2lc =  s2.toLowerCase();\n"
+            + "    if (s1lc > s2lc) { return 1; }\n"
+            + "    if (s1lc < s2lc) { return -1; }\n"
+            + "    return s1 > s2 ? 1 : -1;\n"
+            + "  }\n"
+
+            + "  var typedArray = " + init + ";\n"
+            + "  log((typedArray)[Symbol.toStringTag]);\n"
+            + "  log(typedArray.hasOwnProperty(Symbol.toStringTag));\n"
+            + "  log(Symbol.toStringTag in typedArray);\n"
+
+            + "  var typedArrayProto = typedArray.__proto__;"
+            + "  log((typedArrayProto)[Symbol.toStringTag]);\n"
+            + "  log(typedArrayProto.hasOwnProperty(Symbol.toStringTag));\n"
+            + "  log(Symbol.toStringTag in typedArrayProto);\n"
+
+            + "  var typedArrayProtoProto = typedArray.__proto__.__proto__;"
+            + "  log((typedArrayProtoProto)[Symbol.toStringTag]);\n"
+            + "  log(typedArrayProtoProto.hasOwnProperty(Symbol.toStringTag));\n"
+            + "  log(Symbol.toStringTag in typedArrayProtoProto);\n"
+
+            + "  var syms = Object.getOwnPropertySymbols(typedArrayProtoProto);\n"
+            + "  var symStrs = [];"
+            + "  for (i = 0; i < syms.length; i++) {\n"
+            + "    symStrs.push(syms[i].toString());\n"
+            + "  }\n"
+            + "  symStrs.sort(sortFunction);\n"
+            + "  log(symStrs);"
             + "</script>\n"
             + "</body></html>";
-
-        loadPageVerifyTitle2(html);
     }
 }
