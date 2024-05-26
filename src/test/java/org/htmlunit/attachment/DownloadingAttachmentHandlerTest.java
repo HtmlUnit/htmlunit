f0171c8fbec9f5be9cd220694f6afbeecfaa805d
Marek Andreansky <marek.andreansky@gmail.com>
2024-05-26 21:45:42 +0200

    add drafts of proper test cases

--- src/test/java/org/htmlunit/attachment/DownloadingAttachmentHandlerTest.java
+++ src/test/java/org/htmlunit/attachment/DownloadingAttachmentHandlerTest.java
@@ -2,6 +2,7 @@ package org.htmlunit.attachment;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Path;
@@ -12,36 +13,37 @@ import org.htmlunit.WebClient;
 import org.htmlunit.html.HtmlAnchor;
 import org.htmlunit.html.HtmlPage;
 import org.htmlunit.junit.BrowserRunner;
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 
-//@RunWith(BrowserRunner.class)
 public class DownloadingAttachmentHandlerTest {
 
-	@Test
-	public void test() throws FailingHttpStatusCodeException, MalformedURLException, IOException, InterruptedException {
-		System.out.println(System.getProperty("java.io.tmpdir"));
-		WebClient webClient = new WebClient(BrowserVersion.FIREFOX);
-		webClient.setAttachmentHandler(new DownloadingAttachmentHandler());
-//		HtmlPage page = webClient.getPage("https://github.com/HtmlUnit/htmlunit/releases");
-//		page.getAnchorByText("htmlunit-4.1.0-javadoc.jar.asc").click();
-		HtmlPage page = webClient.getPage("https://www.ghisler.com/download.htm");
-		HtmlAnchor anchorByHref = page.getAnchorByHref("https://totalcommander.ch/1103/tcmd1103x32_64.exe");
-//		HtmlPage page = webClient.getPage("https://www.ronsplace.ca/products/ronsdataedit/download");
-//		HtmlAnchor anchorByHref = page.getAnchorByHref("/contentproduct/data-edit/setup.exe");
-		System.out.println(anchorByHref.asXml());
-		anchorByHref.click();
-		
-		Thread.sleep(100000);
-		
-//		Path.of(System.getProperty("java.io.tmpdir")
-		
-	}
-	
-//	TODO test downloading a file that is an octet stream
-//	TODO test downloading a file that has content disposition and proper attachment header
-//	TODO test downloading a file that does not have an extension (no dot in filename)
-//	TODO test constructor failing if the download folder does not exist or user does not have write access
+	private static final String FILENAME_WITHOUT_EXTENSION = "fileNoDot";
+	private static final String FILENAME_WITH_EXTENSION = "file1.txt";
+	@Rule
+	public File downloadFolder = new TemporaryFolder().getRoot();
 
+	@Test
+	public void whenDownloadFolderWriteable_downloadAttachment() throws IOException {
+		DownloadingAttachmentHandler downloadingAttachmentHandler = new DownloadingAttachmentHandler(
+				downloadFolder.toPath());
+//		TODO add html page with attachment response that is using content disposition
+//		TODO add html page with attachment response that is using octet stream
+//		new HtmlPage().
+		downloadingAttachmentHandler.handleAttachment(null, FILENAME_WITH_EXTENSION);
+		Assert.assertTrue(new File(downloadFolder, FILENAME_WITH_EXTENSION).exists());
+		downloadingAttachmentHandler.handleAttachment(null, FILENAME_WITHOUT_EXTENSION);
+		Assert.assertTrue(new File(downloadFolder, FILENAME_WITHOUT_EXTENSION).exists());
+	}
+
+	@Test
+	public void whenDownloadFolderDoesNotExit_throwException() {
+		Assert.assertThrows(IOException.class, () -> {
+			new DownloadingAttachmentHandler(downloadFolder.toPath().resolve("nonExistentFolder"));
+		});
+	}
 
 }
