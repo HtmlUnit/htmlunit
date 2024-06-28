9fe88655cec67f9bdc7c3b0c0f6a24cae0249ac5
Ronald Brill <rbri@rbri.de>
2024-06-28 19:01:31 +0200

    DownloadingAttachmentHandler code cleanup and some test cases

--- src/main/java/org/htmlunit/attachment/DownloadingAttachmentHandler.java
+++ src/main/java/org/htmlunit/attachment/DownloadingAttachmentHandler.java
@@ -1,79 +1,122 @@
+/*
+ * Copyright (c) 2002-2024 Gargoyle Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.htmlunit.attachment;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.htmlunit.Page;
-import org.htmlunit.WebResponseData;
+import org.htmlunit.WebResponse;
 
+/**
+ * Implementation of an {@link AttachmentHandler} that mimics how browsers handle attachments, specifically
+ * <ul>
+ * <li>download file into a default folder when attachment response is detected</li>
+ * <li>infer filename from octet stream response and use that when saving file</li>
+ * <li>if a file already exists, append number to it.
+ * Keep incrementing numbers until you find a slot that is free (thats how Chrome handles duplicate filenames).</li>
+ * </ul>
+ *
+ * @author Marek Andreansky
+ * @author Ronald Brill
+ */
 public class DownloadingAttachmentHandler implements AttachmentHandler {
 
     private static final Log LOG = LogFactory.getLog(DownloadingAttachmentHandler.class);
-	private Path downloadFolder;
 
-	public DownloadingAttachmentHandler(Path downloadFolder) throws IOException {
-		this.downloadFolder = downloadFolder;
-		if (!Files.isWritable(downloadFolder)) throw new IOException("Can't write to ".concat(downloadFolder.toString()));
-	}
+    private Path downloadFolder_;
 
-	public DownloadingAttachmentHandler() throws IOException {
-		this(Path.of(System.getProperty("java.io.tmpdir")));
-	}
-	
-	@Override
-	public void handleAttachment(Page page, String attachmentFilename) {
-		attachmentFilename = getFilenameFromUrlIfEmpty(page, attachmentFilename);
-		Path downloadDestinationFilePath = downloadFolder.resolve(attachmentFilename);
-		InputStream contentAsStream;
-		try {
-			contentAsStream = page.getWebResponse().getContentAsStream();
-		} catch (IOException e) {
-			LOG.error("Failed to get attachment response content as stream");
-			return;
-		}
-		try {
-			Files.copy(contentAsStream, downloadDestinationFilePath);
-		} catch (FileAlreadyExistsException e) {
-			try {
-				Files.copy(contentAsStream, findNextAvailableFilename(attachmentFilename));
-			} catch (IOException e1) {
-				logFailureToSaveAttachment();
-				return;
-			}
-		} catch (IOException e) {
-			logFailureToSaveAttachment();
-			return;
-		}
-		page.getWebResponse().cleanUp();
-	}
+    /**
+     * Creates a new DownloadingAttachmentHandler that stores all downloaded files in the
+     * provided directory. The directory must exist and be writable.
+     *
+     * @param downloadFolder the path to the folder for storing all downloaded files
+     * @throws IOException if the folder does not exist or the folder is not writable
+     */
+    public DownloadingAttachmentHandler(final Path downloadFolder) throws IOException {
+        downloadFolder_ = downloadFolder;
+        if (Files.notExists(downloadFolder)) {
+            throw new IOException("The provided download folder '"
+                        + downloadFolder.toString() + "' does not exist");
+        }
+        if (!Files.isWritable(downloadFolder)) {
+            throw new IOException("Can't write to the download folder '"
+                        + downloadFolder + "'");
+        }
+    }
 
-	private void logFailureToSaveAttachment() {
-		LOG.error("Failed to write attachment response to ".concat(downloadFolder.toString()));
-	}
+    /**
+     * Creates a new DownloadingAttachmentHandler that stores all downloaded files in the
+     * 'temp'-dir (System.getProperty("java.io.tmpdir")).
+     *
+     * @throws IOException if the folder does not exist or the folder is not writable
+     */
+    public DownloadingAttachmentHandler() throws IOException {
+        this(Path.of(System.getProperty("java.io.tmpdir")));
+    }
 
-	private String getFilenameFromUrlIfEmpty(Page page, String attachmentFilename) {
-		if (attachmentFilename==null || attachmentFilename.trim().isEmpty()) {
-			String file = page.getWebResponse().getWebRequest().getUrl().getFile();
-			attachmentFilename = file.substring(file.lastIndexOf('/') + 1);
-		}
-		return attachmentFilename;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void handleAttachment(final Page page, final String attachmentFilename) {
+        final Path destination = determineDestionationFile(page, attachmentFilename);
 
-	private Path findNextAvailableFilename(String fileName) {
-		Path newPath;
-		Integer count = 0;
-		do  {
-			count++;
-		int fileExtensionstartPosition = (fileName.contains(".") ) ? fileName.lastIndexOf('.') : fileName.length() ;
-		newPath = downloadFolder.resolve(fileName.substring(0, fileExtensionstartPosition).concat(" (" + count + ")").concat(fileName.substring(fileExtensionstartPosition)));
-		} while (Files.exists(newPath, LinkOption.NOFOLLOW_LINKS));
-		return newPath;
-	}
+        final WebResponse webResponse = page.getWebResponse();
+        try (InputStream contentAsStream = webResponse.getContentAsStream()) {
+            FileUtils.copyToFile(contentAsStream, destination.toFile());
+        }
+        catch (final Exception e) {
+            LOG.error("Failed to write attachment response content to '"
+                            + destination.toAbsolutePath() + "'");
+            return;
+        }
 
+        webResponse.cleanUp();
+    }
+
+    private Path determineDestionationFile(final Page page, final String attachmentFilename) {
+        String fileName = attachmentFilename;
+
+        if (fileName == null || fileName.trim().isEmpty()) {
+            final String file = page.getWebResponse().getWebRequest().getUrl().getFile();
+            fileName = file.substring(file.lastIndexOf('/') + 1);
+        }
+
+        if (fileName.trim().isEmpty()) {
+            fileName = "download";
+        }
+
+        Path newPath = downloadFolder_.resolve(fileName);
+        int count = 1;
+        while (Files.exists(newPath, LinkOption.NOFOLLOW_LINKS)) {
+            final int fileExtensionstartPosition =
+                        (fileName.contains(".")) ? fileName.lastIndexOf('.') : fileName.length();
+            newPath = downloadFolder_.resolve(fileName.substring(0,
+                        fileExtensionstartPosition)
+                            .concat("(" + count + ")")
+                            .concat(fileName.substring(fileExtensionstartPosition)));
+            count++;
+        }
+
+        return newPath.toAbsolutePath();
+    }
 }
--- src/test/java/org/htmlunit/attachment/DownloadingAttachmentHandlerTest.java
+++ src/test/java/org/htmlunit/attachment/DownloadingAttachmentHandlerTest.java
@@ -1,49 +1,204 @@
+/*
+ * Copyright (c) 2002-2024 Gargoyle Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.htmlunit.attachment;
 
-import static org.junit.Assert.*;
-
 import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.nio.file.Path;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
-import org.htmlunit.BrowserVersion;
-import org.htmlunit.FailingHttpStatusCodeException;
+import org.apache.commons.io.FileUtils;
+import org.htmlunit.MockWebConnection;
+import org.htmlunit.Page;
+import org.htmlunit.SimpleWebTestCase;
 import org.htmlunit.WebClient;
 import org.htmlunit.html.HtmlAnchor;
 import org.htmlunit.html.HtmlPage;
 import org.htmlunit.junit.BrowserRunner;
-import org.junit.Assert;
-import org.junit.Rule;
+import org.htmlunit.util.MimeType;
+import org.htmlunit.util.NameValuePair;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 
-public class DownloadingAttachmentHandlerTest {
+/**
+ * Tests for {@link DownloadingAttachmentHandler}.
+ *
+ * @author Marek Andreansky
+ * @author Ronald Brill
+ */
+@RunWith(BrowserRunner.class)
+public class DownloadingAttachmentHandlerTest extends SimpleWebTestCase {
 
-	private static final String FILENAME_WITHOUT_EXTENSION = "fileNoDot";
-	private static final String FILENAME_WITH_EXTENSION = "file1.txt";
-	@Rule
-	public File downloadFolder = new TemporaryFolder().getRoot();
+    /**
+     * @throws Exception if an error occurs
+     */
+    @Test
+    public void basic() throws Exception {
+        final String content1 = "<html><body>\n"
+            + "<form method='POST' name='form' action='" + URL_SECOND + "'>\n"
+            + "<input type='submit' value='ok'>\n"
+            + "</form>\n"
+            + "<a href='#' onclick='document.form.submit()'>click me</a>\n"
+            + "</body></html>";
+        final String content2 = "download file contents";
 
-	@Test
-	public void whenDownloadFolderWriteable_downloadAttachment() throws IOException {
-		DownloadingAttachmentHandler downloadingAttachmentHandler = new DownloadingAttachmentHandler(
-				downloadFolder.toPath());
-//		TODO add html page with attachment response that is using content disposition
-//		TODO add html page with attachment response that is using octet stream
-//		new HtmlPage().
-		downloadingAttachmentHandler.handleAttachment(null, FILENAME_WITH_EXTENSION);
-		Assert.assertTrue(new File(downloadFolder, FILENAME_WITH_EXTENSION).exists());
-		downloadingAttachmentHandler.handleAttachment(null, FILENAME_WITHOUT_EXTENSION);
-		Assert.assertTrue(new File(downloadFolder, FILENAME_WITHOUT_EXTENSION).exists());
-	}
+        final File tmpFolder = new File(System.getProperty("java.io.tmpdir"));
+        final File downloadFolder = new File(tmpFolder, "downloading-attachment-test");
 
-	@Test
-	public void whenDownloadFolderDoesNotExit_throwException() {
-		Assert.assertThrows(IOException.class, () -> {
-			new DownloadingAttachmentHandler(downloadFolder.toPath().resolve("nonExistentFolder"));
-		});
-	}
+        try {
+            FileUtils.forceMkdir(downloadFolder);
 
+            assertEquals(0, FileUtils.listFiles(downloadFolder, null, false).size());
+
+            final WebClient client = getWebClient();
+            client.setAttachmentHandler(new DownloadingAttachmentHandler(downloadFolder.toPath()));
+
+            final List<NameValuePair> headers = new ArrayList<>();
+            headers.add(new NameValuePair("Content-Disposition", "attachment"));
+
+            final MockWebConnection conn = new MockWebConnection();
+            conn.setResponse(URL_FIRST, content1);
+            conn.setResponse(URL_SECOND, content2, 200, "OK", MimeType.TEXT_HTML, headers);
+            client.setWebConnection(conn);
+
+            final HtmlPage result = client.getPage(URL_FIRST);
+            final HtmlAnchor anchor = result.getAnchors().get(0);
+            final Page clickResult = anchor.click();
+            assertEquals(result, clickResult);
+
+            Collection<File> files = FileUtils.listFiles(downloadFolder, null, false);
+            assertEquals(1, files.size());
+            assertEquals("download", files.iterator().next().getName());
+
+            anchor.click();
+            files = FileUtils.listFiles(downloadFolder, null, false);
+            assertEquals(2, files.size());
+            final Iterator<File> filesIter = files.iterator();
+            assertEquals("download", filesIter.next().getName());
+            assertEquals("download(1)", filesIter.next().getName());
+        }
+        finally {
+            FileUtils.deleteDirectory(downloadFolder);
+        }
+    }
+
+    /**
+     * @throws Exception if an error occurs
+     */
+    @Test
+    public void basicFileNameFromUrl() throws Exception {
+        final String content1 = "<html><body>\n"
+            + "<form method='POST' name='form' action='" + URL_SECOND + "test.txt'>\n"
+            + "<input type='submit' value='ok'>\n"
+            + "</form>\n"
+            + "<a href='#' onclick='document.form.submit()'>click me</a>\n"
+            + "</body></html>";
+        final String content2 = "download file contents";
+
+        final File tmpFolder = new File(System.getProperty("java.io.tmpdir"));
+        final File downloadFolder = new File(tmpFolder, "downloading-attachment-test");
+
+        try {
+            FileUtils.forceMkdir(downloadFolder);
+
+            assertEquals(0, FileUtils.listFiles(downloadFolder, null, false).size());
+
+            final WebClient client = getWebClient();
+            client.setAttachmentHandler(new DownloadingAttachmentHandler(downloadFolder.toPath()));
+
+            final List<NameValuePair> headers = new ArrayList<>();
+            headers.add(new NameValuePair("Content-Disposition", "attachment"));
+
+            final MockWebConnection conn = new MockWebConnection();
+            conn.setResponse(URL_FIRST, content1);
+            conn.setResponse(new URL(URL_SECOND, "test.txt"), content2, 200, "OK", MimeType.TEXT_HTML, headers);
+            client.setWebConnection(conn);
+
+            final HtmlPage result = client.getPage(URL_FIRST);
+            final HtmlAnchor anchor = result.getAnchors().get(0);
+            final Page clickResult = anchor.click();
+            assertEquals(result, clickResult);
+
+            Collection<File> files = FileUtils.listFiles(downloadFolder, null, false);
+            assertEquals(1, files.size());
+            assertEquals("test.txt", files.iterator().next().getName());
+
+            anchor.click();
+            files = FileUtils.listFiles(downloadFolder, null, false);
+            assertEquals(2, files.size());
+            final Iterator<File> filesIter = files.iterator();
+            assertEquals("test(1).txt", filesIter.next().getName());
+            assertEquals("test.txt", filesIter.next().getName());
+        }
+        finally {
+            FileUtils.deleteDirectory(downloadFolder);
+        }
+    }
+
+    /**
+     * @throws Exception if an error occurs
+     */
+    @Test
+    public void basicFileNameFromHeader() throws Exception {
+        final String content1 = "<html><body>\n"
+            + "<form method='POST' name='form' action='" + URL_SECOND + "test.txt'>\n"
+            + "<input type='submit' value='ok'>\n"
+            + "</form>\n"
+            + "<a href='#' onclick='document.form.submit()'>click me</a>\n"
+            + "</body></html>";
+        final String content2 = "download file contents";
+
+        final File tmpFolder = new File(System.getProperty("java.io.tmpdir"));
+        final File downloadFolder = new File(tmpFolder, "downloading-attachment-test");
+
+        try {
+            FileUtils.forceMkdir(downloadFolder);
+
+            assertEquals(0, FileUtils.listFiles(downloadFolder, null, false).size());
+
+            final WebClient client = getWebClient();
+            client.setAttachmentHandler(new DownloadingAttachmentHandler(downloadFolder.toPath()));
+
+            final List<NameValuePair> headers = new ArrayList<>();
+            headers.add(new NameValuePair("Content-Disposition", "attachment; filename=sample.pdf"));
+
+            final MockWebConnection conn = new MockWebConnection();
+            conn.setResponse(URL_FIRST, content1);
+            conn.setResponse(new URL(URL_SECOND, "test.txt"), content2, 200, "OK", MimeType.TEXT_HTML, headers);
+            client.setWebConnection(conn);
+
+            final HtmlPage result = client.getPage(URL_FIRST);
+            final HtmlAnchor anchor = result.getAnchors().get(0);
+            final Page clickResult = anchor.click();
+            assertEquals(result, clickResult);
+
+            Collection<File> files = FileUtils.listFiles(downloadFolder, null, false);
+            assertEquals(1, files.size());
+            assertEquals("sample.pdf", files.iterator().next().getName());
+
+            anchor.click();
+            files = FileUtils.listFiles(downloadFolder, null, false);
+            assertEquals(2, files.size());
+            final Iterator<File> filesIter = files.iterator();
+            assertEquals("sample(1).pdf", filesIter.next().getName());
+            assertEquals("sample.pdf", filesIter.next().getName());
+        }
+        finally {
+            FileUtils.deleteDirectory(downloadFolder);
+        }
+    }
 }
