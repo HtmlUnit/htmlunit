168b95157289aa36bb80633d2bd94c4f16f6b8d7
Ronald Brill <rbri@rbri.de>
2024-07-14 14:56:17 +0200

    fix click handling, only process the download but not the posponed actions

--- src/test/java/org/htmlunit/libraries/HtmxTest.java
+++ src/test/java/org/htmlunit/libraries/HtmxTest.java
@@ -39,7 +39,7 @@ public abstract class HtmxTest extends WebDriverTestCase {
 
     private static final boolean BUG_HUNTING = false;
 
-    private static final int RETRIES = 2;
+    private static final int RETRIES = 1;
     private static final long RUN_TIME = 42 * DEFAULT_WAIT_TIME;
 
     protected void htmx(final String subDir) throws Exception {
