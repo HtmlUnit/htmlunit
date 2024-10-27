d27d5eb770fb5eac0865862e41682f3cd5ce9c89
Ronald Brill <rbri@rbri.de>
2024-10-27 14:24:06 +0100

    more code used for profiling

--- src/test/java/org/htmlunit/html/performance/HugePagePerformanceTest.java
+++ src/test/java/org/htmlunit/html/performance/HugePagePerformanceTest.java
@@ -31,17 +31,38 @@ public final class HugePagePerformanceTest {
     }
 
     public static void main(final String[] args) throws IOException {
-        final URL fileURL = WebClient.class.getClassLoader()
+//        final URL fileURL = WebClient.class.getClassLoader()
+//                .getResource("testfiles/huge-pages/html-standard-2024-10-17.html");
+        final URL benchmarkFileURL = WebClient.class.getClassLoader()
+                .getResource("testfiles/huge-pages/benchmark.html");
+        final URL htmlStandardfileURL = WebClient.class.getClassLoader()
                 .getResource("testfiles/huge-pages/html-standard-2024-10-17.html");
+        final URL wikipediaURL = WebClient.class.getClassLoader()
+                .getResource("testfiles/huge-pages/wikipedia.html");
 
         try (WebClient webClient = new WebClient(BrowserVersion.CHROME, false, null, -1)) {
             webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+            webClient.getOptions().setJavaScriptEnabled(false);
+            webClient.getOptions().setCssEnabled(false);
+            webClient.getOptions().setHistoryPageCacheLimit(0);
+            webClient.getOptions().setHistorySizeLimit(0);
+            webClient.getOptions().setWebSocketEnabled(false);
+            webClient.setFrameContentHandler(baseFrameElement -> false);
 
             final long start = System.currentTimeMillis();
-            for (int i = 0; i < 20; i++) {
-                webClient.getPage(fileURL);
-                System.out.println("  " + i);
+
+//            for (int i = 0; i < 20000; i++) {
+//                webClient.getPage(benchmarkFileURL);
+//            }
+
+            for (int i = 0; i < 40; i++) {
+                webClient.getPage(htmlStandardfileURL);
             }
+
+//            for (int i = 0; i < 10000; i++) {
+//                webClient.getPage(wikipediaURL);
+//            }
+
             System.out.println("## " + (System.currentTimeMillis() - start));
         }
     }
