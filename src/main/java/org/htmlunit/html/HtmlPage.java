43ec32b510a6ad02be946354ae188ba4f710d48e
Ronald Brill <rbri@rbri.de>
2024-11-24 18:49:53 +0100

    fix page index

--- src/main/java/org/htmlunit/html/HtmlPage.java
+++ src/main/java/org/htmlunit/html/HtmlPage.java
@@ -1743,12 +1743,7 @@ public class HtmlPage extends SgmlPage {
      */
     void notifyNodeAdded(final DomNode node) {
         if (node instanceof DomElement) {
-            final DomElement element = (DomElement) node;
-
-            if (ATTRIBUTE_NOT_DEFINED != element.getAttribute(DomElement.NAME_ATTRIBUTE)
-                    || ATTRIBUTE_NOT_DEFINED != element.getAttribute(DomElement.ID_ATTRIBUTE)) {
-                addMappedElement((DomElement) node, true);
-            }
+            addMappedElement((DomElement) node, true);
 
             if (node instanceof BaseFrameElement) {
                 frameElements_.add((BaseFrameElement) node);
@@ -1866,6 +1861,17 @@ public class HtmlPage extends SgmlPage {
                 removeElement(map, child, attribute, true);
             }
         }
+    }
+
+    /**
+     * Indicates if the attribute name indicates that the owning element is mapped.
+     * @param document the owning document
+     * @param attributeName the name of the attribute to consider
+     * @return {@code true} if the owning element should be mapped in its owning page
+     */
+    static boolean isMappedElement(final Document document, final String attributeName) {
+        return document instanceof HtmlPage
+            && (DomElement.NAME_ATTRIBUTE.equals(attributeName) || DomElement.ID_ATTRIBUTE.equals(attributeName));
     }
 
     private void calculateBase() {
