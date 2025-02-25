29d91b1f855079e09d9e09d48940df4d434c31b0
Ronald Brill <rbri@rbri.de>
2025-02-25 17:32:25 +0100

    back to the old NYI version

--- src/test/java/org/htmlunit/javascript/host/html/HTMLDialogElementTest.java
+++ src/test/java/org/htmlunit/javascript/host/html/HTMLDialogElementTest.java
@@ -17,6 +17,7 @@ package org.htmlunit.javascript.host.html;
 import org.htmlunit.WebDriverTestCase;
 import org.htmlunit.junit.BrowserRunner;
 import org.htmlunit.junit.annotation.Alerts;
+import org.htmlunit.junit.annotation.HtmlUnitNYI;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
@@ -758,6 +759,18 @@ public class HTMLDialogElementTest extends WebDriverTestCase {
     @Test
     @Alerts({"false", "true", "false",
              "[object Event]", "close", "false", "false", "false", "[object HTMLDialogElement]"})
+    @HtmlUnitNYI(CHROME = {"false", "true",
+                           "[object Event]", "close", "false", "false", "false", "[object HTMLDialogElement]",
+                           "false"},
+            EDGE = {"false", "true",
+                    "[object Event]", "close", "false", "false", "false", "[object HTMLDialogElement]",
+                    "false"},
+            FF = {"false", "true",
+                  "[object Event]", "close", "false", "false", "false", "[object HTMLDialogElement]",
+                  "false"},
+            FF_ESR = {"false", "true",
+                      "[object Event]", "close", "false", "false", "false", "[object HTMLDialogElement]",
+                      "false"})
     public void formClosesDialog() throws Exception {
         final String html =
             "<html>\n"
@@ -802,6 +815,18 @@ public class HTMLDialogElementTest extends WebDriverTestCase {
     @Test
     @Alerts({"false", "true", "false",
              "[object Event]", "close", "false", "false", "false", "[object HTMLDialogElement]"})
+    @HtmlUnitNYI(CHROME = {"false", "true",
+                           "[object Event]", "close", "false", "false", "false", "[object HTMLDialogElement]",
+                           "false"},
+            EDGE = {"false", "true",
+                    "[object Event]", "close", "false", "false", "false", "[object HTMLDialogElement]",
+                    "false"},
+            FF = {"false", "true",
+                  "[object Event]", "close", "false", "false", "false", "[object HTMLDialogElement]",
+                  "false"},
+            FF_ESR = {"false", "true",
+                      "[object Event]", "close", "false", "false", "false", "[object HTMLDialogElement]",
+                      "false"})
     public void formClosesDialogWithoutJs() throws Exception {
         final String html =
             "<html>\n"
