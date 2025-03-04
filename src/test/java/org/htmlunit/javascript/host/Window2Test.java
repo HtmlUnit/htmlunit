HtmlUnit/htmlunit

commit 7a3c32b354eaf9a90a9c7ae549aa6c7eb9694f62

use java.util.Base64

src/main/java/org/htmlunit/javascript/host/WindowOrWorkerGlobalScopeMixin.java
@@ -16,8 +16,8 @@ package org.htmlunit.javascript.host;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.Base64;
 
-import org.apache.commons.codec.binary.Base64;
 import org.htmlunit.Page;
 import org.htmlunit.WebWindow;
 import org.htmlunit.corejs.javascript.Context;
@@ -63,7 +63,7 @@ public final class WindowOrWorkerGlobalScopeMixin {
             }
         }
         final byte[] bytes = encodedData.getBytes(StandardCharsets.ISO_8859_1);
-        return new String(Base64.decodeBase64(bytes), StandardCharsets.ISO_8859_1);
+        return new String(Base64.getDecoder().decode(bytes), StandardCharsets.ISO_8859_1);
     }
 
     /**
@@ -79,7 +79,7 @@ public final class WindowOrWorkerGlobalScopeMixin {
             }
         }
         final byte[] bytes = stringToEncode.getBytes(StandardCharsets.ISO_8859_1);
-        return new String(Base64.encodeBase64(bytes), StandardCharsets.UTF_8);
+        return new String(Base64.getEncoder().encode(bytes), StandardCharsets.UTF_8);
     }
 
     /**

src/main/java/org/htmlunit/javascript/host/file/FileReader.java
@@ -18,9 +18,9 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.charset.UnsupportedCharsetException;
+import java.util.Base64;
 import java.util.Locale;
 
-import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.Charsets;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
@@ -106,7 +106,7 @@ public class FileReader extends EventTarget {
         result_ = DataURLConnection.DATA_PREFIX;
 
         final byte[] bytes = ((Blob) object).getBytes();
-        final String value = new String(Base64.encodeBase64(bytes), StandardCharsets.US_ASCII);
+        final String value = new String(Base64.getEncoder().encode(bytes), StandardCharsets.US_ASCII);
 
         String contentType = ((Blob) object).getType();
         if (StringUtils.isEmpty(contentType)) {

src/main/java/org/htmlunit/platform/canvas/rendering/AwtRenderingBackend.java
@@ -34,6 +34,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
@@ -43,7 +44,6 @@ import java.util.Map;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageReader;
 
-import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.htmlunit.platform.image.ImageIOImageData;
@@ -537,7 +537,7 @@ public class AwtRenderingBackend implements RenderingBackend {
             ImageIO.write(image_, imageType, bos);
 
             final byte[] imageBytes = bos.toByteArray();
-            return new String(Base64.encodeBase64(imageBytes), StandardCharsets.US_ASCII);
+            return new String(Base64.getEncoder().encode(imageBytes), StandardCharsets.US_ASCII);
         }
     }
 

src/main/java/org/htmlunit/protocol/data/DataUrlDecoder.java
@@ -23,9 +23,8 @@ import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.charset.IllegalCharsetNameException;
 import java.nio.charset.UnsupportedCharsetException;
+import java.util.Base64;
 
-import org.apache.commons.codec.DecoderException;
-import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
 import org.htmlunit.util.MimeType;
 
@@ -93,11 +92,11 @@ public class DataUrlDecoder {
             byte[] data = url.substring(comma + 1).getBytes(charset);
             data = decodeUrl(data);
             if (base64) {
-                data = Base64.decodeBase64(data);
+                data = Base64.getDecoder().decode(data);
             }
             return new DataUrlDecoder(data, mediaType, charset);
         }
-        catch (final DecoderException e) {
+        catch (final IllegalArgumentException e) {
             final UnsupportedEncodingException ex =
                     new UnsupportedEncodingException("Invalid data url: '" + url + "' (data decoding failed)");
             ex.initCause(e);
@@ -167,7 +166,7 @@ public class DataUrlDecoder {
     }
 
     // adapted from apache commons codec
-    private static byte[] decodeUrl(final byte[] bytes) throws DecoderException {
+    private static byte[] decodeUrl(final byte[] bytes) throws IllegalArgumentException  {
         if (bytes == null) {
             return null;
         }
@@ -181,7 +180,7 @@ public class DataUrlDecoder {
                     buffer.write((char) ((u << 4) + l));
                 }
                 catch (final ArrayIndexOutOfBoundsException e) {
-                    throw new DecoderException("Invalid URL encoding: ", e);
+                    throw new IllegalArgumentException("Invalid URL encoding: ", e);
                 }
             }
             else {
@@ -191,10 +190,10 @@ public class DataUrlDecoder {
         return buffer.toByteArray();
     }
 
-    private static int digit16(final byte b) throws DecoderException {
+    private static int digit16(final byte b) throws IllegalArgumentException  {
         final int i = Character.digit((char) b, 16);
         if (i == -1) {
-            throw new DecoderException("Invalid URL encoding: not a valid digit (radix 16): " + b);
+            throw new IllegalArgumentException("Invalid URL encoding: not a valid digit (radix 16): " + b);
         }
         return i;
     }

src/test/java/org/htmlunit/WebTestCase.java
@@ -29,6 +29,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
@@ -36,7 +37,6 @@ import java.util.function.Supplier;
 
 import javax.imageio.ImageIO;
 
-import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.SerializationUtils;
 import org.htmlunit.junit.RetryRule;
@@ -553,7 +553,7 @@ public abstract class WebTestCase {
 
     protected void compareImages(final String expected, final String current) throws IOException {
         final String currentBase64Image = current.split(",")[1];
-        final byte[] currentImageBytes = Base64.decodeBase64(currentBase64Image);
+        final byte[] currentImageBytes = Base64.getDecoder().decode(currentBase64Image);
 
         try (ByteArrayInputStream currentBis = new ByteArrayInputStream(currentImageBytes)) {
             final BufferedImage currentImage = ImageIO.read(currentBis);
@@ -565,7 +565,7 @@ public abstract class WebTestCase {
     protected void compareImages(final String expected,
             final String current, final BufferedImage currentImage) throws IOException {
         final String expectedBase64Image = expected.split(",")[1];
-        final byte[] expectedImageBytes = Base64.decodeBase64(expectedBase64Image);
+        final byte[] expectedImageBytes = Base64.getDecoder().decode(expectedBase64Image);
 
         try (ByteArrayInputStream expectedBis = new ByteArrayInputStream(expectedImageBytes)) {
             final BufferedImage expectedImage = ImageIO.read(expectedBis);

src/test/java/org/htmlunit/archunit/ArchitectureTest.java
@@ -97,8 +97,7 @@ public class ArchitectureTest {
     public static final ArchRule jdkBase64Rule = noClasses()
         .that()
             .resideOutsideOfPackage("org.htmlunit.jetty..")
-            .and().resideOutsideOfPackage("org.htmlunit.csp..")
-        .should().dependOnClassesThat().haveFullyQualifiedName("java.util.Base64");
+        .should().dependOnClassesThat().haveFullyQualifiedName("org.apache.commons.codec.binary.Base64");
 
 
     /**

commit ce985509abddda6f4f0fae52869144ef5fc843fd

Failing atob test case for malformed input
This should catch a regression potentially introduced in 7a3c32b354eaf9a90

src/test/java/org/htmlunit/javascript/host/Window2Test.java
@@ -185,6 +185,24 @@ public class Window2Test extends WebDriverTestCase {
      * @throws Exception if the test fails
      */
     @Test
+    @Alerts({"InvalidCharacterError/DOMException"})
+    public void atobMalformedInput() throws Exception {
+        final String html
+            = "<html><head></head><body>\n"
+            + "<script>\n"
+            + LOG_TITLE_FUNCTION
+            + "  try {\n"
+            + "    window.atob('b');\n"
+            + "  } catch(e) {logEx(e)}\n"
+            + "</script>\n"
+            + "</body></html>";
+        loadPageVerifyTitle2(html);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
     @Alerts({"InvalidCharacterError/DOMException", "InvalidCharacterError/DOMException"})
     public void atobUnicode() throws Exception {
         final String html