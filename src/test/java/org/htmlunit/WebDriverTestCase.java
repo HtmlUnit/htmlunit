b291cbc7190a2ac2058cc7250c942ad2bd5469be
Ronald Brill <rbri@rbri.de>
2025-04-16 19:07:17 +0200

    fix build

--- src/test/java/org/htmlunit/WebDriverTestCase.java
+++ src/test/java/org/htmlunit/WebDriverTestCase.java
@@ -89,7 +89,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
-import org.openqa.selenium.devtools.v134.emulation.Emulation;
+import org.openqa.selenium.devtools.v135.emulation.Emulation;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeDriverService;
 import org.openqa.selenium.edge.EdgeOptions;
