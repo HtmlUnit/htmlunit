5cc9a9e401bc6a13619957ce4c586473792b284c
Ronald Brill <rbri@rbri.de>
2025-06-01 18:46:40 +0200

    checkstyle

--- src/test/java/org/htmlunit/general/huge/CtorPrototypeTest.java
+++ src/test/java/org/htmlunit/general/huge/CtorPrototypeTest.java
@@ -12,7 +12,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.htmlunit.general.huge;import java.util.ArrayList;
+package org.htmlunit.general.huge;
+
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
