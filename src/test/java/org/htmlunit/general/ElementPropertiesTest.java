ddbddfba85c0e5c8f39f7f1969280dd4e1360022
Ronald Brill <rbri@rbri.de>
2025-09-12 13:27:07 +0200

    adjust expectations

--- src/test/java/org/htmlunit/general/ElementPropertiesTest.java
+++ src/test/java/org/htmlunit/general/ElementPropertiesTest.java
@@ -9593,6 +9593,22 @@ public class ElementPropertiesTest extends WebDriverTestCase {
                 + "rotateAxisAngle(),rotateFromVector(),scale(),scale3d(),scaleNonUniform(),"
                 + "skewX(),skewY(),toFloat32Array(),toFloat64Array(),toJSON(),toString(),"
                 + "transformPoint(),translate()")
+    @HtmlUnitNYI(CHROME = "a,b,c,d,e,f,flipX(),flipY(),inverse(),is2D,m11,m12,m13,m14,"
+                + "m21,m22,m23,m24,m31,m32,m33,m34,m41,m42,m43,m44,multiply(),rotate(),"
+                + "rotateAxisAngle(),"
+                + "skewX(),skewY(),toFloat32Array(),toFloat64Array(),translate()",
+            EDGE = "a,b,c,d,e,f,flipX(),flipY(),inverse(),is2D,m11,m12,m13,m14,"
+                + "m21,m22,m23,m24,m31,m32,m33,m34,m41,m42,m43,m44,multiply(),rotate(),"
+                + "rotateAxisAngle(),"
+                + "skewX(),skewY(),toFloat32Array(),toFloat64Array(),translate()",
+            FF = "a,b,c,d,e,f,flipX(),flipY(),inverse(),is2D,m11,m12,m13,m14,"
+                + "m21,m22,m23,m24,m31,m32,m33,m34,m41,m42,m43,m44,multiply(),rotate(),"
+                + "rotateAxisAngle(),"
+                + "skewX(),skewY(),toFloat32Array(),toFloat64Array(),translate()",
+            FF_ESR = "a,b,c,d,e,f,flipX(),flipY(),inverse(),is2D,m11,m12,m13,m14,"
+                + "m21,m22,m23,m24,m31,m32,m33,m34,m41,m42,m43,m44,multiply(),rotate(),"
+                + "rotateAxisAngle(),"
+                + "skewX(),skewY(),toFloat32Array(),toFloat64Array(),translate()")
     public void domMatrixReadOnly() throws Exception {
         testString("", "new DOMMatrixReadOnly()");
     }
@@ -9605,32 +9621,52 @@ public class ElementPropertiesTest extends WebDriverTestCase {
     @Test
     @Alerts(CHROME = "a,b,c,d,e,f,flipX(),flipY(),inverse(),invertSelf(),is2D,isIdentity,"
                 + "m11,m12,m13,m14,m21,m22,m23,m24,m31,m32,m33,m34,m41,m42,m43,m44,multiply(),"
-                + "multiplySelf(),preMultiplySelf(),rotate(),rotateAxisAngle(),"
+                + "rotate(),rotateAxisAngle(),"
                 + "rotateAxisAngleSelf(),rotateFromVector(),rotateFromVectorSelf(),rotateSelf(),"
                 + "scale(),scale3d(),scale3dSelf(),scaleNonUniform(),scaleSelf(),setMatrixValue(),"
                 + "skewX(),skewXSelf(),skewY(),skewYSelf(),toFloat32Array(),toFloat64Array(),"
                 + "toJSON(),toString(),transformPoint(),translate(),translateSelf()",
             EDGE = "a,b,c,d,e,f,flipX(),flipY(),inverse(),invertSelf(),is2D,isIdentity,"
                 + "m11,m12,m13,m14,m21,m22,m23,m24,m31,m32,m33,m34,m41,m42,m43,m44,multiply(),"
-                + "multiplySelf(),preMultiplySelf(),rotate(),rotateAxisAngle(),"
+                + "rotate(),rotateAxisAngle(),"
                 + "rotateAxisAngleSelf(),rotateFromVector(),rotateFromVectorSelf(),rotateSelf(),"
                 + "scale(),scale3d(),scale3dSelf(),scaleNonUniform(),scaleSelf(),setMatrixValue(),"
                 + "skewX(),skewXSelf(),skewY(),skewYSelf(),toFloat32Array(),toFloat64Array(),"
                 + "toJSON(),toString(),transformPoint(),translate(),translateSelf()",
             FF = "a,b,c,d,e,f,flipX(),flipY(),inverse(),invertSelf(),is2D,isIdentity,"
                 + "m11,m12,m13,m14,m21,m22,m23,m24,m31,m32,m33,m34,m41,m42,m43,m44,multiply(),"
-                + "multiplySelf(),preMultiplySelf(),rotate(),rotateAxisAngle(),"
+                + "rotate(),rotateAxisAngle(),"
                 + "rotateAxisAngleSelf(),rotateFromVector(),rotateFromVectorSelf(),rotateSelf(),"
                 + "scale(),scale3d(),scale3dSelf(),scaleNonUniform(),scaleSelf(),setMatrixValue(),"
                 + "skewX(),skewXSelf(),skewY(),skewYSelf(),toFloat32Array(),toFloat64Array(),"
                 + "toJSON(),toString(),transformPoint(),translate(),translateSelf()",
             FF_ESR = "a,b,c,d,e,f,flipX(),flipY(),inverse(),invertSelf(),is2D,isIdentity,"
                 + "m11,m12,m13,m14,m21,m22,m23,m24,m31,m32,m33,m34,m41,m42,m43,m44,multiply(),"
-                + "multiplySelf(),preMultiplySelf(),rotate(),rotateAxisAngle(),"
+                + "rotate(),rotateAxisAngle(),"
                 + "rotateAxisAngleSelf(),rotateFromVector(),rotateFromVectorSelf(),rotateSelf(),"
                 + "scale(),scale3d(),scale3dSelf(),scaleNonUniform(),scaleSelf(),setMatrixValue(),"
                 + "skewX(),skewXSelf(),skewY(),skewYSelf(),toFloat32Array(),toFloat64Array(),"
                 + "toJSON(),toString(),transformPoint(),translate(),translateSelf()")
+    @HtmlUnitNYI(CHROME = "a,b,c,d,e,f,flipX(),flipY(),inverse(),invertSelf(),is2D,"
+                + "m11,m12,m13,m14,m21,m22,m23,m24,m31,m32,m33,m34,m41,m42,m43,m44,multiply(),"
+                + "rotate(),rotateAxisAngle(),"
+                + "skewX(),skewY(),toFloat32Array(),toFloat64Array(),"
+                + "translate()",
+            EDGE = "a,b,c,d,e,f,flipX(),flipY(),inverse(),invertSelf(),is2D,"
+                + "m11,m12,m13,m14,m21,m22,m23,m24,m31,m32,m33,m34,m41,m42,m43,m44,multiply(),"
+                + "rotate(),rotateAxisAngle(),"
+                + "skewX(),skewY(),toFloat32Array(),toFloat64Array(),"
+                + "translate()",
+            FF = "a,b,c,d,e,f,flipX(),flipY(),inverse(),invertSelf(),is2D,"
+                + "m11,m12,m13,m14,m21,m22,m23,m24,m31,m32,m33,m34,m41,m42,m43,m44,multiply(),"
+                + "rotate(),rotateAxisAngle(),"
+                + "skewX(),skewY(),toFloat32Array(),toFloat64Array(),"
+                + "translate()",
+            FF_ESR = "a,b,c,d,e,f,flipX(),flipY(),inverse(),invertSelf(),is2D,"
+                + "m11,m12,m13,m14,m21,m22,m23,m24,m31,m32,m33,m34,m41,m42,m43,m44,multiply(),"
+                + "rotate(),rotateAxisAngle(),"
+                + "skewX(),skewY(),toFloat32Array(),toFloat64Array(),"
+                + "translate()")
     public void domMatrix() throws Exception {
         testString("", "new DOMMatrix()");
     }
