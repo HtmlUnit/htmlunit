337746c2edbd974dc2656ce0d1b2604d2c1f8759
Ronald Brill <rbri@rbri.de>
2025-09-16 13:58:28 +0200

    retested and min added

--- src/test/java/org/htmlunit/libraries/HtmxTest2x0x2.java
+++ src/test/java/org/htmlunit/libraries/HtmxTest2x0x2.java
@@ -14,6 +14,7 @@
  */
 package org.htmlunit.libraries;
 
+import org.htmlunit.WebClient;
 import org.htmlunit.junit.annotation.Alerts;
 import org.htmlunit.junit.annotation.HtmlUnitNYI;
 import org.junit.jupiter.api.Test;
@@ -33,9 +34,36 @@ public class HtmxTest2x0x2 extends HtmxTest {
             FF = "passes:579failures:0",
             FF_ESR = "passes:579failures:0")
     @HtmlUnitNYI(
-            CHROME = "passes:579failures:0",
-            EDGE = "passes:579failures:0")
+            CHROME = "passes:576failures:3",
+            EDGE = "passes:576failures:3",
+            FF = "passes:576failures:3",
+            FF_ESR = "passes:576failures:3")
     public void htmx() throws Exception {
-        htmx("htmx-2.0.2");
+        htmx("htmx-2.0.2", false);
+    }
+
+    /**
+     * @throws Exception if an error occurs
+     */
+    @Test
+    @Alerts(DEFAULT = "passes:514failures:66",
+            FF = "passes:514failures:65",
+            FF_ESR = "passes:514failures:65")
+    @HtmlUnitNYI(
+            CHROME = "passes:511failures:68",
+            EDGE = "passes:511failures:68",
+            FF = "passes:511failures:68",
+            FF_ESR = "passes:511failures:68")
+    public void htmxMin() throws Exception {
+        htmx("htmx-2.0.2", true);
+    }
+
+    @Override
+    protected void setupWebClient(final WebClient webClient) {
+        super.setupWebClient(webClient);
+
+        // min
+        // ReferenceError: "getInputValues" is not defined.
+        webClient.getOptions().setThrowExceptionOnScriptError(false);
     }
 }
