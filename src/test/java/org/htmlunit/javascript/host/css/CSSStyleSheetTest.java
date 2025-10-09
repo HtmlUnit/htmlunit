0d77e0d0d0d4ebee550ff1d61fce844e371cd247
Ronald Brill <rbri@rbri.de>
2025-10-09 10:51:54 +0200

    Chrome/Edge 141, Firefox 143, FirefoxESR 140

--- src/test/java/org/htmlunit/javascript/host/css/CSSStyleSheetTest.java
+++ src/test/java/org/htmlunit/javascript/host/css/CSSStyleSheetTest.java
@@ -2071,7 +2071,7 @@ public class CSSStyleSheetTest extends WebDriverTestCase {
     @Alerts(CHROME = "break at: 10 664.25 / 621",
             EDGE = "break at: 10 664.25 / 630",
             FF = "break at: 10 675.2000122070312 / 675",
-            FF_ESR = "break at: 11 734.63330078125 / 677")
+            FF_ESR = "break at: 10 675.2000122070312 / 675")
     @HtmlUnitNYI(CHROME = "break at: 16 637 / 605",
             EDGE = "break at: 16 637 / 605",
             FF = "break at: 15 616 / 605",
