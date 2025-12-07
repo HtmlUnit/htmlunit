6e0196abd5b20de6191f455460a2aea0764283e3
Ronald Brill <rbri@rbri.de>
2025-12-07 11:25:55 +0100

    ups

--- src/test/java/org/htmlunit/WebDriverTestCase.java
+++ src/test/java/org/htmlunit/WebDriverTestCase.java
@@ -90,7 +90,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
-import org.openqa.selenium.devtools.v141.emulation.Emulation;
+import org.openqa.selenium.devtools.v143.emulation.Emulation;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeDriverService;
 import org.openqa.selenium.edge.EdgeOptions;
