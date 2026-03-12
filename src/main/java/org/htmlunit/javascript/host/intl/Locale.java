947687a4ebc0184a1d0b6dae74d336fac7418693
duonglaiquang <duonglaiquang@gmail.com>
2026-03-12 15:23:19 +0900

    Locale: implement Intl.Locale

---
+++ src/main/java/org/htmlunit/javascript/host/intl/Locale.java
@@ -0,0 +1,326 @@
+/*
+ * Copyright (c) 2002-2026 Gargoyle Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.htmlunit.javascript.host.intl;
+
+import java.util.IllformedLocaleException;
+import java.util.List;
+
+import org.apache.commons.lang3.LocaleUtils;
+import org.htmlunit.corejs.javascript.Context;
+import org.htmlunit.corejs.javascript.Function;
+import org.htmlunit.corejs.javascript.FunctionObject;
+import org.htmlunit.corejs.javascript.Scriptable;
+import org.htmlunit.corejs.javascript.ScriptableObject;
+import org.htmlunit.javascript.HtmlUnitScriptable;
+import org.htmlunit.javascript.JavaScriptEngine;
+import org.htmlunit.javascript.configuration.JsxClass;
+import org.htmlunit.javascript.configuration.JsxConstructor;
+import org.htmlunit.javascript.configuration.JsxFunction;
+import org.htmlunit.javascript.configuration.JsxGetter;
+
+/**
+ * A JavaScript object for {@code Intl.Locale}.
+ *
+ * @author Lai Quang Duong
+ */
+@JsxClass
+public class Locale extends HtmlUnitScriptable {
+
+    private static final List<String> ALLOWED_HOUR_CYCLES = List.of("h11", "h12", "h23", "h24");
+    private static final List<String> ALLOWED_CASE_FIRSTS = List.of("upper", "lower", "false");
+
+    private java.util.Locale locale_;
+    private String language_;
+    private String script_;
+    private String region_;
+    private String calendar_;
+    private String collation_;
+    private String numberingSystem_;
+    private String caseFirst_;
+    private String hourCycle_;
+    private boolean numeric_;
+
+    /**
+     * Default constructor.
+     */
+    public Locale() {
+        super();
+    }
+
+    private Locale(final java.util.Locale locale) {
+        super();
+        locale_ = locale;
+        language_ = locale.getLanguage();
+        if (!locale.getScript().isEmpty()) {
+            script_ = locale.getScript();
+        }
+        if (!locale.getCountry().isEmpty()) {
+            region_ = locale.getCountry();
+        }
+        if (locale.hasExtensions()) {
+            calendar_ = locale.getUnicodeLocaleType("ca");
+            collation_ = locale.getUnicodeLocaleType("co");
+            numberingSystem_ = locale.getUnicodeLocaleType("nu");
+            caseFirst_ = locale.getUnicodeLocaleType("kf");
+            hourCycle_ = locale.getUnicodeLocaleType("hc");
+            numeric_ = Boolean.parseBoolean(locale.getUnicodeLocaleType("kn"));
+        }
+    }
+
+    /**
+     * JavaScript constructor.
+     * @param cx the current context
+     * @param scope the scope
+     * @param args the arguments
+     * @param ctorObj the constructor function
+     * @param inNewExpr whether called via new
+     * @return the new Locale instance
+     */
+    @JsxConstructor
+    public static Scriptable jsConstructor(final Context cx, final Scriptable scope,
+            final Object[] args, final Function ctorObj, final boolean inNewExpr) {
+        if (args.length == 0 || JavaScriptEngine.isUndefined(args[0])) {
+            throw JavaScriptEngine.typeError("Invalid element in locales argument");
+        }
+
+        final String languageTag;
+        if (args[0] instanceof Locale loc) {
+            languageTag = loc.toString();
+        }
+        else {
+            languageTag = JavaScriptEngine.toString(args[0]);
+        }
+        if (languageTag.isEmpty()) {
+            throw JavaScriptEngine.rangeError("Invalid language tag: ");
+        }
+
+        java.util.Locale locale;
+        try {
+            locale = new java.util.Locale.Builder()
+                    .setLanguageTag(languageTag)
+                    .build();
+        }
+        catch (final IllformedLocaleException e) {
+            throw JavaScriptEngine.rangeError("Invalid language tag: " + languageTag);
+        }
+
+        // Override by options if present
+        if (args.length > 1 && !JavaScriptEngine.isUndefined(args[1])) {
+            locale = overrideExistingWithOptions(locale,
+                    ScriptableObject.ensureScriptableObject(args[1]));
+        }
+
+        final Locale l = new Locale(locale);
+        l.setParentScope(getTopLevelScope(ctorObj));
+        l.setPrototype(((FunctionObject) ctorObj).getClassPrototype());
+        return l;
+    }
+
+    private static java.util.Locale overrideExistingWithOptions(
+            final java.util.Locale existing, final ScriptableObject options) {
+        final java.util.Locale.Builder builder = new java.util.Locale.Builder().setLocale(existing);
+
+        setStringOption(builder, options, "language");
+        setStringOption(builder, options, "script");
+        setStringOption(builder, options, "region");
+        setUnicodeKeyword(builder, options, "calendar", "ca", null);
+        setUnicodeKeyword(builder, options, "collation", "co", null);
+        setUnicodeKeyword(builder, options, "numberingSystem", "nu", null);
+        setUnicodeKeyword(builder, options, "caseFirst", "kf", ALLOWED_CASE_FIRSTS);
+        setUnicodeKeyword(builder, options, "hourCycle", "hc", ALLOWED_HOUR_CYCLES);
+
+        final Object numeric = ScriptableObject.getProperty(options, "numeric");
+        if (numeric != Scriptable.NOT_FOUND && !JavaScriptEngine.isUndefined(numeric)) {
+            final boolean isNumeric = numeric instanceof Boolean ? (Boolean) numeric : true;
+            builder.setUnicodeLocaleKeyword("kn", Boolean.toString(isNumeric));
+        }
+
+        return builder.build();
+    }
+
+    private static void setStringOption(final java.util.Locale.Builder builder,
+            final ScriptableObject options, final String optionName) {
+        final Object value = ScriptableObject.getProperty(options, optionName);
+        if (value == Scriptable.NOT_FOUND || JavaScriptEngine.isUndefined(value)) {
+            return;
+        }
+        try {
+            final String s = JavaScriptEngine.toString(value);
+            switch (optionName) {
+                case "language":
+                    builder.setLanguage(s);
+                    break;
+                case "script":
+                    builder.setScript(s);
+                    break;
+                case "region":
+                    builder.setRegion(s);
+                    break;
+                default:
+                    break;
+            }
+        }
+        catch (final Exception e) {
+            throw JavaScriptEngine.rangeError("Invalid value for option \"" + optionName + "\"");
+        }
+    }
+
+    private static void setUnicodeKeyword(final java.util.Locale.Builder builder,
+            final ScriptableObject options, final String optionName, final String unicodeKey,
+            final List<String> allowedValues) {
+        final Object value = ScriptableObject.getProperty(options, optionName);
+        if (value == Scriptable.NOT_FOUND || JavaScriptEngine.isUndefined(value)) {
+            return;
+        }
+        final String s;
+        try {
+            s = JavaScriptEngine.toString(value);
+        }
+        catch (final Exception e) {
+            throw JavaScriptEngine.rangeError("Invalid value for option \"" + optionName + "\"");
+        }
+        if (allowedValues != null && !allowedValues.contains(s)) {
+            throw JavaScriptEngine.rangeError("Invalid value for option \"" + optionName + "\"");
+        }
+        builder.setUnicodeLocaleKeyword(unicodeKey, s);
+    }
+
+    /** @return the language */
+    @JsxGetter
+    public Object getLanguage() {
+        return language_ != null ? language_ : JavaScriptEngine.UNDEFINED;
+    }
+
+    /** @return the script */
+    @JsxGetter
+    public Object getScript() {
+        return script_ != null ? script_ : JavaScriptEngine.UNDEFINED;
+    }
+
+    /** @return the region */
+    @JsxGetter
+    public Object getRegion() {
+        return region_ != null ? region_ : JavaScriptEngine.UNDEFINED;
+    }
+
+    /** @return the calendar type */
+    @JsxGetter
+    public Object getCalendar() {
+        return calendar_ != null ? calendar_ : JavaScriptEngine.UNDEFINED;
+    }
+
+    /** @return the collation type */
+    @JsxGetter
+    public Object getCollation() {
+        return collation_ != null ? collation_ : JavaScriptEngine.UNDEFINED;
+    }
+
+    /** @return the numbering system */
+    @JsxGetter
+    public Object getNumberingSystem() {
+        return numberingSystem_ != null ? numberingSystem_ : JavaScriptEngine.UNDEFINED;
+    }
+
+    /** @return the case first setting */
+    @JsxGetter
+    public Object getCaseFirst() {
+        return caseFirst_ != null ? caseFirst_ : JavaScriptEngine.UNDEFINED;
+    }
+
+    /** @return the hour cycle */
+    @JsxGetter
+    public Object getHourCycle() {
+        return hourCycle_ != null ? hourCycle_ : JavaScriptEngine.UNDEFINED;
+    }
+
+    /** @return whether numeric sorting is used */
+    @JsxGetter
+    public boolean isNumeric() {
+        return numeric_;
+    }
+
+    /** @return the base name (without Unicode extensions) */
+    @JsxGetter
+    public Object getBaseName() {
+        final String variant = locale_.getVariant().replace("_", "-");
+        return language_
+                + (script_ != null ? "-" + script_ : "")
+                + (region_ != null ? "-" + region_ : "")
+                + (!variant.isEmpty() ? "-" + variant : "");
+    }
+
+    /**
+     * Returns a Locale with maximized subtags.
+     * @return a new Locale instance with maximized subtags
+     */
+    @JsxFunction
+    public Object maximize() {
+        final String region;
+        if (region_ != null) {
+            region = region_;
+        }
+        else {
+            final java.util.List<java.util.Locale> locales =
+                    LocaleUtils.countriesByLanguage(language_);
+            if (!locales.isEmpty()) {
+                region = locales.get(0).getCountry();
+            }
+            else {
+                region = null;
+            }
+        }
+
+        final java.util.Locale locale = new java.util.Locale.Builder()
+                .setLanguage(language_)
+                .setScript(script_)
+                .setRegion(region)
+                .setExtension('u', locale_.getExtension('u'))
+                .build();
+
+        final Locale l = new Locale(locale);
+        l.setParentScope(getWindow(this));
+        l.setPrototype(this.getPrototype());
+        return l;
+    }
+
+    /**
+     * Returns a Locale with minimized subtags.
+     * @return a new Locale instance with minimized subtags
+     */
+    @JsxFunction
+    public Object minimize() {
+        final java.util.Locale locale = new java.util.Locale.Builder()
+                .setLanguage(language_)
+                .setExtension('u', locale_.getExtension('u'))
+                .build();
+
+        final Locale l = new Locale(locale);
+        l.setParentScope(getWindow(this));
+        l.setPrototype(this.getPrototype());
+        return l;
+    }
+
+    /**
+     * @return the locale's Unicode locale identifier string
+     */
+    @Override
+    @JsxFunction
+    public String toString() {
+        if (locale_ == null) {
+            return super.toString();
+        }
+        return locale_.toLanguageTag();
+    }
+}
---
+++ src/test/java/org/htmlunit/javascript/host/intl/LocaleTest.java
@@ -0,0 +1,270 @@
+/*
+ * Copyright (c) 2002-2026 Gargoyle Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.htmlunit.javascript.host.intl;
+
+import org.htmlunit.WebDriverTestCase;
+import org.htmlunit.junit.annotation.Alerts;
+import org.htmlunit.junit.annotation.HtmlUnitNYI;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for {@link Locale}.
+ *
+ * @author Lai Quang Duong
+ */
+public class LocaleTest extends WebDriverTestCase {
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts({"en", "undefined", "undefined", "en", "en",
+             "zh", "undefined", "Hant", "zh-Hant", "zh-Hant",
+             "zh", "TW", "Hant", "zh-Hant-TW", "zh-Hant-TW",
+             "ja", "JP", "undefined", "ja-JP", "ja-JP",
+             "en", "US", "undefined", "en-US", "en-US",
+             "ja-JP"})
+    public void construction() throws Exception {
+        final String html = DOCTYPE_HTML
+            + "<html><head>\n"
+            + "<script>\n"
+            + LOG_TITLE_FUNCTION
+            + "  function test() {\n"
+            + "    function check(l) { log(l.language); log(l.region); log(l.script); log(l.baseName); log(l.toString()); }\n"
+            + "    check(new Intl.Locale('en'));\n"
+            + "    check(new Intl.Locale('zh-Hant'));\n"
+            + "    check(new Intl.Locale('zh-Hant-TW'));\n"
+            + "    check(new Intl.Locale('ja-JP'));\n"
+            + "    check(new Intl.Locale('EN-US'));\n"
+            + "    log(new Intl.Locale(new Intl.Locale('ja-JP')).toString());\n"
+            + "  }\n"
+            + "</script>\n"
+            + "</head><body onload='test()'>\n"
+            + "</body></html>";
+
+        loadPageVerifyTitle2(html);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts({"TypeError", "RangeError", "RangeError", "RangeError",
+             "invalid", "en-US", "RangeError", "RangeError"})
+    public void constructorErrors() throws Exception {
+        final String html = DOCTYPE_HTML
+            + "<html><head>\n"
+            + "<script>\n"
+            + LOG_TITLE_FUNCTION
+            + "  function tryLocale(tag) { try { log(new Intl.Locale(tag).toString()); } catch(e) { logEx(e) } }\n"
+            + "  function test() {\n"
+            + "    try { new Intl.Locale(); } catch(e) { logEx(e) }\n"
+            + "    tryLocale('123');\n"
+            + "    tryLocale('');\n"
+            + "    tryLocale('a');\n"
+            + "    tryLocale('invalid');\n"
+            + "    tryLocale(['en-US']);\n"
+            + "    tryLocale([]);\n"
+            + "    tryLocale(['en-US', 'fr']);\n"
+            + "  }\n"
+            + "</script>\n"
+            + "</head><body onload='test()'>\n"
+            + "</body></html>";
+
+        loadPageVerifyTitle2(html);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts({"undefined", "undefined", "undefined", "undefined", "undefined", "false",
+             "buddhist", "emoji", "latn", "h23", "lower", "true",
+             "ja", "JP", "Jpan", "japanese", "h12", "ja-Jpan-JP",
+             "de", "phonebk", "de-u-co-phonebk",
+             "th", "TH", "thai", "th-TH-u-nu-thai",
+             "en", "upper", "en-u-kf-upper"})
+    public void extensions() throws Exception {
+        final String html = DOCTYPE_HTML
+            + "<html><head>\n"
+            + "<script>\n"
+            + LOG_TITLE_FUNCTION
+            + "  function test() {\n"
+            + "    function check(l) { log(l.calendar); log(l.collation); log(l.numberingSystem); log(l.hourCycle); log(l.caseFirst); log(l.numeric); }\n"
+            + "    check(new Intl.Locale('en-US'));\n"
+            + "    check(new Intl.Locale('en-US-u-ca-buddhist-co-emoji-nu-latn-hc-h23-kf-lower-kn-true'));\n"
+            + "    var l = new Intl.Locale('ja-Jpan-JP-u-ca-japanese-hc-h12');\n"
+            + "    log(l.language); log(l.region); log(l.script);\n"
+            + "    log(l.calendar); log(l.hourCycle); log(l.baseName);\n"
+            + "    var co = new Intl.Locale('de-u-co-phonebk');\n"
+            + "    log(co.language); log(co.collation); log(co.toString());\n"
+            + "    var nu = new Intl.Locale('th-TH-u-nu-thai');\n"
+            + "    log(nu.language); log(nu.region); log(nu.numberingSystem); log(nu.toString());\n"
+            + "    var kf = new Intl.Locale('en-u-kf-upper');\n"
+            + "    log(kf.language); log(kf.caseFirst); log(kf.toString());\n"
+            + "  }\n"
+            + "</script>\n"
+            + "</head><body onload='test()'>\n"
+            + "</body></html>";
+
+        loadPageVerifyTitle2(html);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts({"en", "true", "en-u-kn",
+             "false", "en-u-kn-false",
+             "false", "en-u-kn-false"})
+    @HtmlUnitNYI(CHROME = {"en", "true", "en-u-kn-true",
+                           "false", "en-u-kn-false",
+                           "false", "en-u-kn-false"},
+            EDGE = {"en", "true", "en-u-kn-true",
+                    "false", "en-u-kn-false",
+                    "false", "en-u-kn-false"},
+            FF = {"en", "true", "en-u-kn-true",
+                  "false", "en-u-kn-false",
+                  "false", "en-u-kn-false"},
+            FF_ESR = {"en", "true", "en-u-kn-true",
+                      "false", "en-u-kn-false",
+                      "false", "en-u-kn-false"})
+    public void numeric() throws Exception {
+        final String html = DOCTYPE_HTML
+            + "<html><head>\n"
+            + "<script>\n"
+            + LOG_TITLE_FUNCTION
+            + "  function test() {\n"
+            + "    var l = new Intl.Locale('en-u-kn-true');\n"
+            + "    log(l.language); log(l.numeric); log(l.toString());\n"
+            + "    var l1 = new Intl.Locale('en-u-kn-false');\n"
+            + "    log(l1.numeric); log(l1.toString());\n"
+            + "    var l2 = new Intl.Locale('en', {numeric: false});\n"
+            + "    log(l2.numeric); log(l2.toString());\n"
+            + "  }\n"
+            + "</script>\n"
+            + "</head><body onload='test()'>\n"
+            + "</body></html>";
+
+        loadPageVerifyTitle2(html);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts({"en", "RU", "Cyrl", "buddhist", "zhuyin", "thai", "h11", "true",
+             "en", "CA", "en-CA",
+             "en", "GB", "en-GB",
+             "islamic", "en-US-u-ca-islamic",
+             "buddhist", "en-US-u-ca-buddhist"})
+    public void optionsOverride() throws Exception {
+        final String html = DOCTYPE_HTML
+            + "<html><head>\n"
+            + "<script>\n"
+            + LOG_TITLE_FUNCTION
+            + "  function test() {\n"
+            + "    var l = new Intl.Locale('fr-Latn-CA', {\n"
+            + "      language: 'en', script: 'Cyrl', region: 'RU',\n"
+            + "      calendar: 'buddhist', collation: 'zhuyin',\n"
+            + "      numberingSystem: 'thai', hourCycle: 'h11',\n"
+            + "      numeric: true\n"
+            + "    });\n"
+            + "    log(l.language); log(l.region); log(l.script);\n"
+            + "    log(l.calendar); log(l.collation); log(l.numberingSystem);\n"
+            + "    log(l.hourCycle); log(l.numeric);\n"
+            + "    var l1 = new Intl.Locale('fr-CA', {language: 'en'});\n"
+            + "    log(l1.language); log(l1.region); log(l1.toString());\n"
+            + "    var l2 = new Intl.Locale('en-US', {region: 'GB'});\n"
+            + "    log(l2.language); log(l2.region); log(l2.toString());\n"
+            + "    var l3 = new Intl.Locale('en-US', {calendar: 'islamic'});\n"
+            + "    log(l3.calendar); log(l3.toString());\n"
+            + "    var l4 = new Intl.Locale('en-US-u-ca-gregory', {calendar: 'buddhist'});\n"
+            + "    log(l4.calendar); log(l4.toString());\n"
+            + "  }\n"
+            + "</script>\n"
+            + "</head><body onload='test()'>\n"
+            + "</body></html>";
+
+        loadPageVerifyTitle2(html);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts({"RangeError", "RangeError", "RangeError", "RangeError", "RangeError"})
+    public void invalidOptions() throws Exception {
+        final String html = DOCTYPE_HTML
+            + "<html><head>\n"
+            + "<script>\n"
+            + LOG_TITLE_FUNCTION
+            + "  function test() {\n"
+            + "    try { new Intl.Locale('en', {language: '123'}); } catch(e) { logEx(e) }\n"
+            + "    try { new Intl.Locale('en', {region: 'TOOLONG'}); } catch(e) { logEx(e) }\n"
+            + "    try { new Intl.Locale('en', {script: 'X'}); } catch(e) { logEx(e) }\n"
+            + "    try { new Intl.Locale('en', {hourCycle: 'invalid'}); } catch(e) { logEx(e) }\n"
+            + "    try { new Intl.Locale('en', {caseFirst: 'invalid'}); } catch(e) { logEx(e) }\n"
+            + "  }\n"
+            + "</script>\n"
+            + "</head><body onload='test()'>\n"
+            + "</body></html>";
+
+        loadPageVerifyTitle2(html);
+    }
+
+    /**
+     * @throws Exception if the test fails
+     */
+    @Test
+    @Alerts({"ja-Jpan-JP-u-ca-japanese-hc-h12",
+             "ja-u-ca-japanese-hc-h12",
+             "ja-Jpan-JP-u-ca-japanese-hc-h12",
+             "ja-Jpan-JP"})
+    @HtmlUnitNYI(CHROME = {"ja-Jpan-JP-u-ca-japanese-hc-h12",
+                           "ja-u-ca-japanese-hc-h12",
+                           "ja-JP-u-ca-japanese-hc-h12",
+                           "ja-Jpan-JP"},
+            EDGE = {"ja-Jpan-JP-u-ca-japanese-hc-h12",
+                    "ja-u-ca-japanese-hc-h12",
+                    "ja-JP-u-ca-japanese-hc-h12",
+                    "ja-Jpan-JP"},
+            FF = {"ja-Jpan-JP-u-ca-japanese-hc-h12",
+                  "ja-u-ca-japanese-hc-h12",
+                  "ja-JP-u-ca-japanese-hc-h12",
+                  "ja-Jpan-JP"},
+            FF_ESR = {"ja-Jpan-JP-u-ca-japanese-hc-h12",
+                      "ja-u-ca-japanese-hc-h12",
+                      "ja-JP-u-ca-japanese-hc-h12",
+                      "ja-Jpan-JP"})
+    public void minimizeMaximize() throws Exception {
+        final String html = DOCTYPE_HTML
+            + "<html><head>\n"
+            + "<script>\n"
+            + LOG_TITLE_FUNCTION
+            + "  function test() {\n"
+            + "    var l = new Intl.Locale('ja-Jpan-JP-u-ca-japanese-hc-h12');\n"
+            + "    log(l.toString());\n"
+            + "    log(l.minimize().toString());\n"
+            + "    log(new Intl.Locale('ja-u-ca-japanese-hc-h12').maximize().toString());\n"
+            + "    log(l.baseName);\n"
+            + "  }\n"
+            + "</script>\n"
+            + "</head><body onload='test()'>\n"
+            + "</body></html>";
+
+        loadPageVerifyTitle2(html);
+    }
+}
