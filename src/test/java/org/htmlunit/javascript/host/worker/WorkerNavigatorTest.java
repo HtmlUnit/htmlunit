e161d15ae7bfd159877512abfb95e9815910a7da
Ronald Brill <rbri@rbri.de>
2026-06-18 13:07:11 +0200

    FF 152

--- src/test/java/org/htmlunit/javascript/host/worker/WorkerNavigatorTest.java
+++ src/test/java/org/htmlunit/javascript/host/worker/WorkerNavigatorTest.java
@@ -166,7 +166,7 @@ public class WorkerNavigatorTest extends WebDriverTestCase {
                         + "(KHTML,\\slike\\sGecko)\\sChrome/149.0.0.0\\sSafari/537.36",
             EDGE = "Mozilla/5.0\\s(Windows\\sNT\\s10.0;\\sWin64;\\sx64)\\sAppleWebKit/537.36\\s"
                         + "(KHTML,\\slike\\sGecko)\\sChrome/149.0.0.0\\sSafari/537.36\\sEdg/149.0.0.0",
-            FF = "Mozilla/5.0\\s(Windows\\sNT\\s10.0;\\sWin64;\\sx64;\\srv:151.0)\\sGecko/20100101\\sFirefox/151.0",
+            FF = "Mozilla/5.0\\s(Windows\\sNT\\s10.0;\\sWin64;\\sx64;\\srv:152.0)\\sGecko/20100101\\sFirefox/152.0",
             FF_ESR = "Mozilla/5.0\\s(Windows\\sNT\\s10.0;\\sWin64;\\sx64;\\srv:140.0)\\sGecko/20100101\\sFirefox/140.0")
     public void userAgent() throws Exception {
         final String workerJs = "postMessage(navigator.userAgent);\n";
