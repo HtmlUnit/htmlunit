0c717b7111842fd176c1803612a11cf5d35d310a
Ronald Brill <rbri@rbri.de>
2026-06-30 16:53:26 +0200

    escharts 6.1.0

---
+++ src/test/java/org/htmlunit/libraries/echarts/ECharts_6_1_0_Test.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2002-2026 Gargoyle Software Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.htmlunit.libraries.echarts;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.Duration;
+import java.util.Base64;
+
+import org.apache.commons.io.IOUtils;
+import org.htmlunit.WebDriverTestCase;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Tests for Apache ECharts.
+ *
+ * @author Ronald Brill
+ */
+public class ECharts_6_1_0_Test extends WebDriverTestCase {
+
+    /**
+     * @throws Exception if an error occurs
+     */
+    @Test
+    public void sample() throws Exception {
+        startWebServer("src/test/resources/libraries/echarts/echarts_6_1_0", null);
+
+        readExpectedAlertFromPng("sample");
+
+        final WebDriver driver = getWebDriver();
+        driver.get(URL_FIRST + "index.html");
+
+        driver.findElement(By.id("printButtonId")).click();
+
+        final WebElement textArea = driver.findElement(By.id("myLog"));
+        waitForOutput(DEFAULT_WAIT_TIME.multipliedBy(1_000), textArea);
+
+        verifyTextArea2(driver, getExpectedAlerts());
+    }
+
+    private static void waitForOutput(final Duration maxWaitTime, final WebElement textArea) throws Exception {
+        final long maxWait = System.currentTimeMillis() + maxWaitTime.toMillis();
+
+        while (System.currentTimeMillis() < maxWait) {
+            final String value = textArea.getDomProperty("value");
+            if (value != null && value.startsWith("data:image/png;base64,")) {
+                break;
+            }
+
+            Thread.sleep(100);
+        }
+    }
+
+
+    private void readExpectedAlertFromPng(final String testName) throws IOException {
+        String resourceName = testName + "/nyi_" + getBrowserVersion().getNickname() + ".png";
+        if (!useRealBrowser()) {
+            try (InputStream is = getClass().getResourceAsStream(resourceName)) {
+                if (is != null) {
+                    final byte[] bytes = IOUtils.toByteArray(is);
+                    final String alert = "data:image/png;base64," + Base64.getEncoder().encodeToString(bytes);
+                    setExpectedAlerts(alert);
+
+                    return;
+                }
+            }
+        }
+
+        resourceName = testName + "/expected_" + getBrowserVersion().getNickname() + ".png";
+        try (InputStream is = getClass().getResourceAsStream(resourceName)) {
+            if (is == null) {
+                throw new IllegalArgumentException("Resource named '" + resourceName + "' not found");
+            }
+
+            final byte[] bytes = IOUtils.toByteArray(is);
+            final String alert = "data:image/png;base64," + Base64.getEncoder().encodeToString(bytes);
+            setExpectedAlerts(alert);
+        }
+    }
+}
