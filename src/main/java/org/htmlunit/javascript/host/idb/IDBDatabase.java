758426617cc729bdcd82c10e0f4a3dfba1c8277a
Ronald Brill <rbri@rbri.de>
2026-07-09 13:21:23 +0200

    improved jdoc

--- src/main/java/org/htmlunit/javascript/host/idb/IDBDatabase.java
+++ src/main/java/org/htmlunit/javascript/host/idb/IDBDatabase.java
@@ -23,6 +23,8 @@ import org.htmlunit.javascript.host.event.EventTarget;
  *
  * @author Ahmed Ashour
  * @author Ronald Brill
+ *
+ * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/IDBDatabase">MDN Documentation</a>
  */
 @JsxClass
 public class IDBDatabase extends EventTarget {
--- src/main/java/org/htmlunit/javascript/host/idb/IDBIndex.java
+++ src/main/java/org/htmlunit/javascript/host/idb/IDBIndex.java
@@ -23,6 +23,8 @@ import org.htmlunit.javascript.configuration.JsxConstructor;
  *
  * @author Ahmed Ashour
  * @author Ronald Brill
+ *
+ * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/IDBIndex">MDN Documentation</a>
  */
 @JsxClass
 public class IDBIndex extends HtmlUnitScriptable {
