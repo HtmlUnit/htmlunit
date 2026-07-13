9f35edf9e628bdc6e16a257627a06c0ae34d1f2f
Ronald Brill <rbri@rbri.de>
2026-07-13 12:48:38 +0200

    improved jdoc

--- src/main/java/org/htmlunit/javascript/host/payment/PaymentResponse.java
+++ src/main/java/org/htmlunit/javascript/host/payment/PaymentResponse.java
@@ -22,16 +22,18 @@ import org.htmlunit.javascript.configuration.JsxConstructor;
 import org.htmlunit.javascript.host.event.EventTarget;
 
 /**
- * A JavaScript object for {@code PaymentResponse}.
+ * JavaScript host object for {@code PaymentResponse}.
  *
  * @author Ahmed Ashour
  * @author Ronald Brill
+ *
+ * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/PaymentResponse">MDN Documentation</a>
  */
 @JsxClass({CHROME, EDGE})
 public class PaymentResponse extends EventTarget {
 
     /**
-     * JavaScript constructor.
+     * Creates an instance of this object.
      */
     @Override
     @JsxConstructor
